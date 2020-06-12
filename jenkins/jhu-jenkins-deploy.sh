cf85ec4dcdbac7fa8fb4f55f822713a6be693905
Brian Clark <clark.bg@gmail.com>
2020-06-12 12:04:44 -0400

    Add variable comment, add nascent thought

--- jenkins/jhu-jenkins-deploy.sh
+++ jenkins/jhu-jenkins-deploy.sh
@@ -15,4 +15,4 @@ indicator="jhu"
 cd "${WORKSPACE}/ansible" || exit
 
 # Ansible!
-ansible-playbook "${indicator}"-ansible-deploy.yaml -i inventory
+ansible-playbook "${indicator}"-ansible-deploy.yaml --extra-vars "indicator=$(" -i inventory

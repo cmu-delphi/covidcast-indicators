6189f63da253b6ad6d1bc11d0039e35f7f2e6cf8
Brian Clark <clark.bg@gmail.com>
2020-06-12 14:43:39 -0400

    Pass indicator from shell script as extra-vars in Ansible invocation

--- jenkins/jhu-jenkins-deploy.sh
+++ jenkins/jhu-jenkins-deploy.sh
@@ -15,4 +15,4 @@ indicator="jhu"
 cd "${WORKSPACE}/ansible" || exit
 
 # Ansible!
-ansible-playbook "${indicator}"-ansible-deploy.yaml --extra-vars "indicator=$(" -i inventory
+ansible-playbook "${indicator}"-ansible-deploy.yaml --extra-vars "indicator=${indicator}" -i inventory

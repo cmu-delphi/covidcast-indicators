0505c2a466ebdd9a7a3d141a48391d9750e888de
Chua Eu Jing <chuaeujing@gmail.com>
2020-06-15 23:43:51 -0400

    Added test for run

---
+++ cdc_covidnet/tests/test_run.py
@@ -0,0 +1,22 @@
+from os.path import join, exists
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from delphi_cdc_covidnet.run import run_module
+
+class TestRun:
+    def test_match_old_to_new_output(self):
+        output_fnames = ["20200307_state_wip_covidnet.csv", "20200314_state_wip_covidnet.csv"]
+
+        # Run the whole program to completion
+        run_module()
+
+        for fname in output_fnames:
+            # Target output file exist
+            assert exists(join("receiving", fname))
+
+            # Contents match
+            expected_df = pd.read_csv(join("receiving_test", fname))
+            actual_df = pd.read_csv(join("receiving", fname))
+            assert_frame_equal(expected_df, actual_df, check_less_precise=5)

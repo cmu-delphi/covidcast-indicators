5cadcb310e5d4cd916d2efc6c0e85e6771f2afc1
Alex Reinhart <alex@refsmmat.com>
2020-07-02 14:11:35 -0400

    Remove individual test output during teardown

--- facebook/delphiFacebook/tests/testthat/teardown-run.R
+++ facebook/delphiFacebook/tests/testthat/teardown-run.R
@@ -5,6 +5,7 @@ files <- c(
   dir(test_path("weights_out"), full.names = TRUE, all.files = TRUE, no.. = TRUE),
   dir(test_path("receiving"), full.names = TRUE, all.files = TRUE, no.. = TRUE),
   dir(test_path("receiving_full"), full.names = TRUE, all.files = TRUE, no.. = TRUE),
+  dir(test_path("individual_full"), full.names = TRUE, all.files = TRUE, no.. = TRUE),
   dir(test_path("archive"), full.names = TRUE, all.files = TRUE, no.. = TRUE)
 )
 sapply(files, file.remove)
@@ -14,3 +15,4 @@ file.remove(test_path("weights_out"))
 file.remove(test_path("receiving"))
 file.remove(test_path("archive"))
 file.remove(test_path("receiving_full"))
+file.remove(test_path("individual_full"))

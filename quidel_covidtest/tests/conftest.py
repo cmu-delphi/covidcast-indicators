df302b3facdc3e4874c9e56946b787df6278dfb2
Jingjing Tang <jingjingtang@Jingjings-MacBook-Pro.local>
2020-08-01 10:04:31 -0400

    fixed errors in case:no new data

--- quidel_covidtest/tests/conftest.py
+++ quidel_covidtest/tests/conftest.py
@@ -16,6 +16,6 @@ def run_as_module():
             os.remove(join("receiving", fname))
     for fname in os.listdir("cache"):
         if ".csv" in fname:
-            os.remove(join("receiving", fname))
+            os.remove(join("cache", fname))
 
     run_module()

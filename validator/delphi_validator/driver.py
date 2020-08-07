7b23269816f1687c1830c4ed80ec108ef56e3d8e
amartyabasu <polarisedprodigy@gmail.com>
2020-08-07 19:01:27 -0400

    Removing prototype code.

--- validator/delphi_validator/driver.py
+++ validator/delphi_validator/driver.py
@@ -1,10 +1,6 @@
 from fbsurveyvalidation import *
 from datafetcher import *
 
-# Defining start date and end date for the last fb-survey pipeline execution
-survey_sdate = "2020-06-13"
-survey_edate = "2020-06-15"
-
 # Dev Alert: Remove template extention
 params = read_params("..\params.json.template")
 dtobj_sdate = datetime.strptime(params['start_date'], '%Y-%m-%d')

b9a86efbc5e8fc9416dfc05ff3b89bd36344857f
Vishakha <59063647+vishakha1812@users.noreply.github.com>
2020-08-11 23:40:12 -0700

    Added new constants.py file

---
+++ google_health/delphi_google_health/constants.py
@@ -0,0 +1,14 @@
+"""
+REGISTRY FOR SIGANL NAMES, GEO TYPES
+"""
+RAW = "raw_search"
+SMOOTHED = "smoothed_search"
+STATE = "state"
+HRR = "hrr"
+MSA = "msa"
+DMA = "dma"
+
+SIGNALS = [RAW, SMOOTHED]
+GEO_TYPES = [STATE, HRR, MSA, DMA]
+
+

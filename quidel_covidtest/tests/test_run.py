bfb422e353a443bc2e3a18f536700246efafefd5
Vishakha <59063647+vishakha1812@users.noreply.github.com>
2020-08-14 14:27:27 -0700

    Updated test_run.py to include new sensor names

--- quidel_covidtest/tests/test_run.py
+++ quidel_covidtest/tests/test_run.py
@@ -3,12 +3,15 @@ from os.path import join
 
 import pandas as pd
 
+from delphi_utils import read_params
 from delphi_quidel_covidtest.run import run_module
+from delphi_quidel_covidtest.constants import GEO_RESOLUTIONS, SENSORS
+from delphi_quidel_covidtest.handle_wip_sensor import add_prefix
 
 
 class TestRun:
     def test_output_files(self, run_as_module):
-        
+
         # Test output exists
         csv_files = listdir("receiving")
 
@@ -25,13 +28,10 @@ class TestRun:
             "20200619",
             "20200620",
         ]
-        geos = ["county", "hrr", "msa", "state"]
-        sensors = [
-            "covid_ag_raw_pct_positive",
-            "covid_ag_smoothed_pct_positive",
-            "wip_covid_ag_raw_test_per_device",
-            "wip_covid_ag_smoothed_test_per_device"
-        ]
+        geos = GEO_RESOLUTIONS.copy()
+        sensors = add_prefix(SENSORS,
+                             wip_signal=read_params()["wip_signal"],
+                             prefix="wip_")
 
         expected_files = []
         for date in dates:
@@ -40,17 +40,16 @@ class TestRun:
                     expected_files += [date + "_" + geo + "_" + sensor + ".csv"]
 
         assert set(expected_files).issubset(set(csv_files))
-        
+
         # Test output format
         df = pd.read_csv(
             join("./receiving", "20200610_state_covid_ag_raw_pct_positive.csv")
         )
         assert (df.columns.values == ["geo_id", "val", "se", "sample_size"]).all()
-        
+
         # test_intermediate_file
         flag = None
         for fname in listdir("./cache"):
             if ".csv" in fname:
                 flag = 1
         assert flag is not None
-        

786cef4916f66c9145ee04269e1437bcd88b5fac
Vishakha <59063647+vishakha1812@users.noreply.github.com>
2020-08-26 12:48:15 -0700

    Add constants.py

---
+++ emr_hosp/delphi_emr_hosp/constants.py
@@ -0,0 +1,13 @@
+"""Registry for signal names and geo types"""
+SMOOTHED = "smoothed_covid19"
+SMOOTHED_ADJ = "smoothed_adj_covid19"
+SIGNALS = [SMOOTHED, SMOOTHED_ADJ]
+
+GEO_TYPES = ["msa", "hrr", "state", "county"]
+
+MSA = "msa"
+HRR = "hrr"
+STATE = "state"
+COUNTY = "county"
+FIPS = "fips"
+NA = "NA"

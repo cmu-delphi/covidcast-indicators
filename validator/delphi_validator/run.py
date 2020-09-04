9c54f438084f75f85342616d0719cc1067be25cf
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2020-09-04 16:24:22 -0400

    [wip] validation package\n*replace print/exit with raised exceptions

--- validator/delphi_validator/run.py
+++ validator/delphi_validator/run.py
@@ -7,15 +7,12 @@ when the module is run with `python -m delphi_validator`.
 import numpy as np
 import pandas as pd
 from delphi_utils import read_params
-from .fbsurveyvalidation import fbsurvey_validation
-from .datafetcher import *
+from .validate import validate
+from .datafetcher import read_filenames
 
 
 def run_module():
-
-    params = read_params()
-
-
+    params = read_params()["validation"]
     dtobj_sdate = datetime.strptime(params['start_date'], '%Y-%m-%d')
     dtobj_edate = datetime.strptime(params['end_date'], '%Y-%m-%d')
     max_check_lookbehind = int(params["ref_window_size"])
@@ -23,4 +20,4 @@ def run_module():
     # Collecting all filenames
     daily_filnames = read_filenames(params["data_folder"])
 
-    fbsurvey_validation(daily_filnames, dtobj_sdate, dtobj_edate)
+    validate(daily_filnames, dtobj_sdate, dtobj_edate)

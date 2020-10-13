4a7951eb12c1a806a3394f15e4c06c3a8cf06365
nmdefries <42820733+nmdefries@users.noreply.github.com>
2020-10-13 18:34:52 -0400

    Update __init__.py

--- validator/delphi_validator/__init__.py
+++ validator/delphi_validator/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Module to pull and clean indicators from the XXXXX source.
+"""Module to validate indicator source data before uploading to the public COVIDcast API.
 
 This file defines the functions that are made public by the module. As the
 module is intended to be executed though the main method, these are primarily

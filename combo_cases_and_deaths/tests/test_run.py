6da359821ae5ea9cec293254ce097724deb151af
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2020-10-22 10:37:48 -0400

    Add docstrings to test functions

--- combo_cases_and_deaths/tests/test_run.py
+++ combo_cases_and_deaths/tests/test_run.py
@@ -11,6 +11,12 @@ from delphi_combo_cases_and_deaths.constants import METRICS, SMOOTH_TYPES, SENSO
 
 
 def test_issue_dates():
+    """The smoothed value for a particular date is computed from the raw
+    values for a span of dates. We want users to be able to see in the
+    API all the raw values that went into the smoothed computation,
+    for transparency and peer review. This means that each issue
+    should contain more days of raw data than smoothed data.
+    """
     reference_dr = [date.today(), date.today()]
     params = {'date_range': reference_dr}
     n_changed = 0
@@ -23,14 +29,16 @@ def test_issue_dates():
         if dr[0] != reference_dr[0]:
             n_changed += 1
             variants_changed.append(sensor_name)
-    assert n_changed == len(variants) / 2, f"""Raw variants should post more days than smoothed.
+    assert n_changed == len(variants) / 2, f"""
+Raw variants should post more days than smoothed.
 All variants: {variants}
 Date-extended variants: {variants_changed}
 """
 
 
 def test_handle_wip_signal():
-
+    """Verify that "wip_" prefixes are being applied appropriately.
+    """
     signal_list = [sensor_signal(metric, sensor, smoother)[1]
                    for (metric, sensor, smoother) in
                    product(METRICS, SENSORS, SMOOTH_TYPES)]
@@ -51,6 +59,10 @@ def test_handle_wip_signal():
     assert all(not s.startswith("wip_") for s in signal_names[1:])
 
 def test_unstable_sources():
+    """Verify that combine_usafacts_and_jhu assembles the combined data
+    frame correctly for all cases where 0, 1, or both signals are
+    available.
+    """
     placeholder = lambda geo: pd.DataFrame(
         [(date.today(),"pr" if geo == "state" else "72000",1,1,1,0)],
         columns="time_value geo_value value stderr sample_size direction".split())
@@ -70,12 +82,16 @@ def test_unstable_sources():
         ]:
             df = combine_usafacts_and_jhu("", geo, date_range, fetcher)
             assert df.size == expected_size * len(COLUMN_MAPPING), f"""
+Wrong number of rows in combined data frame for the number of available signals.
+
 input for {geo}:
 {fetcher('usa-facts',geo)}
 {fetcher('jhu-csse',geo)}
 
 output:
 {df}
+
+expected rows: {expected_size}
 """
 
 class MyTestCase(unittest.TestCase):

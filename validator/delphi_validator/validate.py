5d8f2207b13cca2456c6b3e675af53462ddc6ec3
JedGrabman <jedg@google.com>
2020-11-02 17:15:54 -0500

    First draft of geo_id validator
    Code to validate that all geo_id values are valid, by comparing against a list of known values.

--- validator/delphi_validator/validate.py
+++ validator/delphi_validator/validate.py
@@ -244,6 +244,23 @@ class Validator():
 
         self.increment_total_checks()
 
+    def check_bad_geo_value(self, geo_type, df_to_test):
+        """
+        Check for unknown geo_ids, by comparing to historic data
+
+        Arguments:
+            - geo_type: string from CSV name specifying geo type (state, county, msa, etc.) of data
+            - df_to_test: pandas dataframe of CSV source data containing the geo_ids to check
+        """
+        file_path = join(r'../validator/csv', geo_type + '_geo.csv')
+        valid_geo_df = pd.read_csv(file_path, dtype = {'geo_id': str})
+        valid_geos = valid_geo_df['geo_value'].values
+        unexpected_geos = [geo for geo in df_to_test['geo_id'] if geo not in valid_geos]
+        if len(unexpected_geos) > 0:
+            self.raised_errors.append(ValidationError(
+                ("check_bad_geo_value", geo_type),
+                unexpected_geos, "invalid geo_ids found"))
+
     def check_bad_geo_id(self, df_to_test, nameformat, geo_type):
         """
         Check validity of geo type and values, according to regex pattern.
@@ -722,6 +739,7 @@ class Validator():
             self.check_df_format(data_df, filename)
             self.check_bad_geo_id(
                 data_df, filename, match.groupdict()['geo_type'])
+            self.check_bad_geo_value(match.groupdict()['geo_type'], data_df)
             self.check_bad_val(data_df, filename, match.groupdict()['signal'])
             self.check_bad_se(data_df, filename)
             self.check_bad_sample_size(data_df, filename)

4d6f65e5c32863631feb1a415663249f20fd65e3
Dmitry Shemetov <dshemetov@ucdavis.edu>
2020-11-04 18:18:48 -0800

    Updated smoothing utility: * mostly documentation fixes, confrm to pydocstyle * add missing documentation for the argument boundary_method * remove the len(signal) < window_length constraint; forgot I wrote the boundary methods to handle these cases

--- _delphi_utils_python/tests/test_smooth.py
+++ _delphi_utils_python/tests/test_smooth.py
@@ -167,11 +167,10 @@ class TestSmoothers:
         with pytest.raises(ValueError):
             imputed_signal = smoother.savgol_impute(signal)
 
-        # test window_length > len(signal)
+        # test window_length > len(signal) and boundary_method="identity"
         signal = np.arange(20)
         smoother = Smoother(smoother_name="savgol", boundary_method="identity", window_length=30)
-        with pytest.raises(ValueError):
-            smoothed_signal = smoother.smooth(signal)
+        smoothed_signal = smoother.smooth(signal)
 
         # test the boundary methods
         signal = np.arange(20)

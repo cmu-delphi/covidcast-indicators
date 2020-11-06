5ecc1ecafa892fc055e8f1fbc0332295eaa1a418
JedGrabman <jedg@google.com>
2020-11-06 18:06:10 -0500

    Capture geo_ids not in case data

--- validator/scripts/unique_geoids.R
+++ validator/scripts/unique_geoids.R
@@ -2,9 +2,11 @@ library(covidcast)
 
 geo_types = c("county", "state", "hrr", "msa")
 for(type in geo_types){
-  dtf = covidcast_signal("indicator-combination", "confirmed_7dav_incidence_num", start_day = "2020-10-01", end_day = "2020-10-01", geo_type = type)
+  dtf_deaths = covidcast_signal("jhu-csse" ,"deaths_incidence_num", start_day = "2020-10-01", end_day = "2020-10-01", geo_type = type)
+  dtf_cases = covidcast_signal("indicator-combination", "confirmed_7dav_incidence_num", start_day = "2020-10-01", end_day = "2020-10-01", geo_type = type)
+  unique_geo_values = sort(union(dtf_deaths$geo_value, dtf_cases$geo_value))
   file_name = paste0("../static/", type, "_geo.csv")
-  write.table(unique(dtf$geo_value), file = file_name, row.names = F, col.names = "geo_id")
+  write.table(unique_geo_values, file = file_name, row.names = F, col.names = "geo_id")
 }
 
 dtf = covidcast_signal("ght", "raw_search", start_day = "2020-10-01", end_day = "2020-10-01", geo_type = "dma")

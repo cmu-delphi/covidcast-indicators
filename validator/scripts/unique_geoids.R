22e88e2b193df340724055216fe747600bd6942e
JedGrabman <jedg@google.com>
2020-11-09 13:35:57 -0500

    Make geo_id checks robust
    Automatically determine signals and data sources to use for retrieving geo_values. This adds robustness at the cost of efficiency.

--- validator/scripts/unique_geoids.R
+++ validator/scripts/unique_geoids.R
@@ -1,15 +1,18 @@
 library(covidcast)
+library(dplyr)
+meta_info = covidcast_meta()
+locations_by_type = meta_info %>% group_by(geo_type) %>% summarize(Value = max(num_locations))
 
-geo_types = c("county", "state", "hrr", "msa")
-for(type in geo_types){
-  dtf = covidcast_signal("indicator-combination", "confirmed_7dav_incidence_num", start_day = "2020-10-01", end_day = "2020-10-01", geo_type = type)
+results = list()
+for (i in 1:nrow(locations_by_type)){
+  type = locations_by_type$geo_type[i]
+  max_locations = locations_by_type$Value[i]
+  max_row = with(meta_info, meta_info[geo_type == type & num_locations == max_locations,][1,])
+  data_source = max_row$data_source
+  signal = max_row$signal
+  results[[i]] = covidcast_signal(data_source, signal, geo_type = type)
+  geo_values = sort(unique(results[[i]]$geo_value))
   file_name = paste0("../static/", type, "_geo.csv")
-  write.table(unique(dtf$geo_value), file = file_name, row.names = F, col.names = "geo_id")
+  write.table(geo_values, file = file_name, row.names = F, col.names = "geo_id")
 }
 
-dtf = covidcast_signal("ght", "raw_search", start_day = "2020-10-01", end_day = "2020-10-01", geo_type = "dma")
-file_name = "../static/dma_geo.csv"
-write.table(unique(dtf$geo_value), file = file_name, row.names = F, col.names = "geo_id")
-
-national_file = "../static/national_geo.csv"
-write.table("us", file = national_file, row.names = F, col.names = "geo_id")

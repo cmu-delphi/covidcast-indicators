a7eff9cf4ff053464beea05e133fbe2f39fb79db
Mike O'Brien <mikeob@google.com>
2020-11-11 11:24:28 -0500

    expand export to supercede indicator-level export functions

--- _delphi_utils_python/delphi_utils/export.py
+++ _delphi_utils_python/delphi_utils/export.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from os.path import join
+from typing import Optional
 
 import pandas as pd
 
 def create_export_csv(
     df: pd.DataFrame,
-    start_date: datetime,
     export_dir: str,
-    metric: str,
     geo_res: str,
     sensor: str,
+    start_date: Optional[datetime] = None,
+    metric: Optional[str] = None
 ):
     """Export data in the format expected by the Delphi API.
 
@@ -21,21 +22,30 @@ def create_export_csv(
         Columns: geo_id, timestamp, val, se, sample_size
     export_dir: str
         Export directory
-    metric: str
-        Metric we are considering
     geo_res: str
         Geographic resolution to which the data has been aggregated
     sensor: str
         Sensor that has been calculated (cumulative_counts vs new_counts)
+    start_date: Optional[datetime]
+        Earliest date to export.  If None, all dates are exported.
+    metric: Optional[str]
+        Metric we are considering, if any
     """
     df = df.copy()
+
     df["timestamp"] = pd.to_datetime(df["timestamp"])
+    if start_date is None:
+        start_date = min(df["timestamp"])
     dates = pd.Series(
         df[df["timestamp"] >= start_date]["timestamp"].unique()
     ).sort_values()
+
     for date in dates:
-        export_fn = f'{date.strftime("%Y%m%d")}_{geo_res}_' f"{metric}_{sensor}.csv"
-        export_file = join(export_dir, export_fn)
+        if metric is None:
+            export_filename = f"{date.strftime('%Y%m%d')}_{geo_res}_{sensor}.csv"
+        else:
+            export_filename = f"{date.strftime('%Y%m%d')}_{geo_res}_{metric}_{sensor}.csv"
+        export_file = join(export_dir, export_filename)
         df[df["timestamp"] == date][["geo_id", "val", "se", "sample_size",]].to_csv(
             export_file, index=False, na_rep="NA"
         )
--- _delphi_utils_python/tests/test_export.py
+++ _delphi_utils_python/tests/test_export.py
@@ -1,5 +1,4 @@
-import pytest
-
+"""Tests for exporting CSV files."""
 from datetime import datetime
 from os import listdir, remove
 from os.path import join
@@ -9,28 +8,32 @@ from delphi_utils import create_export_csv
 
 
 class TestExport:
-    def test_export_csv(self):
+    # List of times for data points.
+    TIMES = [
+        datetime.strptime(x, "%Y-%m-%d")
+        for x in ["2020-02-15", "2020-02-15", "2020-03-01", "2020-03-15"]
+    ]
+
+    # A sample data frame
+    DF = pd.DataFrame(
+        {
+            "geo_id": ["51093", "51175", "51175", "51620"],
+            "timestamp": TIMES,
+            "val": [3.6, 2.1, 2.2, 2.6],
+            "se": [0.15, 0.22, 0.20, 0.34],
+            "sample_size": [100, 100, 101, 100],
+        }
+    )
+
+    def test_export_with_metric(self):
+        """Test that exporting CSVs with the `metrics` argument yields the correct files."""
 
         # Clean receiving directory
         for fname in listdir("test_dir"):
             remove(join("test_dir", fname))
 
-        times = [
-            datetime.strptime(x, "%Y-%m-%d")
-            for x in ["2020-02-15", "2020-02-15", "2020-03-01", "2020-03-15"]
-        ]
-        df = pd.DataFrame(
-            {
-                "geo_id": ["51093", "51175", "51175", "51620"],
-                "timestamp": times,
-                "val": [3.6, 2.1, 2.2, 2.6],
-                "se": [0.15, 0.22, 0.20, 0.34],
-                "sample_size": [100, 100, 101, 100],
-            }
-        )
-
         create_export_csv(
-            df=df,
+            df=self.DF,
             start_date=datetime.strptime("2020-02-15", "%Y-%m-%d"),
             export_dir="test_dir",
             metric="deaths",
@@ -43,5 +46,72 @@ class TestExport:
                 "20200215_county_deaths_test.csv",
                 "20200301_county_deaths_test.csv",
                 "20200315_county_deaths_test.csv",
+            ]
+        )
+
+    def test_export_without_metric(self):
+        """Test that exporting CSVs without the `metrics` argument yields the correct files."""
+
+        # Clean receiving directory
+        for fname in listdir("test_dir"):
+            remove(join("test_dir", fname))
+
+        create_export_csv(
+            df=self.DF,
+            start_date=datetime.strptime("2020-02-15", "%Y-%m-%d"),
+            export_dir="test_dir",
+            geo_res="county",
+            sensor="test",
+        )
+
+        assert set(listdir("test_dir")) == set(
+            [
+                "20200215_county_test.csv",
+                "20200301_county_test.csv",
+                "20200315_county_test.csv",
+            ]
+        )
+
+    def test_export_with_limiting_start_date(self):
+        """Test that the `start_date` prevents earlier dates from being exported."""
+
+        # Clean receiving directory
+        for fname in listdir("test_dir"):
+            remove(join("test_dir", fname))
+
+        create_export_csv(
+            df=self.DF,
+            start_date=datetime.strptime("2020-02-20", "%Y-%m-%d"),
+            export_dir="test_dir",
+            geo_res="county",
+            sensor="test",
+        )
+
+        assert set(listdir("test_dir")) == set(
+            [
+                "20200301_county_test.csv",
+                "20200315_county_test.csv",
+            ]
+        )
+
+    def test_export_with_no_start_date(self):
+        """Test that omitting the `start_date` exports all dates."""
+
+        # Clean receiving directory
+        for fname in listdir("test_dir"):
+            remove(join("test_dir", fname))
+
+        create_export_csv(
+            df=self.DF,
+            export_dir="test_dir",
+            geo_res="state",
+            sensor="test",
+        )
+
+        assert set(listdir("test_dir")) == set(
+            [
+                "20200215_state_test.csv",
+                "20200301_state_test.csv",
+                "20200315_state_test.csv",
             ]
         )

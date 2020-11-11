e326205e734bb6273e693249241a96507885d2fe
Brian Clark <clark.bg@gmail.com>
2020-11-11 16:49:58 -0500

    Add new shell script for build-and-package stage

---
+++ jenkins/jenkins-build-and-package.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#
+# Jenkins build-and-package
+#
+
+set -eo pipefail
+source ~/.bash_profile
+
+# Vars
+local_indicator=$1
+
+#
+# Build
+#
+
+cd "${WORKSPACE}/${local_indicator}" || exit
+
+# Set up venv
+python -m venv env
+source env/bin/activate
+pip install ../_delphi_utils_python/.
+pip install 
+
+#
+# Package
+#
+
+cd "${WORKSPACE}" || exit
+
+# Create .tar.gz for deployment
+# TODO Rename test-artifacts to artifacts when this works!!!
+tar -czvf "${JENKINS_HOME}/test-artifacts/${local_indicator}.tar.gz" "${local_indicator}"
+cd "${WORKSPACE}" || exit
+
+# Create .tar.gz for deployment
+tar -czvf "${JENKINS_HOME}/test-artifacts/${local_indicator}.tar.gz" "${local_indicator}"

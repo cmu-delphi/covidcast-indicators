0c0d05e278be34912aa59f91151468f1c4504eba
rumackaaron <adr66@cornell.edu>
2020-11-11 13:32:37 -0500

    Change signal name again

--- changehc/delphi_changehc/constants.py
+++ changehc/delphi_changehc/constants.py
@@ -1,6 +1,6 @@
 """Registry for signal names and geo types"""
-SMOOTHED = "smoothed_covid"
-SMOOTHED_ADJ = "smoothed_adj_covid"
+SMOOTHED = "smoothed_outpatient_covid"
+SMOOTHED_ADJ = "smoothed_adj_outpatient_covid"
 SIGNALS = [SMOOTHED, SMOOTHED_ADJ]
 NA = "NA"
 HRR = "hrr"

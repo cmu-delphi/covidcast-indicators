115c341b72fd48d0976ef8f53920016b170637cd
Brian Clark <clark.bg@gmail.com>
2020-11-13 19:58:37 -0500

    Add new Jenkinsfile and supporting shell scripts

---
+++ jenkins/build-and-package.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# Jenkins build-and-package
+#
+
+set -eo pipefail
+source ~/.bash_profile
+
+# Vars
+local_indicator=$1
+
+#
+# Build
+#
+
+cd "${WORKSPACE}/${local_indicator}" || exit
+
+# Set up venv
+python -m venv env
+source env/bin/activate
+pip install ../_delphi_utils_python/.
+pip install .
+
+#
+# Package
+#
+
+cd "${WORKSPACE}" || exit
+
+# Create .tar.gz for deployment
+# TODO: Switcheroo to `artifacts` after this goes live!
+tar -czvf "${JENKINS_HOME}/test-artifacts/${local_indicator}.tar.gz" "${local_indicator}"

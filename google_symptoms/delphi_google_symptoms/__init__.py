79c481a195de882e7cbf941b0dd085d71121b90d
andrew <chinandrew96@gmail.com>
2020-11-19 14:17:07 -0800

    Get google symptoms to pass pydocstyle

--- google_symptoms/delphi_google_symptoms/__init__.py
+++ google_symptoms/delphi_google_symptoms/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-"""Module to pull and clean indicators from the Google Research's Open
-   COVID-19 Data project.
+"""Module to pull and clean indicators from the Google Research's Open COVID-19 Data project.
+
 This file defines the functions that are made public by the module. As the
 module is intended to be executed though the main method, these are primarily
 for testing.

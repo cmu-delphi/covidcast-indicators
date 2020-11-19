767a22c1c27834ee2bb5a5b722f48c654b0b9078
Brian Clark <clark.bg@gmail.com>
2020-11-19 16:34:24 -0500

    Add pip upgrade step, add pip retry/timeout to build + package

--- jenkins/build-and-package.sh
+++ jenkins/build-and-package.sh
@@ -18,8 +18,9 @@ cd "${WORKSPACE}/${local_indicator}" || exit
 # Set up venv
 python -m venv env
 source env/bin/activate
-pip install ../_delphi_utils_python/.
-pip install .
+pip install --upgrade pip --retries 10 --timeout 20
+pip install ../_delphi_utils_python/. --retries 10 --timeout 20
+pip install . --retries 10 --timeout 20
 
 #
 # Package

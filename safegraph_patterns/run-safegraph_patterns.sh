1b17cf53e5a01491eee4188a77b82e66522aa792
Kathryn M Mazaitis <krivard@cs.cmu.edu>
2020-11-20 12:05:43 -0500

    Move safegraph patterns results to correct directory

--- safegraph_patterns/run-safegraph_patterns.sh
+++ safegraph_patterns/run-safegraph_patterns.sh
@@ -16,5 +16,5 @@ env/bin/python -m delphi_safegraph_patterns
 # Copy the files to the ingestion directory.
 echo "Copying files to the ingestion directory..."
 # Hack to make cp care less about missing recent files since we don't always have them.
-cp $(date +"receiving/%Y%m*") /common/covidcast/receiving/safegraph_patterns 2>/dev/null
-cp $(date --date='-1 month' +"receiving/%Y%m*") /common/covidcast/receiving/safegraph_patterns
+cp $(date +"receiving/%Y%m*") /common/covidcast/receiving/safegraph 2>/dev/null
+cp $(date --date='-1 month' +"receiving/%Y%m*") /common/covidcast/receiving/safegraph

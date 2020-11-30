cc34365c8f98eb85ab88c41d80f4813eda54e2b7
Mike O'Brien <mikeob@google.com>
2020-11-30 11:35:40 -0500

    change order of super().__init__() call to preserve error function messages

--- validator/delphi_validator/errors.py
+++ validator/delphi_validator/errors.py
@@ -31,8 +31,8 @@ class ValidationError(Exception):
             pass a check, provide the date
             - message: str explaining why an error was raised
         """
+        super().__init__(message)
         self.check_data_id = (check_data_id,) if not isinstance(
             check_data_id, tuple) and not isinstance(check_data_id, list) else tuple(check_data_id)
         self.expression = expression
         self.message = message
-        super().__init__(self.message)

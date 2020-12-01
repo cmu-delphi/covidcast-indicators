2cb25535e5a89b8aa837b00c92ccda44a41bd178
rumackaaron <adr66@cornell.edu>
2020-12-01 15:24:32 -0500

    Add constants for cli

--- changehc/delphi_changehc/constants.py
+++ changehc/delphi_changehc/constants.py
@@ -1,7 +1,9 @@
 """Registry for signal names and geo types"""
 SMOOTHED = "smoothed_covid"
 SMOOTHED_ADJ = "smoothed_adj_covid"
-SIGNALS = [SMOOTHED, SMOOTHED_ADJ]
+SMOOTHED_CLI = "smoothed_cli"
+SMOOTHED_ADJ_CLI = "smoothed_adj_cli"
+SIGNALS = [SMOOTHED, SMOOTHED_ADJ, SMOOTHED_CLI, SMOOTHED_ADJ_CLI]
 NA = "NA"
 HRR = "hrr"
 FIPS = "fips"

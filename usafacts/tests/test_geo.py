23c83a1879063d1bac70004b6e5f042e3a23695e
andrew <chinandrew96@gmail.com>
2020-12-09 15:23:47 -0800

    Finish USAFacts geo refactor

--- usafacts/tests/test_geo.py
+++ usafacts/tests/test_geo.py
@@ -108,19 +108,16 @@ class TestGeoMap:
                 "population": [100, 2100, 300, 25],
             }
         )
-
         new_df = geo_map(df, "hrr", MAP_DF, SENSOR)
-
-        exp_incidence = np.array([13, 27]) / np.array([25, 2500]) * 100000
-        exp_cprop = np.array([60, 165]) / np.array([25, 2500]) * 100000
-
-        assert (new_df["geo_id"].values == [110, 147]).all()
-        assert (new_df["timestamp"].values == ["2020-02-15", "2020-02-15"]).all()
-        assert new_df["new_counts"].values == pytest.approx([13.0, 27.0])
-        assert new_df["cumulative_counts"].values == pytest.approx([60, 165])
-        assert new_df["population"].values == pytest.approx([25, 2500])
-        assert new_df["incidence"].values == pytest.approx(exp_incidence)
-        assert new_df["cumulative_prop"].values == pytest.approx(exp_cprop)
+        assert new_df.round(5).equals(pd.DataFrame({
+            "geo_id": ["110", "123", "140", "145", "147"],
+            "timestamp": ["2020-02-15"]*5,
+            "new_counts": [13.0, 0.11143, 0.09867, 0.00809, 26.78180],
+            "cumulative_counts": [60.0, 0.14858, 0.13156, 0.08093, 164.63893],
+            "population": [25.0, 15.60054, 13.81422, 0.08093, 2470.50431],
+            "incidence": [52000.0, 714.28571, 714.28571, 10000.0, 1084.06214],
+            "cumulative_prop": [240000.0, 952.38095, 952.38095, 100000.0, 6664.18316]
+        }))
 
     def test_msa(self):
         """Tests that values are correctly aggregated at the MSA level."""
@@ -133,16 +130,13 @@ class TestGeoMap:
                 "population": [100, 2100, 300, 25],
             }
         )
-
         new_df = geo_map(df, "msa", MAP_DF, SENSOR)
-
-        exp_incidence = np.array([2, 13]) / np.array([300, 25]) * 100000
-        exp_cprop = np.array([45, 60]) / np.array([300, 25]) * 100000
-
-        assert (new_df["geo_id"].values == [31420, 49340]).all()
-        assert (new_df["timestamp"].values == ["2020-02-15", "2020-02-15"]).all()
-        assert new_df["new_counts"].values == pytest.approx([2.0, 13.0])
-        assert new_df["cumulative_counts"].values == pytest.approx([45, 60])
-        assert new_df["population"].values == pytest.approx([300, 25])
-        assert new_df["incidence"].values == pytest.approx(exp_incidence)
-        assert new_df["cumulative_prop"].values == pytest.approx(exp_cprop)
+        assert new_df.round(5).equals(pd.DataFrame({
+            "geo_id": ["31420", "49340"],
+            "timestamp": ["2020-02-15"]*2,
+            "new_counts": [2.0, 13.0],
+            "cumulative_counts": [45.0, 60.0],
+            "population": [300, 25],
+            "incidence": [666.66667, 52000.0],
+            "cumulative_prop": [15000.0, 240000.0]
+        }))

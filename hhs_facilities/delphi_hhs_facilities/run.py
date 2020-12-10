759c81ed944cb0bbd2d87dad928993583239985e
andrew <chinandrew96@gmail.com>
2020-12-10 14:27:49 -0800

    Add skeleton for indicator

---
+++ hhs_facilities/delphi_hhs_facilities/run.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""Functions to call when running the function.
+
+This module should contain a function called `run_module`, that is executed
+when the module is run with `python -m MODULE_NAME`.
+"""
+
+# from datetime import date, datetime, timedelta
+
+# from delphi_epidata import Epidata
+# from delphi_utils import read_params
+# from delphi_utils.export import create_export_csv
+# from delphi_utils.geomap import GeoMapper
+# import pandas as pd
+
+# from .constants import SIGNALS, GEO_RESOLUTIONS
+
+
+def run_module():
+    """
+    Skeleton to run hhs_facilities indicator.
+
+    Returns
+    -------
+    prints the updated signal names
+    """
+    # params = read_params()
+    # pull data from API
+    # for geo, signals in product(SIGNALS, GEO_RESOLUTIONS):
+        # map to desired geo
+        # do any extra processing that is required
+        # export

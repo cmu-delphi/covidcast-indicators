c5368819e8ac4bda0733d03dbf8b5ffa0c7d894e
andrew <chinandrew96@gmail.com>
2020-12-11 15:29:36 -0800

    add geo tests

--- hhs_facilities/tests/test_generate_signals.py
+++ hhs_facilities/tests/test_generate_signals.py
@@ -1,4 +1,5 @@
-"""Tests for running the HHS Facilities indicator."""
+"""Tests for running the signal generation functions."""
+
 import pandas as pd
 import numpy as np
 
@@ -6,28 +7,28 @@ from delphi_hhs_facilities.generate_signals import generate_signal, sum_cols
 
 
 class TestGenerateSignals:
-    """Tests for the `generate_signals` module."""
 
     def test_generate_signals(self):
-        """Placeholder."""
-        test_input = pd.DataFrame({"a": [1, 2, 3, 4, np.nan],
-                                   "b": [2, np.nan, 4, 6, np.nan],
-                                   "geo_id": ["x", "x", "x", "y", "z"],
-                                   "timestamp": [pd.Timestamp("20200201"),
-                                                 pd.Timestamp("20200201"),
-                                                 pd.Timestamp("20200202"),
-                                                 pd.Timestamp("20200203"),
-                                                 pd.Timestamp("20200204")]
-                                   })
+        test_input = pd.DataFrame(
+            {"a": [1, 2, 3, 4, np.nan],
+             "b": [2, np.nan, 4, 6, np.nan],
+             "geo_id": ["x", "x", "x", "y", "z"],
+             "timestamp": [pd.Timestamp("20200201"),
+                           pd.Timestamp("20200201"),
+                           pd.Timestamp("20200202"),
+                           pd.Timestamp("20200203"),
+                           pd.Timestamp("20200204")]
+             })
         test_output = generate_signal(test_input, ["a", "b"], sum_cols, -1)
-        expected = pd.DataFrame({"timestamp": [pd.Timestamp("20200131"),
-                                               pd.Timestamp("20200201"),
-                                               pd.Timestamp("20200202")],
-                                 "geo_id": ["x", "x", "y"],
-                                 "val": [5., 7., 10.],
-                                 "se": [np.nan]*3,
-                                 "sample_size": [np.nan]*3
-                                 })
+        expected = pd.DataFrame(
+            {"timestamp": [pd.Timestamp("20200131"),
+                           pd.Timestamp("20200201"),
+                           pd.Timestamp("20200202")],
+             "geo_id": ["x", "x", "y"],
+             "val": [5., 7., 10.],
+             "se": [np.nan]*3,
+             "sample_size": [np.nan]*3
+             })
         pd.testing.assert_frame_equal(test_output, expected)
 
     def test_sum_cols(self):

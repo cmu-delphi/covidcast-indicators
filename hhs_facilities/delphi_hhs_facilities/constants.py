c4156c62dcb495463cf65b6ae749cfd9eb0ab484
andrew <chinandrew96@gmail.com>
2020-12-11 11:01:35 -0800

    Add initial indicator
    fix constants

--- hhs_facilities/delphi_hhs_facilities/constants.py
+++ hhs_facilities/delphi_hhs_facilities/constants.py
@@ -1,6 +1,19 @@
 """Registry for geographies to process."""
 
+ADMISSION_TOTAL = "total_admission"
 
-GEO_RESOLUTIONS = {
+SIGNALS = [
+    # (name, columns to use, operation, date offset)
+    (ADMISSION_TOTAL,
+     ["previous_day_admission_adult_covid_confirmed_7_day_sum",
+      "previous_day_admission_pediatric_covid_confirmed_7_day_sum"],
+     sum,
+     -1),
+]
 
-}
+GEO_RESOLUTIONS = [
+    "county",
+    "msa",
+    "state",
+    "hrr"
+]
--- hhs_facilities/delphi_hhs_facilities/run.py
+++ hhs_facilities/delphi_hhs_facilities/run.py
@@ -6,6 +6,9 @@ when the module is run with `python -m delphi_hhs_facilities    `.
 """
 
 # from datetime import date, datetime, timedelta
+from itertools import product
+from typing import Callable
+
 
 # from delphi_epidata import Epidata
 from delphi_utils import read_params
@@ -14,39 +17,46 @@ from delphi_utils.geomap import GeoMapper
 import numpy as np
 import pandas as pd
 
-from .constants import GEO_RESOLUTIONS
+from .constants import GEO_RESOLUTIONS, SIGNALS
 
 
 def run_module():
     """Run entire hhs_facilities indicator."""
     params = read_params()
     # raw_df = query_api()  pull data from API
-    # below 3 lines are all just for local testing while API is not online yet.
-    raw_df = pd.read_csv("delphi_hhs_facilities/sample_data.csv", dtype={"fips_code": "str"})
+    # below 5 lines are all just for local testing while API is not online yet.
+    raw_df = pd.read_csv("delphi_hhs_facilities/sample_data.csv",
+                         dtype={"fips_code": "str"},
+                         nrows=10000)
     raw_df["timestamp"] = pd.to_datetime(raw_df["collection_week"])
     raw_df["fips"] = raw_df["fips_code"]
 
     gmpr = GeoMapper()
-    for geo, signal in GEO_RESOLUTIONS:
+    for geo, (sig_name, sig_cols, sig_func, sig_offset) in product(GEO_RESOLUTIONS, SIGNALS):
         mapped_df = convert_geo(raw_df, geo, gmpr)
-        output_df = process_df(mapped_df)
-        create_export_csv(output_df, params["export_dir"], geo, "testsignal")
+        output_df = process_df(mapped_df, sig_cols, sig_func, sig_offset)
+        create_export_csv(output_df, params["export_dir"], geo, sig_name)
 
 
 def convert_geo(df: pd.DataFrame, geo: str, gmpr: GeoMapper) -> pd.DataFrame:
     """Map a df to desired regions."""
     if geo == "county":
         output_df = df.copy()
-        output_df["geo_id"] = output_df["county"]
+        output_df["geo_id"] = output_df["fips"]
     else:
         output_df = gmpr.add_geocode(df, "fips", geo)
         output_df["geo_id"] = output_df[geo]
     return output_df
 
 
-def process_df(df: pd.DataFrame) -> pd.DataFrame:
+def process_df(df: pd.DataFrame,
+               input_cols: list,
+               signal_func: Callable,
+               date_offset: int) -> pd.DataFrame:
     """Process a df to get the value of interest."""
+    df_cols = [df[i] for i in input_cols]
+    df["val"] = signal_func(df_cols)
     df["se"] = np.nan
     df["sample_size"] = np.nan
-    df["val"] = 1  # Do real processing here
+    df["timestamp"] = df["timestamp"] + pd.Timedelta(days=date_offset)
     return df

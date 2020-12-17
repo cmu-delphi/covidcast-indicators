d0080a62a61c3624037ce6bf7edb6d408625988d
andrew <chinandrew96@gmail.com>
2020-12-17 15:19:42 -0800

    Update to loop through calls

--- hhs_facilities/delphi_hhs_facilities/pull.py
+++ hhs_facilities/delphi_hhs_facilities/pull.py
@@ -18,15 +18,21 @@ def pull_data() -> pd.DataFrame:
     -------
     DataFrame of HHS data.
     """
+    today = int(date.today().strftime("%Y%m%d"))
+    past_reference_day = int(date(2020, 1, 1).strftime("%Y%m%d"))  # first available date in DB
     mapper = GeoMapper()
-    request_all_states = ",".join(mapper.get_geo_values("state_id"))
-    today = date.today()
-    past_reference_day = date(year=2020, month=1, day=1)  # first available date in DB
-    date_range = [int(x.strftime("%Y%m%d")) for x in [past_reference_day, today]]
-    response = Epidata.covid_hosp_facility(request_all_states, date_range)
-    if response["result"] != 1:
-        raise Exception(f"Bad result from Epidata: {response['message']}")
-    all_columns = pd.DataFrame(response["epidata"])
+    all_states = mapper.get_geo_values("state_id")
+    responses = []
+    for state in all_states:
+        state_hospital_ids = Epidata.covid_hosp_facility_lookup(state)
+        for i in range(0, len(state_hospital_ids), 10):
+            response = Epidata.covid_hosp_facility(state_hospital_ids[i:i+10],
+                                                   past_reference_day,
+                                                   today)
+            if response["result"] != 1:
+                raise Exception(f"Bad result from Epidata: {response['message']}")
+            responses += response["epidata"]
+    all_columns = pd.DataFrame(responses)
     all_columns.replace(NAN_VALUES, np.nan, inplace=True)
     all_columns["timestamp"] = pd.to_datetime(all_columns["collection_week"])
     return all_columns

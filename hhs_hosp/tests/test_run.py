d83fdd3bf1f2681f71e9ea8dfb7bd56391c1b47f
andrew <chinandrew96@gmail.com>
2020-12-18 12:23:38 -0800

    Add date intervals and loop

--- hhs_hosp/tests/test_run.py
+++ hhs_hosp/tests/test_run.py
@@ -1,8 +1,12 @@
-from datetime import datetime
+from datetime import datetime, date
 
-import delphi_hhs
-from delphi_hhs.run import int_date_to_previous_day_datetime
+from delphi_hhs.run import _date_to_int, int_date_to_previous_day_datetime, generate_date_ranges
 import pandas as pd
+
+
+def test__date_to_int():
+    """Check that dates are converted to the right int."""
+    assert _date_to_int(date(2020, 5, 1)) == 20200501
 
 def test_date_conversion():
     """Check that we convert dates properly between Epidata and datetime format."""
@@ -15,3 +19,20 @@ def test_date_conversion():
     for got, expected in zip(result, expected_result):
         assert isinstance(got, datetime), f"Bad type: {type(got)}\n{result}"
         assert got == expected
+
+
+def test_generate_date_ranges():
+    """Check ranges generated partition the specified inputs."""
+    assert generate_date_ranges(date(2020, 1, 1), date(2020, 1, 31)) == \
+           [{'from': 20200101, 'to': 20200131}]
+    assert generate_date_ranges(date(2020, 1, 1), date(2020, 2, 1)) == \
+           [{'from': 20200101, 'to': 20200131},
+            {'from': 20200201, 'to': 20200201}]
+    assert generate_date_ranges(date(2020, 1, 1), date(2020, 5, 12)) == \
+           [{'from': 20200101, 'to': 20200131},
+            {'from': 20200201, 'to': 20200302},
+            {'from': 20200303, 'to': 20200402},
+            {'from': 20200403, 'to': 20200503},
+            {'from': 20200504, 'to': 20200512}]
+
+

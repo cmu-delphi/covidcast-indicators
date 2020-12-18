6cd1fa7e6e699b2fa706254ace3c6bcc5fe5f8fd
andrew <chinandrew96@gmail.com>
2020-12-18 15:32:51 -0800

    Get epidata pull working

--- hhs_facilities/delphi_hhs_facilities/pull.py
+++ hhs_facilities/delphi_hhs_facilities/pull.py
@@ -1,11 +1,11 @@
 """Functions for mapping geographic regions."""
 
-from datetime import date, datetime, timedelta
+from datetime import date
 
 import pandas as pd
 import numpy as np
-from delphi_epidata import Epidata
 from delphi_utils.geomap import GeoMapper
+from delphi_epidata import Epidata
 
 from .constants import NAN_VALUES
 
@@ -24,15 +24,17 @@ def pull_data() -> pd.DataFrame:
     all_states = mapper.get_geo_values("state_id")
     responses = []
     for state in all_states:
-        state_hospital_ids = Epidata.covid_hosp_facility_lookup(state)
+        lookup_response = Epidata.covid_hosp_facility_lookup(state)
+        if lookup_response["result"] != 1:
+            raise Exception(f"Bad result from Epidata: {lookup_response['message']}")
+        state_hospital_ids = [i["hospital_pk"] for i in lookup_response["epidata"]]
         for i in range(0, len(state_hospital_ids), 10):
             response = Epidata.covid_hosp_facility(state_hospital_ids[i:i+10],
-                                                   past_reference_day,
-                                                   today)
+                                                   Epidata.range(past_reference_day, today))
             if response["result"] != 1:
                 raise Exception(f"Bad result from Epidata: {response['message']}")
             responses += response["epidata"]
     all_columns = pd.DataFrame(responses)
     all_columns.replace(NAN_VALUES, np.nan, inplace=True)
-    all_columns["timestamp"] = pd.to_datetime(all_columns["collection_week"])
+    all_columns["timestamp"] = pd.to_datetime(all_columns["collection_week"], format="%Y%m%d")
     return all_columns
--- hhs_facilities/tests/test_pull.py
+++ hhs_facilities/tests/test_pull.py
@@ -2,19 +2,64 @@
 
 from unittest.mock import patch
 
+import pytest
 import pandas as pd
 import numpy as np
 
-from delphi_utils.geomap import GeoMapper
 from delphi_hhs_facilities.pull import pull_data
 
 
 class TestPull:
 
     @patch("delphi_hhs_facilities.pull.Epidata.covid_hosp_facility")
-    def test_pull(self, covid_hosp_facility):
-        # test bad response
-        # test values correct
-        # test data types correct (fips, zip, and timestamp)
-        # test nans parsed
-        assert True
+    @patch("delphi_hhs_facilities.pull.Epidata.covid_hosp_facility_lookup")
+    def test_pull(self, covid_hosp_facility_lookup, covid_hosp_facility):
+        covid_hosp_facility_lookup.return_value = {
+            "result": 1,
+            "epidata": [{"hospital_pk": "020001"}, {"hospital_pk": "020006"}],
+            "message": "success"
+        }
+        covid_hosp_facility.return_value = {
+            "result": 1,
+            "epidata": [{"collection_week": 20201204,
+                         "total_beds_7_day_sum": 2360,
+                         "all_adult_hospital_beds_7_day_sum": -999999,
+                         "inpatient_beds_7_day_avg": -999999.0,
+                         "total_icu_beds_7_day_avg": np.nan,
+                         "total_staffed_adult_icu_beds_7_day_avg": 32.4},
+                        {"collection_week": 20201204,
+                         "total_beds_7_day_sum": 1000,
+                         "all_adult_hospital_beds_7_day_sum": 1917,
+                         "inpatient_beds_7_day_avg": 330.6,
+                         "total_icu_beds_7_day_avg": 76.7,
+                         "total_staffed_adult_icu_beds_7_day_avg": 12.1}],
+            "message": "success"}
+        output = pull_data()
+        assert output.shape == (120, 7)  # 2 mock rows * 60 states, 6 mock + 1 new timestamp column
+        # verify nans cast properly
+        assert np.isnan(output["all_adult_hospital_beds_7_day_sum"][0])
+        assert np.isnan(output["inpatient_beds_7_day_avg"][0])
+        assert np.isnan(output["total_icu_beds_7_day_avg"][0])
+        pd.testing.assert_series_equal(output.timestamp, 
+                                       pd.Series([pd.Timestamp("2020-12-04")]*120),
+                                       check_names=False)
+        pd.testing.assert_series_equal(output.loc[0],
+                                       pd.Series({"collection_week": 20201204,
+                                                  "total_beds_7_day_sum": 2360,
+                                                  "all_adult_hospital_beds_7_day_sum": np.nan,
+                                                  "inpatient_beds_7_day_avg": np.nan,
+                                                  "total_icu_beds_7_day_avg": np.nan,
+                                                  "total_staffed_adult_icu_beds_7_day_avg": 32.4,
+                                                  "timestamp": pd.Timestamp("2020-12-04")}), 
+                                       check_names=False)
+
+        # test failure cases
+        covid_hosp_facility.return_value = {"result": 2, "message": "data fail"}
+        with pytest.raises(Exception) as exc:
+            pull_data()
+            assert "data fail" in exc
+
+        covid_hosp_facility_lookup.return_value = {"result": 2, "message": "lookup fail"}
+        with pytest.raises(Exception) as exc:
+            pull_data()
+            assert "lookup fail" in exc

afe4e4ee02a6568351042745954ef1284ab35e8e
Ben Smith <benjamin.y.smith@gmail.com>
2020-12-23 17:23:01 -0500

    Extact slack notifier

---
+++ _delphi_utils_python/delphi_utils/slack_notifier.py
@@ -0,0 +1,20 @@
+"""Slack notifier for use in Delphi indicators """
+from slack import WebClient
+from slack.errors import SlackApiError
+
+class SlackNotifier:
+    """Notifies slack channels of messages"""
+    def __init__(self, slack_channel, slack_token):
+        self.slack_channel = slack_channel
+        self.client = WebClient(token = slack_token)
+
+    def post_message(self, blocks):
+        """Post a message to the slack channel"""
+        try:
+            self.client.chat_postMessage(
+                channel=self.slack_channel,
+                blocks=blocks
+            )
+        except SlackApiError as e:
+            # You will get a SlackApiError if "ok" is False
+            assert False, e.response["error"]

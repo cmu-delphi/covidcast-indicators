523656cef2e5301749139c53290299c5099479b9
Ben Smith <benjamin.y.smith@gmail.com>
2021-01-05 11:01:47 -0500

    Update docstrings for pylint

--- _delphi_utils_python/delphi_utils/slack_notifier.py
+++ _delphi_utils_python/delphi_utils/slack_notifier.py
@@ -1,14 +1,16 @@
-"""Slack notifier for use in Delphi indicators """
+"""Slack notifier for use in Delphi indicators."""
 from slack import WebClient
 
 class SlackNotifier:
-    """Notifies slack channels of messages"""
+    """Notifies slack channels of messages."""
+
     def __init__(self, slack_channel, slack_token):
+        """Initializes with a slack channel and token."""
         self.slack_channel = slack_channel
         self.client = WebClient(token = slack_token)
 
     def post_message(self, blocks):
-        """Post a message to the slack channel"""
+        """Post a message to the slack channel."""
         self.client.chat_postMessage(
             channel=self.slack_channel,
             blocks=blocks

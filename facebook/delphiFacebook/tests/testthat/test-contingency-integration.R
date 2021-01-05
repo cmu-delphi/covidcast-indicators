a4c70b9fc75dfaaae7e3b729b5d1954034e34901
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2021-01-05 17:01:43 -0500

    setup for end-to-end tests

--- facebook/delphiFacebook/tests/testthat/test-contingency-integration.R
+++ facebook/delphiFacebook/tests/testthat/test-contingency-integration.R
@@ -1,9 +1,8 @@
-### As with `test-contingency-gold.R`, this test file relies on `setup-run.R` 
-### to run the pipeline. This test checks the configuration listed in 
-### `params-contingency-test.json`, which loads `input/responses.csv`, a small 
-### selected subset of test responses.
+### This test file relies on `setup-run.R` to run the pipeline. This test checks
+### the configuration listed in `params-contingency-test.json`, which loads
+### `input/responses.csv`, a small selected subset of test responses.
 
-context("Testing the run_contingency_tables function on a small dataset")
+context("Testing the run_contingency_tables function on test data")
 
 test_that("testing lack of existence of csv files", {
   # Since the contingency tables tool removes aggregates that pose a privacy

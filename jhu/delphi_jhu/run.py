97491b588f7bf3413d6875c9cb953a6bd8cc2003
Ben Smith <benjamin.y.smith@gmail.com>
2021-01-11 15:26:35 -0500

    Add better logging to JHU

--- jhu/delphi_jhu/run.py
+++ jhu/delphi_jhu/run.py
@@ -4,8 +4,10 @@
 This module should contain a function called `run_module`, that is executed
 when the module is run with `python -m MODULE_NAME`.
 """
-from datetime import datetime
+from datetime import datetime as dt
 from itertools import product
+import datetime
+import time
 
 import numpy as np
 from delphi_utils import (
@@ -64,6 +66,9 @@ GEO_RESOLUTIONS = [
 
 def run_module():
     """Run the JHU indicator module."""
+    start_time = time.time()
+    csv_export_count = 0
+    oldest_final_export_date = None
     params = read_params()
     export_start_date = params["export_start_date"]
     export_dir = params["export_dir"]
@@ -110,14 +115,20 @@ def run_module():
         #     metric = f"wip_{metric}"
         #     sensor_name = WIP_SENSOR_NAME_MAP[sensor][0]
         sensor_name = SMOOTHERS_MAP[smoother][1] + sensor_name
-        create_export_csv(
+        exported_csv_dates = create_export_csv(
             df,
             export_dir=export_dir,
-            start_date=datetime.strptime(export_start_date, "%Y-%m-%d"),
+            start_date=dt.strptime(export_start_date, "%Y-%m-%d"),
             metric=metric,
             geo_res=geo_res,
             sensor=sensor_name,
         )
+        if not exported_csv_dates.empty:
+            csv_export_count += exported_csv_dates.size
+            if not oldest_final_export_date:
+                oldest_final_export_date = max(exported_csv_dates)
+            oldest_final_export_date = min(
+                oldest_final_export_date, max(exported_csv_dates))
 
     if not arch_diff is None:
         # Diff exports, and make incremental versions
@@ -137,3 +148,13 @@ def run_module():
         # Report failures: someone should probably look at them
         for exported_file in fails:
             print(f"Failed to archive '{exported_file}'")
+
+    elapsed_time_in_seconds = round(time.time() - start_time, 2)
+    max_lag_in_days = None
+    if oldest_final_export_date:
+        max_lag_in_days = (datetime.datetime.now() - oldest_final_export_date).days
+    logger.info("Completed indicator run",
+        elapsed_time_in_seconds = elapsed_time_in_seconds,
+        csv_export_count = csv_export_count,
+        max_lag_in_days = max_lag_in_days,
+        oldest_final_export_date = oldest_final_export_date.strftime("%Y-%m-%d"))

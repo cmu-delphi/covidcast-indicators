babd1e8612f6af2591989ac41b9d422940c7dee9
rumackaaron <adr66@cornell.edu>
2021-01-12 10:23:45 -0500

    Revert column names to old style

--- doctor_visits/delphi_doctor_visits/config.py
+++ doctor_visits/delphi_doctor_visits/config.py
@@ -21,17 +21,17 @@ class Config:
     CLI_COLS = ["Covid_like", "Flu_like", "Mixed"]
     FLU1_COL = ["Flu1"]
     COUNT_COLS = CLI_COLS + FLU1_COL + ["Denominator"]
-    DATE_COL = "servicedate"
-    GEO_COL = "patCountyFIPS"
-    AGE_COL = "patAgeGroup"
-    HRR_COLS = ["patHRRname", "patHRRid"]
+    DATE_COL = "ServiceDate" #"servicedate"
+    GEO_COL = "PatCountyFIPS" #"patCountyFIPS"
+    AGE_COL = "PatAgeGroup" #"patAgeGroup"
+    HRR_COLS = ["Pat HRR Name", "Pat HRR ID"]#["patHRRname", "patHRRid"]
     ID_COLS = [DATE_COL] + [GEO_COL] + [AGE_COL] + HRR_COLS
     FILT_COLS = ID_COLS + COUNT_COLS
-    DTYPES = {"servicedate": str, "patCountyFIPS": str,
+    DTYPES = {DATE_COL: str, GEO_COL: str,
               "Denominator": int, "Flu1": int,
               "Covid_like": int, "Flu_like": int,
-              "Mixed": int, "patAgeGroup": str,
-              "patHRRname": str, "patHRRid": float}
+              "Mixed": int, AGE_COL: str,
+              HRR_COLS[0]: str, HRR_COLS[1]: float}
 
     SMOOTHER_BANDWIDTH = 100  # bandwidth for the linear left Gaussian filter
     MAX_BACKFILL_WINDOW = 7  # maximum number of days used to average a backfill correction

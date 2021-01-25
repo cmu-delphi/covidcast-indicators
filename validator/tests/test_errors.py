956f9c6b9922fb4e2108550de235e1560201f402
Mike O'Brien <mikeob@google.com>
2021-01-25 10:38:37 -0500

    documentation and organization

--- validator/tests/test_errors.py
+++ validator/tests/test_errors.py
@@ -7,6 +7,8 @@ class TestValidationFailure:
     """Tests for ValidationFailure class."""
 
     def test_init(self):
+        """Test initialization of ValidationFailures."""
+        # Normal positional arguments.
         vf1 = ValidationFailure("chk", "1990-10-03", "state", "deaths", "msg")
         assert vf1.check_name == "chk"
         assert vf1.date == dt.date(1990, 10, 3)
@@ -14,13 +16,22 @@ class TestValidationFailure:
         assert vf1.signal == "deaths"
         assert vf1.message == "msg"
 
-        vf2 = ValidationFailure("chk", filename="20200311_county_cases_7dav.csv",
-                                date=dt.date(400, 4, 4), geo_type="ignored", signal="also_ignored")
+        # Missing arguments filled in.
+        vf2 = ValidationFailure("chk", dt.date(1990, 10, 3))
         assert vf2.check_name == "chk"
-        assert vf2.date == dt.date(2020, 3, 11)
-        assert vf2.geo_type == "county"
-        assert vf2.signal == "cases_7dav"
+        assert vf2.date == dt.date(1990, 10, 3)
+        assert vf2.geo_type == None
+        assert vf2.signal == None
         assert vf2.message == ""
+
+        # Values extracted from a filename.
+        vf3 = ValidationFailure("chk", filename="20200311_county_cases_7dav.csv",
+                                date=dt.date(400, 4, 4), geo_type="ignored", signal="also_ignored")
+        assert vf3.check_name == "chk"
+        assert vf3.date == dt.date(2020, 3, 11)
+        assert vf3.geo_type == "county"
+        assert vf3.signal == "cases_7dav"
+        assert vf3.message == ""
 
         with pytest.raises(AssertionError):
             # file name not formatted correctly
@@ -31,6 +42,7 @@ class TestValidationFailure:
             ValidationFailure("chk", filename="2020-03-11_county_cases_7dav.csv")
 
     def test_eq(self):
+        """Test equality checking of ValidationFailures."""
         vf = ValidationFailure("a", dt.date(2021, 1, 20), "b", "c", "d")
         assert vf == ValidationFailure("a", dt.date(2021, 1, 20), "b", "c", "")
         assert vf == ValidationFailure(None, dt.date(2021, 1, 20), "b", "c", "")
@@ -41,8 +53,6 @@ class TestValidationFailure:
         assert vf == ValidationFailure("a", dt.date(2021, 1, 20), None, None, "")
         assert vf == ValidationFailure(None, None, None, None, "")
 
-    def test_not_eq(self):
-        vf = ValidationFailure("a", dt.date(2021, 1, 20), "b", "c", "d")
         assert vf != ValidationFailure("x", dt.date(2021, 1, 20), "b", "c", "")
         assert vf != ValidationFailure(None, dt.date(2021, 1, 21), "b", "c", "")
         assert vf != ValidationFailure("a", None, "x", "c", "")
@@ -58,9 +68,6 @@ class TestValidationFailure:
                                  ValidationFailure(None, dt.date(2021, 1, 20), "b", "c", ""),
                                  ValidationFailure("a", None, "b", "c", "")])
 
-    def test_not_is_suppressed(self):
-        """Tests the non-suppression of failures."""
-        vf = ValidationFailure("a", dt.date(2021, 1, 20), "b", "c", "d")
         assert not vf.is_suppressed([ValidationFailure("x", dt.date(2021, 1, 20), "b", "c", ""),
                                      ValidationFailure(None, dt.date(2021, 1, 21), "b", "c", ""),
                                      ValidationFailure("a", None, "x", "c", ""),

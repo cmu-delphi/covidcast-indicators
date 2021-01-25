b0351a3c39580561f8f08f6fbf6f0077b5722033
Mike O'Brien <mikeob@google.com>
2021-01-24 23:37:42 -0500

    updated documentation of ValidationFailure functions

--- validator/delphi_validator/errors.py
+++ validator/delphi_validator/errors.py
@@ -4,7 +4,7 @@ Custom validator exceptions.
 """
 # from dataclasses import dataclass
 import datetime as dt
-from typing import Optional
+from typing import Union, Optional
 
 class APIDataFetchError(Exception):
     """Exception raised when reading API data goes wrong.
@@ -24,22 +24,48 @@ class ValidationFailure:
     """Structured report of single validation failure."""
     def __init__(self,
                  check_name: str,
-                 date: Optional[dt.date]=None,
+                 date: Optional[Union[str, dt.date]]=None,
                  geo_type: Optional[str]=None,
                  signal: Optional[str]=None,
                  message: str="",
                  filename: Optional[str]=None):
+        """Initialize a ValidationFailure.
+        Parameters
+        ----------
+        check_name: str
+            Name of check at which the failure happened.
+        date: Optional[Union[str, dt.date]]
+            Date corresponding to the data over which the failure happened.
+            Strings are interpretted in ISO format ("YYYY-MM-DD").
+            When `None`, the failure is not tied to any date.
+        geo_type: Optional[str]
+            Geo resolution of the data at which the failure occurred.
+            When `None`, the failure is not tied to any geo resolution.
+        signal: Optional[str]
+            COVIDcast signal for which the failure occurred.
+            When `None`, the failure is not tied to any signal.
+        message: str
+            Additional context about the failure
+        filename: Optional[str]
+            Filename from which the failing data came.
+            When provided, any values passed to the `date`, `geo_type`, and `signal` parameters are
+            ignored and instead derived from this parameter, under the assumption that the filename
+            is formatted as "{date}_{geo_type}_{signal}.{extension}", where `date` is in the format
+            "YYYYMMDD".
+
+        Attributes
+        ----------
+        check_name: str
+            See above.
+        date: Optional[dt.date]
+            See above.
+        geo_type: Optional[str]
+            See above.
+        signal: Optional[str]
+            See above.
+        message: str
+            See above.
         """
-            # Which check the failure came from
-    check_name: str
-    # Date of the failure
-    date: Optional[dt.date]
-    # Geo resolution of the failure
-    geo_type: Optional[str]
-    # Signal name of the failure
-    signal: Optional[str]
-    # Additional context about the failure
-    message: str."""
         self.check_name = check_name
         self.message = message
         if filename:
@@ -55,6 +81,12 @@ class ValidationFailure:
         self.signal = signal
 
     def __eq__(self, other):
+        """Compare this object with ValidationFailure `other` for equality.
+        
+        Two ValidationFailures are considered equal if their `check_name`, `date`, `geo_type`, and
+        `signal` attributes correspondingly match.  A value of `None` in any of these attributes is
+        considered to match any corresponding value on the other ValidationFailure.
+        """
         def match_with_wildcard(x, y):
             """Determine if x and y are equal or None."""
             return x is None or y is None or x == y

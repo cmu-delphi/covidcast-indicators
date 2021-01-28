c26bfe811e1a582fc82ec702896272e3b0dff2a3
andrew <chinandrew96@gmail.com>
2021-01-28 10:18:28 -0800

    Add data containers and tests

---
+++ nowcast/delphi_nowcast/data_containers.py
@@ -0,0 +1,95 @@
+"""Data container classes for holding sensor configurations and data needed for fusion."""
+
+from dataclasses import dataclass, field
+from typing import List, Union
+
+from numpy import ndarray, nan, nanmean, isnan
+from pandas import date_range
+
+
+@dataclass(frozen=True)
+class SensorConfig:
+    """Dataclass for specifying a sensor's name, number of lag days, and origin source/signal."""
+
+    source: str
+    signal: str
+    name: str
+    lag: int
+
+
+@dataclass
+class LocationSeries:
+    """Data class for holding time series data specific to a single location."""
+
+    geo_value: str = None
+    geo_type: str = None
+    dates: List[int] = field(default_factory=lambda: [])
+    values: Union[List, ndarray] = field(default_factory=lambda: [])
+
+    def __post_init__(self):
+        """Input validation."""
+        if (self.dates is None and self.values is not None) or \
+                (self.dates is not None and self.values is None):
+            raise ValueError("Must init with both date and values.")
+
+        if (self.dates is not None and self.values is not None) and \
+                (len(self.dates) != len(self.values)):
+            raise ValueError("Length of dates and values differs.")
+
+        if len(set(self.dates)) < len(self.dates):
+            raise ValueError("Duplicate dates not allowed.")
+
+    def add_data(self, date, value):
+        """Append a date and value to existing attributes.
+
+        Safer than appending individually since the two lists shouldn't have different lengths.
+        """
+        self.dates.append(date)
+        self.values.append(value)
+
+    @property
+    def empty(self):
+        """Check if there is no stored data in the class."""
+        return not self.dates and not self.values
+
+    def get_value(self, date: int) -> float:
+        """Return value for a given date or nan if not available."""
+        try:
+            return self.values[self.dates.index(date)]
+        except ValueError:
+            return nan
+
+    def get_data_range(self,
+                       start_date: int,
+                       end_date: int,
+                       imputation_method: str = None) -> List[float]:
+        """
+        Return value of LocationSeries between two dates with optional imputation.
+
+        Parameters
+        ----------
+        start_date
+            First day to include in range.
+        end_date
+            Last day to include in range.
+        imputation_method
+            Optional type of imputation to conduct. Currently only "mean" is supported.
+
+        Returns
+        -------
+            List of values, one for each day in the range.
+        """
+        if start_date < min(self.dates) or end_date > max(self.dates):
+            raise ValueError(f"Data range must be within existing dates "
+                             f"{min(self.dates)}-{max(self.dates)}.")
+        all_dates = [int(i.strftime("%Y%m%d")) for i in date_range(str(start_date), str(end_date))]
+        out_values = []
+        for day in all_dates:
+            out_values.append(self.get_value(day))
+        if imputation_method is None or not out_values:
+            return out_values
+        if imputation_method == "mean":
+            mean = nanmean(out_values)
+            out_values = [i if not isnan(i) else mean for i in out_values]
+            return out_values
+        raise ValueError("Invalid imputation method. Must be None or 'mean'")
---
+++ nowcast/tests/test_data_containers.py
@@ -0,0 +1,59 @@
+import pytest
+import numpy as np
+
+from delphi_nowcast.data_containers import LocationSeries
+
+
+class TestLocationSeries:
+
+    def test___post_init__(self):
+        with pytest.raises(ValueError) as exc:
+            LocationSeries(dates=[1])
+            assert str(exc.value) == "Must init with both date and values."
+        with pytest.raises(ValueError) as exc:
+            LocationSeries(dates=[1], values=[1,1])
+            assert str(exc.value) == "Length of dates and values differs."
+        with pytest.raises(ValueError) as exc:
+            LocationSeries(dates=[1, 1], values=[1, 2])
+            assert str(exc.value) == "Duplicate dates not allowed."
+
+    def test_empty(self):
+        test_ls = LocationSeries(dates=[], values=[])
+        assert test_ls.empty
+        test_ls = LocationSeries(dates=[1], values=[2])
+        assert not test_ls.empty
+
+    def test_add_data(self):
+        test_ls = LocationSeries(dates=[1], values=[2])
+        test_ls.add_data(3, 4)
+        assert test_ls == LocationSeries(dates=[1, 3], values=[2, 4])
+
+    def test_get_value(self):
+        test_ls = LocationSeries(dates=[1], values=[2])
+        assert test_ls.get_value(1) == 2
+        assert np.isnan(test_ls.get_value(-1))
+
+    def test_get_data_range_out_of_bounds(self):
+        test_ls = LocationSeries(dates=[20200101, 20200102, 20200103], values=[7, 8, 9])
+        with pytest.raises(ValueError) as exc:
+            test_ls.get_data_range(20191231, 20200103)
+            assert str(exc.value) == "Data range must be within existing dates 1-3"
+        with pytest.raises(ValueError) as exc:
+            test_ls.get_data_range(20200101, 20200104)
+            assert str(exc.value) == "Data range must be within existing dates 1-3"
+
+    def test_get_data_range_no_impute(self):
+        test_ls = LocationSeries(dates=[20200101, 20200102, 20200103], values=[7, np.nan, 9])
+        assert test_ls.get_data_range(20200101, 20200103, None) == [7, np.nan, 9]
+        assert test_ls.get_data_range(20200101, 20200102, None) == [7, np.nan]
+
+    def test_get_data_range_mean_impute(self):
+        test_ls = LocationSeries(dates=[20200101, 20200102, 20200103], values=[7, np.nan, 9])
+        assert test_ls.get_data_range(20200101, 20200103, "mean") == [7, 8.0, 9]
+        assert test_ls.get_data_range(20200101, 20200102, "mean") == [7, 7]
+
+    def test_get_data_range_invalid_impute(self):
+        test_ls = LocationSeries(dates=[20200101, 20200102, 20200103], values=[7, np.nan, 9])
+        with pytest.raises(ValueError) as exc:
+            test_ls.get_data_range(20200101, 20200103, "fakeimpute")
+            assert str(exc.value) == "Invalid imputation method. Must be None or 'mean'"

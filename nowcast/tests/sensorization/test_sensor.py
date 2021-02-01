6b758f5f2c03f6f2d187b343a027be90d4654157
andrew <chinandrew96@gmail.com>
2021-02-01 08:11:15 -0800

    Prefix mock functions with mock_

--- nowcast/tests/sensorization/test_sensor.py
+++ nowcast/tests/sensorization/test_sensor.py
@@ -13,10 +13,10 @@ class TestComputeSensors:
 
     @patch("delphi_nowcast.sensorization.sensor.compute_ar_sensor")
     @patch("delphi_nowcast.sensorization.sensor.get_indicator_data")
-    def test_compute_sensors_no_covariates(self, get_indicator_data, compute_ar_sensor):
+    def test_compute_sensors_no_covariates(self, mock_get_indicator_data, mock_compute_ar_sensor):
         """Test only ground truth sensor is returned if no data is available to compute the rest."""
-        get_indicator_data.return_value = {}
-        compute_ar_sensor.return_value = 1.5
+        mock_get_indicator_data.return_value = {}
+        mock_compute_ar_sensor.return_value = 1.5
         test_sensors = [SensorConfig("a", "b", "c", 1), SensorConfig("x", "y", "z", 2)]
         test_ground_truth_sensor = SensorConfig("i", "j", "k", 3)
         test_ground_truth = [LocationSeries("ca", "state")]
@@ -29,12 +29,12 @@ class TestComputeSensors:
     @patch("delphi_nowcast.sensorization.sensor.compute_regression_sensor")
     @patch("delphi_nowcast.sensorization.sensor.compute_ar_sensor")
     @patch("delphi_nowcast.sensorization.sensor.get_indicator_data")
-    def test_compute_sensors_covariates(self, get_indicator_data, compute_ar_sensor, compute_regression_sensor):
+    def test_compute_sensors_covariates(self, mock_get_indicator_data, mock_compute_ar_sensor, mock_compute_regression_sensor):
         """Test ground truth sensor and non-na regression sensor ar returned"""
-        get_indicator_data.return_value = {("a", "b", "state", "ca"): ["placeholder"],
+        mock_get_indicator_data.return_value = {("a", "b", "state", "ca"): ["placeholder"],
                                            ("x", "y", "state", "ca"): ["placeholder"]}
-        compute_ar_sensor.return_value = 1.5
-        compute_regression_sensor.side_effect = [2.5, np.nan]  # nan means 2nd sensor is skipped
+        mock_compute_ar_sensor.return_value = 1.5
+        mock_compute_regression_sensor.side_effect = [2.5, np.nan]  # nan means 2nd sensor is skipped
         test_sensors = [SensorConfig("a", "b", "c", 1), SensorConfig("x", "y", "z", 2)]
         test_ground_truth_sensor = SensorConfig("i", "j", "k", 3)
         test_ground_truth = [LocationSeries("ca", "state")]
@@ -49,9 +49,9 @@ class TestComputeSensors:
 class TestHisoricalSensors:
 
     @patch("delphi_nowcast.sensorization.sensor.get_historical_sensor_data")
-    def test_historical_sensors_some_data(self, historical):
+    def test_historical_sensors_some_data(self, mock_historical):
         """Test non empty data is returned for first two sensors."""
-        historical.side_effect = [(LocationSeries(dates=[date(2020, 1, 1)], values=[2]), []),
+        mock_historical.side_effect = [(LocationSeries(dates=[date(2020, 1, 1)], values=[2]), []),
                                   (LocationSeries(dates=[date(2020, 1, 3)], values=[4]), []),
                                   (LocationSeries(), [])]
         test_sensors = [SensorConfig("i", "j", "k", 3), SensorConfig("a", "b", "c", 1), SensorConfig("x", "y", "z", 2)]
@@ -63,9 +63,9 @@ class TestHisoricalSensors:
         }
 
     @patch("delphi_nowcast.sensorization.sensor.get_historical_sensor_data")
-    def test_historical_sensors_no_data(self, historical):
+    def test_historical_sensors_no_data(self, mock_historical):
         """Test nothing returned for any sensor."""
-        historical.return_value = (LocationSeries(), [])
+        mock_historical.return_value = (LocationSeries(), [])
         test_sensors = [SensorConfig("i", "j", "k", 3), SensorConfig("a", "b", "c", 1), SensorConfig("x", "y", "z", 2)]
         test_ground_truth = [LocationSeries("ca", "state")]
         assert historical_sensors(

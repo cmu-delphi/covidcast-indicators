c9833620f241b7641d45dc7c99fcdd57c8a72575
Mike O'Brien <mikeob@google.com>
2021-02-08 16:24:39 -0500

    change runner to a function

--- _delphi_utils_python/delphi_utils/runner.py
+++ _delphi_utils_python/delphi_utils/runner.py
@@ -1,43 +1,18 @@
 """Indicator running utilities."""
-
-import abc
-from logging import Logger
-from typing import Any, Dict, Type
-
-from .archive import ArchiveDiffer
+from typing import Any, Callable, Dict, Optional
 from delphi_validator.validate import Validator
+from .archive import ArchiveDiffer
+from .utils import read_params
 
-class Indicator(abc.ABC):
-    """Abstract base class for all indicators."""
-
-    @abc.abstractmethod
-    def __init__(self, params: Dict[str, Any]):
-        """Construct the indicator using key-value parameters."""
-        self._name = ""
-        raise NotImplementedError
-
-    @property
-    def name(self) -> str:
-        """Extract the name of the indicator."""
-        return self._name
-
-
-    @abc.abstractmethod
-    def run(self):
-        """Run the indicator."""
-        raise NotImplementedError
-
-
-def run_indicator(indicator_type: Type[Indicator],
-                  validator_type:  Type[Validator],
-                  archiver_type:  Type[ArchiveDiffer],
-                  params: Dict[str, Any]):
+def run_indicator_pipeline(indicator_fn:  Callable[[Dict[str, Any]], None],
+                           validator_fn:  Callable[[Dict[str, Any]], Optional[Validator]],
+                           archiver_fn:  Callable[[Dict[str, Any]], Optional[ArchiveDiffer]]):
     """Runs the indicator."""
-    indicator = indicator_type(params)
-    validator = validator_type(params["validation"])
-    archiver = archiver_type(params)
-
-    indicator.run()
-    validation_report = validator.validate()
-    if validation_report.success():         
+    params = read_params()
+    indicator_fn(params)
+    validator = validator_fn(params)
+    archiver = archiver_fn(params)
+    if validator:
+        validation_report = validator.validate()
+    if archiver and (not validator or validation_report.success()):
         archiver.archive()

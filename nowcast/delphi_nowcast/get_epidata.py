b6e585ca4cb0340c956150f2c77fbad9ecd6a95c
andrew <chinandrew96@gmail.com>
2021-02-15 12:18:10 -0800

    switch to epidata client

--- nowcast/delphi_nowcast/get_epidata.py
+++ nowcast/delphi_nowcast/get_epidata.py
@@ -5,40 +5,12 @@ from itertools import product
 
 from numpy import isnan
 from pandas import date_range
-from aiohttp import ClientSession
 
-# from ..delphi_epidata import Epidata  # used for local testing
 from delphi_epidata import Epidata
 
-from ..data_containers import LocationSeries, SensorConfig
+from delphi_nowcast.data_containers import LocationSeries, SensorConfig
 
 EPIDATA_START_DATE = 20200101
-
-async def get(params, session, sensor, location):
-    """Helper function to make Epidata GET requests."""
-    async with session.get(Epidata.BASE_URL, params=params) as response:
-        return await response.json(), sensor, location
-
-
-async def fetch_epidata(combos, as_of):
-    """Helper function to asynchronously make and aggregate Epidata GET requests."""
-    tasks = []
-    async with ClientSession() as session:
-        for sensor, location in combos:
-            params = {
-                    "endpoint": "covidcast",
-                    "data_source": sensor.source,
-                    "signals": sensor.signal,
-                    "time_type": "day",
-                    "geo_type": location.geo_type,
-                    "time_values": f"{EPIDATA_START_DATE}-{as_of}",
-                    "geo_value": location.geo_value,
-                    "as_of": as_of
-                }
-            task = asyncio.create_task(get(params, session, sensor, location))
-            tasks.append(task)
-        responses = await asyncio.gather(*tasks)
-        return responses
 
 
 def get_indicator_data(sensors: List[SensorConfig],
@@ -61,29 +33,41 @@ def get_indicator_data(sensors: List[SensorConfig],
     # gets all available data up to as_of day for now, could be optimized to only get a window
     output = {}
     all_combos = product(sensors, locations)
-    loop = asyncio.get_event_loop()
-    future = asyncio.ensure_future(fetch_epidata(all_combos, as_of.strftime("%Y%m%d")))
-    responses = loop.run_until_complete(future)
-    for response, sensor, location in responses:
+    all_params = [
+        {"source": "covidcast",
+         "data_source": sensor.source,
+         "signals": sensor.signal,
+         "time_type": "day",
+         "geo_type": location.geo_type,
+         "geo_value": location.geo_value,
+         "time_values": f"{EPIDATA_START_DATE}-{as_of.strftime('%Y%m%d')}",
+         "as_of": as_of.strftime("%Y%m%d")}
+        for sensor, location in all_combos
+    ]
+    responses = Epidata.async_epidata(all_params)
+    for response, params in responses:
         # -2 = no results, 1 = success. Truncated data or server errors may lead to this Exception.
         if response["result"] not in (-2, 1):
             raise Exception(f"Bad result from Epidata: {response['message']}")
         data = LocationSeries(
-            geo_value=location.geo_value,
-            geo_type=location.geo_type,
-            data={datetime.strptime(i["time_value"], "%Y%m%d").date(): i["value"]
+            geo_value=params["geo_value"],
+            geo_type=params["geo_type"],
+            data={datetime.strptime(str(i["time_value"]), "%Y%m%d").date(): i["value"]
                   for i in response.get("epidata", []) if not isnan(i["value"])}
         )
         if data.data:
-            output[(sensor.source, sensor.signal, location.geo_type, location.geo_value)] = data
+            output[(params["data_source"],
+                    params["signals"],
+                    params["geo_type"],
+                    params["geo_value"])] = data
     return output
 
 
 def get_historical_sensor_data(sensor: SensorConfig,
                                geo_value: str,
                                geo_type: str,
-                               end_date: date,
-                               start_date: date) -> Tuple[LocationSeries, list]:
+                               start_date: date,
+                               end_date: date) -> Tuple[LocationSeries, list]:
     """
     Query Epidata API for historical sensorization data.
     Will only return values if they are not null. If any days are null or are not available,
@@ -114,6 +98,7 @@ def get_historical_sensor_data(sensor: SensorConfig,
         geo_value=geo_value,
         sensor_names=sensor.name,
         lag=sensor.lag)
+    all_dates = [i.date() for i in date_range(start_date, end_date)]
     if response["result"] == 1:
         output = LocationSeries(
             geo_value=geo_value,
@@ -121,11 +106,11 @@ def get_historical_sensor_data(sensor: SensorConfig,
             data={datetime.strptime(str(i["time_value"]), "%Y%m%d").date(): i["value"]
                   for i in response.get("epidata", []) if not isnan(i["value"])}
         )
+        missing_dates = [i for i in all_dates if i not in output.dates]
+        return output, missing_dates
     elif response["result"] == -2:  # no results
         print("No historical results found")
         output = LocationSeries(geo_value=geo_value, geo_type=geo_type)
-    else:
-        raise Exception(f"Bad result from Epidata: {response['message']}")
-    all_dates = [i.date() for i in date_range(start_date, end_date)]
-    missing_dates = [i for i in all_dates if i not in output.dates]
-    return output, missing_dates
+        return output, all_dates
+    raise Exception(f"Bad result from Epidata: {response['message']}")
+

bb941d0accc84ac70d56836d1eb82051f622722b
Mike O'Brien <mikeob@google.com>
2021-02-18 14:11:05 -0500

    don't test aws behavior

--- jhu/tests/conftest.py
+++ jhu/tests/conftest.py
@@ -3,8 +3,6 @@
 from os import listdir, remove
 from os.path import join
 
-from boto3 import Session
-from moto import mock_s3
 import pandas as pd
 import pytest
 
@@ -18,14 +16,6 @@ PARAMS =  {
         "base_url": "test_data/small_{metric}.csv",
         "export_start_date": "2020-03-03",
         "static_file_dir": "../static"
-    },
-    "archive": {
-        "aws_credentials": {
-        "aws_access_key_id": "FAKE_TEST_ACCESS_KEY_ID",
-        "aws_secret_access_key": "FAKE_TEST_SECRET_ACCESS_KEY"
-        },
-        "bucket_name": "test-bucket",
-        "cache_dir": "./cache"
     }
 }
 
@@ -36,13 +26,7 @@ def run_as_module():
         if fname != ".gitkeep":
             remove(join("receiving", fname))
 
-    with mock_s3():
-        # Create the fake bucket we will be using
-        aws_credentials = PARAMS["archive"]["aws_credentials"]
-        s3_client = Session(**aws_credentials).client("s3")
-        s3_client.create_bucket(Bucket=PARAMS["archive"]["bucket_name"])
-
-        run_module(PARAMS)
+    run_module(PARAMS)
 
 @pytest.fixture
 def jhu_confirmed_test_data():

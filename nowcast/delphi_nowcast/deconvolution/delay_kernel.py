56f1cbf225a47cf8e5638fcac8db2e55e824c0ef
Maria Jahja <mariajahja@gmail.com>
2021-02-24 11:08:06 -0500

    add cdc public natl linelist delay

--- nowcast/delphi_nowcast/deconvolution/delay_kernel.py
+++ nowcast/delphi_nowcast/deconvolution/delay_kernel.py
@@ -5,6 +5,49 @@ import pandas as pd
 import scipy.stats as stats
 
 
+def get_national_delay_distribution(
+        update: bool = False,
+        start_date: date = datetime(2020, 10, 1)) -> Tuple[List[float], List[float]]:
+    """
+    Retrieve distribution of delays from public CDC linelist.
+
+    The samples are clipped to [1, 45), units in days, and fit to a gamma distribution.
+    The discretized values are returned.
+
+    Parameters
+    ----------
+    update
+        Boolean whether to pull the latest data and recalculate the distribution
+    start_date
+        starting date to clip the delay samples from the linelist data. this is ignored
+        if update=False. the default is October 1, 2020.
+
+    Returns
+    -------
+        tuple of the distribution and gamma parameters
+    """
+
+    if update:
+        linelist = pd.read_csv("https://data.cdc.gov/api/views/vbim-akqf/rows.csv",
+                               usecols=["pos_spec_dt", "onset_dt"],
+                               parse_dates=["pos_spec_dt", "onset_dt"])
+
+        linelist = linelist[~linelist.onset_dt.isna()]
+        delay_df = linelist[linelist.onset_dt > start_date]
+        delay_df["report_delay"] = (delay_df.pos_spec_dt - delay_df.onset_dt).dt.days
+        delay_df = delay_df[delay_df.report_delay.gt(0) & delay_df.report_delay.lt(45)]
+
+        coefs = stats.gamma.fit(delay_df.report_delay, floc=0)
+    else:
+        coefs = (1.717706108504553, 0, 2.2112872173489597)
+
+    # discretized distribution
+    delay_gam = stats.gamma(*coefs)
+    delay_dist = delay_gam.pdf(range(1, 45))
+    delay_dist /= delay_dist.sum()
+    return list(delay_dist), list(coefs)
+
+
 def get_florida_delay_distribution(
         update: bool = False,
         start_date: date = datetime(2020, 5, 1)) -> Tuple[List[float], List[float]]:

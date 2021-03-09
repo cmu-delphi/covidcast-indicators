b515d43bdbf072e17fafc3da71067a6ddcd8cbf5
Mike O'Brien <mikeob@google.com>
2021-03-09 16:34:13 -0500

    add a main to the indicator runner

--- _delphi_utils_python/delphi_utils/runner.py
+++ _delphi_utils_python/delphi_utils/runner.py
@@ -1,8 +1,12 @@
 """Indicator running utilities."""
+import argparse as ap
+import importlib
 from typing import Any, Callable, Dict, Optional
 from .archive import ArchiveDiffer
+from .archive import from_params as archiver_from_params
 from .utils import read_params
 from .validator.validate import Validator
+from .validator.run import from_params as validator_from_params
 
 Params = Dict[str, Any]
 
@@ -33,3 +37,14 @@ def run_indicator_pipeline(indicator_fn:  Callable[[Params], None],
         validation_report = validator.validate()
     if archiver and (not validator or validation_report.success()):
         archiver.archive()
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser()
+    parser.add_argument("indicator_name",
+                        type=str,
+                        help="Name of the Python package containing the indicator.  This package "
+                             "must export a `run_module(params)` function.")
+    args = parser.parse_args()
+    indicator_module = importlib.import_module(args.indicator_name)
+    run_indicator_pipeline(indicator_module.run_module, validator_from_params, archiver_from_params)

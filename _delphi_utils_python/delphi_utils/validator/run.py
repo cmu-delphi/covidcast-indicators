551b4bc6262de219db75428529afe8840d720a56
Mike O'Brien <mikeob@google.com>
2021-03-29 11:00:45 -0400

    Add --dry_run flag to validator

--- _delphi_utils_python/delphi_utils/validator/run.py
+++ _delphi_utils_python/delphi_utils/validator/run.py
@@ -4,14 +4,19 @@
 This module should contain a function called `run_module`, that is executed
 when the module is run with `python -m delphi_validator`.
 """
+import argparse as ap
 from .. import read_params
 from .validate import Validator
 
 
 def run_module():
     """Run the validator as a module."""
+    parser = ap.ArgumentParser()
+    parser.add_argument("--dry_run", action="store_true", help="When provided, return zero exit"
+                        " status irrespective of the number of failures")
+    args = parser.parse_args()
     validator = Validator(read_params())
-    validator.validate().print_and_exit()
+    validator.validate().print_and_exit(not args.dry_run)
 
 
 def validator_from_params(params):

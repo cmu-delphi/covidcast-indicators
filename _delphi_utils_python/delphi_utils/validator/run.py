84cb1c820799484cc9fe819c895fb9a7afb96a7b
Mike O'Brien <mikeob@google.com>
2021-03-31 15:28:56 -0400

    change logging schemefor validation

--- _delphi_utils_python/delphi_utils/validator/run.py
+++ _delphi_utils_python/delphi_utils/validator/run.py
@@ -5,7 +5,7 @@ This module should contain a function called `run_module`, that is executed
 when the module is run with `python -m delphi_utils.validator`.
 """
 import argparse as ap
-from .. import read_params
+from .. import read_params, get_structured_logger
 from .validate import Validator
 
 
@@ -15,8 +15,12 @@ def run_module():
     parser.add_argument("--dry_run", action="store_true", help="When provided, return zero exit"
                         " status irrespective of the number of failures")
     args = parser.parse_args()
-    validator = Validator(read_params())
-    validator.validate().print_and_exit(not args.dry_run)
+    params = read_params()
+    validator = Validator(params)
+    validator.validate().print_and_exit(
+        get_structured_logger(__name__,
+                              params["common"].get("log_filename", None),
+                              not args.dry_run))
 
 
 def validator_from_params(params):

fd7457cb62467804690e45a7e74e3505b7d404da
David Kretch <david.kretch@summitllc.us>
2021-04-11 20:42:57 -0400

    Add metadata to output files, output NA rows
    * Update `contingency_aggregate.R` to delete lines which previously removed output for groups with missing values as part of their group-by variables, e.g. missing occupation. Now it will output rows for groups with missing values in the group-by variables.
    
    * Update `contingency_write.R` to add start and end dates, period type, aggregation type, and geographic identifiers.
    
    * Add `state_list.csv` in the `static` folder to store state FIPS codes and GADM codes. This file is used to add this info to the output datasets.
    
    * Update `NAMESPACE` to include an extra import: `dplyr::bind_cols`.

--- facebook/delphiFacebook/R/contingency_aggregate.R
+++ facebook/delphiFacebook/R/contingency_aggregate.R
@@ -270,9 +270,6 @@ summarize_aggs <- function(df, crosswalk_data, aggregations, geo_level, params) 
     table(df[, group_vars, with=FALSE], exclude=NULL, dnn=group_vars), 
     stringsAsFactors=FALSE
   )
-  unique_groups_counts <- unique_groups_counts[
-    complete.cases(unique_groups_counts[, group_vars]),
-  ]
   
   # Drop groups with less than threshold sample size.
   unique_groups_counts <- filter(unique_groups_counts, Freq >= params$num_filter)
@@ -327,10 +324,6 @@ summarize_aggs <- function(df, crosswalk_data, aggregations, geo_level, params) 
     aggregation <- aggregations$id[row]
     group_vars <- aggregations$group_by[[row]]
     post_fn <- aggregations$post_fn[[row]]
-
-    dfs_out[[aggregation]] <- dfs_out[[aggregation]][
-      rowSums(is.na(dfs_out[[aggregation]][, c("val", "sample_size", group_vars)])) == 0,
-    ]
 
     dfs_out[[aggregation]] <- apply_privacy_censoring(dfs_out[[aggregation]], params)
 

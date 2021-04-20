e34cd404422dc626806f064e91c8ea636e6d9366
Dmitry Shemetov <dshemetov@ucdavis.edu>
2021-04-20 13:06:04 -0700

    Nans nchs: update export function and tests

--- nchs_mortality/delphi_nchs_mortality/export.py
+++ nchs_mortality/delphi_nchs_mortality/export.py
@@ -29,7 +29,7 @@ def export_csv(df, geo_name, sensor, export_dir, start_date):
         date_short = "weekly_" + str(t.year) + str(t.week).zfill(2)
         export_fn = f"{date_short}_{geo_name}_{sensor}.csv"
         expected_columns = [
-            "geo_id", "val", "se", "sample_size"
+            "geo_id", "val", "se", "sample_size",
             "missing_val", "missing_se", "missing_sample_size"
         ]
         result_df = df[df["timestamp"] == date][expected_columns]

3f0ef7c8d64a17f19819673f3f6f8a004316dc0f
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2021-04-21 11:54:05 -0400

    Add documentation

--- facebook/translation-differ.R
+++ facebook/translation-differ.R
@@ -7,26 +7,35 @@
 ## Rscript translation-differ.R path/to/old/qsf path/to/new/qsf
 ##
 ## Writes the lists of new and changed items to STDOUT, so redirect STDOUT to
-## your desired location and compress as desired.
+## your desired location.
 
 
 #### Background on .qsf files
-## Keep only useful fields (item number and question text). We are only
-## interested in "SQ" (survey questions) and "BL" (block name + contained
-## questions -- ordered?).
-# SurveyElement types are BL (block), FL (flow), SO (survey options), SCO
-# (scoring), PROJ (?), notes, STAT (statistics), QC (question count), SQ
-# (survey questions), and RS (response set). Detailed info:
-# https://gist.github.com/ctesta01/d4255959dace01431fb90618d1e8c241.
 
+## A .qsf file is a json containing two elements, SurveyEntry with survey
+## metadata (start date, ID, name, etc) and SurveyElements with a numbered list
+## of survey components. For the purpose of finding differences in survey
+## contents, we are only interested in examining the SurveyElements item.
 
-# Payload (list) elements:
+## SurveyElement types are BL (block), FL (flow), SO (survey options), SCO
+## (scoring), PROJ (?), notes, STAT (statistics), QC (question count), SQ
+## (survey questions), and RS (response set). Detailed info:
+## https://gist.github.com/ctesta01/d4255959dace01431fb90618d1e8c241
+
+## We are only interested in survey questions ("SQ"). Within each SQ item,
+## details are stored in the Payload element, which can contain the following
+## fields:
+
 # [1] "QuestionText"         "QuestionType"         "Selector"             "Configuration"        "QuestionDescription"  "Choices"              "ChoiceOrder"         
 # [8] "Validation"           "AnalyzeChoices"       "Language"             "QuestionText_Unsafe"  "DataExportTag"        "QuestionID"           "DataVisibility"      
 # [15] "NextChoiceId"         "NextAnswerId"         "DefaultChoices"       "SubSelector"          "DisplayLogic"         "GradingData"          "Answers"             
 # [22] "AnswerOrder"          "ChoiceDataExportTags" "Randomization"        "RecodeValues"         "DynamicChoices"       "DynamicChoicesData"   "SearchSource"        
 # [29] "QuestionJS"  
 
+## The meaning of "Answers" and "Choices" differs for matrix vs non-matrix
+## items. "Choices" list the vertical components -- subquestions for matrix
+## items and answer choices for non-matrix items. "Answers" list the answer
+## choices for matrix items and are missing for non-matrix items.
 
 
 suppressPackageStartupMessages({
@@ -38,8 +47,6 @@ suppressPackageStartupMessages({
 #'
 #' @param old_qsf_path path to older Qualtrics translation file in .qsf format
 #' @param new_qsf_path path to newer Qualtrics translation file in .qsf format
-#'
-#' @return A named list containing new and changed item names
 diff_qsf_files <- function(old_qsf_path, new_qsf_path) {
   old_qsf <- get_qsf_file(old_qsf_path)
   new_qsf <- get_qsf_file(new_qsf_path)
@@ -49,26 +56,37 @@ diff_qsf_files <- function(old_qsf_path, new_qsf_path) {
   return(NULL)
 }
 
-## Fetch and format a single translation file.
+#' Fetch and format a single .qsf translation file.
+#'
+#' @param path path to Qualtrics translation file in .qsf format
+#'
+#' @return A named list
 get_qsf_file <- function(path) {
   # Read file as json.
   qsf <- read_json(path, simplifyVector = TRUE)
   
   questions <- filter(qsf$SurveyElements, Element == "SQ")
-  keep_items <- c("QuestionID", "DataExportTag", "QuestionText", "QuestionType", "Choices", "Answers", "DisplayLogic")
+  keep_items <- c("QuestionID", "DataExportTag", "QuestionText",
+                  "QuestionType", "Choices", "Answers", "DisplayLogic")
   
   questions_out <- list()
   for (question in questions$Payload) {
     question <- question[names(question) %in% keep_items]
     
+    # For matrix questions, "Choices" are the subquestion text and code. For all
+    # other questions, "Choices" are the answer choices and corresponding
+    # response code.
     subquestion <- names(question$Choices)
     question$Choices <- unlist(question$Choices)
     names(question$Choices) <- subquestion
     
+    # For matrix questions, "Answers" are the answer choices and corresponding
+    # response code. For all other questions, this is not provided.
     response_codes <- names(question$Answers)
     question$Answers <- unlist(question$Answers)
     names(question$Answers) <- response_codes
     
+    # Rearrange items to be consistent between matrix and other items.
     if (question$QuestionType == "Matrix") {
       question$Subquestions <- question$Choices
       question$Choices <- question$Answers
@@ -77,7 +95,14 @@ get_qsf_file <- function(path) {
       question$Subquestions <- "NULL"
     }
     
-    question$DisplayLogic <- ifelse(is.null(question$DisplayLogic$`0`$`0`$Description), "NULL", question$DisplayLogic$`0`$`0`$Description)
+    # DisplayLogic "Description" summarizes display logic (e.g. if item is shown
+    # conditional on another question), including the conditioning question
+    # text, comparator, and comparison value.
+    question$DisplayLogic <- ifelse(
+      is.null(question$DisplayLogic$`0`$`0`$Description),
+      "NULL",
+      question$DisplayLogic$`0`$`0`$Description
+      )
     
     questions_out[[question$QuestionID]] <- question
   }
@@ -85,7 +110,10 @@ get_qsf_file <- function(path) {
   return(questions_out)
 }
 
-## Compare old vs new questions.
+#' Compare old vs new questions in the two surveys.
+#'
+#' @param old_qsf trimmed output from `get_qsf_file` for older survey
+#' @param new_qsf trimmed output from `get_qsf_file` for newer survey
 diff_questions <- function(old_qsf, new_qsf) {
   old_names <- names(old_qsf)
   new_names <- names(new_qsf)
@@ -115,7 +143,14 @@ diff_questions <- function(old_qsf, new_qsf) {
   return(NULL)
 }
 
-## Compare a single question field.
+#' Compare a single question field in the two surveys.
+#'
+#' @param names character vector of Qualtrics question IDs
+#' @param change_type character; type of change to look for
+#' @param comparator character; name of question element to compare between
+#'   survey versions
+#' @param old_qsf trimmed output from `get_qsf_file` for older survey
+#' @param new_qsf trimmed output from `get_qsf_file` for newer survey
 diff_question <- function(names, change_type=c("answers", "wording", "display logic", "subquestions"), comparator, old_qsf, new_qsf) {
   change_type <- match.arg(change_type)
   
@@ -130,7 +165,14 @@ diff_question <- function(names, change_type=c("answers", "wording", "display lo
   return(NULL)
 }
 
-## Print results with custom message.
+#' Print results with custom message for each possible change type.
+#'
+#' @param questions character vector of Qualtrics question IDs for items that
+#'   changed between survey versions
+#' @param change_type character; type of change to look for
+#' @param reference_qsf trimmed output from `get_qsf_file` for survey that
+#'   contains descriptive info about a particular type of change. For "removed"
+#'   questions, should be older survey, else newer survey.
 print_questions <- function(questions, change_type=c("added", "removed", "answers", "wording", "display logic", "subquestions"), reference_qsf) {
   if ( length(questions) > 0 ) {
     change_type <- match.arg(change_type)

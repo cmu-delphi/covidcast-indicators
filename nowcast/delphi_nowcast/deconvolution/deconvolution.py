1962e17e4e85935e105e93db1b1c9b1959b4aea0
andrew <chinandrew96@gmail.com>
2021-04-27 15:02:35 -0700

    Add deconvolution helper functions

--- nowcast/delphi_nowcast/deconvolution/deconvolution.py
+++ nowcast/delphi_nowcast/deconvolution/deconvolution.py
@@ -1,6 +1,130 @@
 """Deconvolution functions."""
 
+from functools import partial
+from typing import Callable
 
-def deconvolve_double_smooth_tf_cv(x, y, z):
-    x, y, z
-    pass
+import numpy as np
+from scipy.linalg import toeplitz
+from scipy.sparse import diags as band
+
+
+def _construct_convolution_matrix(signal: np.ndarray,
+                                  kernel: np.ndarray,
+                                  norm: bool) -> np.ndarray:
+    """
+    Constructs full convolution matrix (n+m-1) x n,
+    where n is the signal length and m the kernel length.
+
+    Parameters
+    ----------
+    signal
+        array of values to convolve
+    kernel
+        array with convolution kernel values
+    norm
+        boolean whether to normalize rows to sum to sum(kernel)
+
+    Returns
+    -------
+        convolution matrix
+    """
+    n = signal.shape[0]
+    padding = np.zeros(n - 1)
+    first_col = np.r_[kernel, padding]
+    first_row = np.r_[kernel[0], padding]
+    P = toeplitz(first_col, first_row)
+    if norm:
+        scale = P.sum(axis=1) / kernel.sum()
+        return P / scale[:, np.newaxis]
+    return P
+
+
+def _soft_thresh(x: np.ndarray, lam: float) -> np.ndarray:
+    """Perform soft-thresholding of x with threshold lam."""
+    return np.sign(x) * np.maximum(np.abs(x) - lam, 0)
+
+
+def _fft_convolve(signal: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    """
+    Perform 1D convolution in the frequency domain.
+
+    Parameters
+    ----------
+    signal
+        array of values to convolve
+    kernel
+        array with convolution kernel values
+
+    Returns
+    -------
+        array with convolved signal values
+    """
+    n = signal.shape[0]
+    m = kernel.shape[0]
+    signal_freq = np.fft.fft(signal, n + m - 1)
+    kernel_freq = np.fft.fft(kernel, n + m - 1)
+    return np.fft.ifft(signal_freq * kernel_freq).real[:n]
+
+
+def _impute_with_neighbors(x: np.ndarray) -> np.ndarray:
+    """
+    Impute missing values with the average of the elements immediately
+    before and after.
+
+    Parameters
+    ----------
+    x
+        Signal with missing values.
+
+    Returns
+    -------
+        Imputed signal.
+    """
+    # handle edges
+    if np.isnan(x[0]):
+        x[0] = x[1]
+    if np.isnan(x[-1]):
+        x[-1] = x[-2]
+    imputed_x = np.copy(x)
+    for i, (a, b, c) in enumerate(zip(x, x[1:], x[2:])):
+        if np.isnan(b):
+            imputed_x[i + 1] = (a + c) / 2
+    assert np.isnan(imputed_x).sum() == 0
+    return imputed_x
+
+
+def _construct_poly_interp_mat(x: np.ndarray, k: int = 3):
+    """
+    Generate polynomial interpolation matrix.
+
+    Currently only implemented for 3rd order polynomials.
+
+    Parameters
+    ----------
+    x
+        Input signal.
+    k
+        Order of the polynomial interpolation.
+
+    Returns
+    -------
+        n x (n - k - 1) matrix.
+    """
+    assert k == 3, "poly interpolation matrix only constructed for k=3"
+    n = x.shape[0]
+    S = np.zeros((n, n - k - 1))
+    S[0, 0] = (x[3] - x[0]) / (x[3] - x[2])
+    S[0, 1] = (x[0] - x[2]) / (x[3] - x[2])
+    S[1, 0] = (x[3] - x[1]) / (x[3] - x[2])
+    S[1, 1] = (x[1] - x[2]) / (x[3] - x[2])
+    S[n - 2, n - 6] = (x[n - 3] - x[n - 2]) / (x[n - 3] - x[n - 4])
+    S[n - 2, n - 5] = (x[n - 2] - x[n - 4]) / (x[n - 3] - x[n - 4])
+    S[n - 1, n - 6] = (x[n - 3] - x[n - 1]) / (x[n - 3] - x[n - 4])
+    S[n - 1, n - 5] = (x[n - 1] - x[n - 4]) / (x[n - 3] - x[n - 4])
+    S[2:(n - 2), :] = np.eye(n - k - 1)
+    return S
+
+
+def _linear_extrapolate(x0, y0, x1, y1, x_new):
+    """Linearly extrapolate the value at x_new from 2 given points (x0, y0) and (x1, y1)."""
+    return y0 + ((x_new - x0) / (x1 - x0)) * (y1 - y0)
---
+++ nowcast/tests/deconvolution/test_deconvolution.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pytest
+
+
+from delphi_nowcast.deconvolution.deconvolution import (_linear_extrapolate, _construct_poly_interp_mat,
+                                                        _impute_with_neighbors, _fft_convolve, _soft_thresh)
+
+class Test_SoftThresh:
+
+    def test__soft_thresh(self):
+        np.testing.assert_array_equal(_soft_thresh(np.arange(-3,4), 1),
+                                      np.array([-2, -1,  0,  0,  0,  1,  2]))
+
+class Test_FFTConvolve:
+
+    def test__fft_convolve(self):
+        np.testing.assert_array_equal(_fft_convolve(np.array([1, 0, 1]), np.array([2, 7])),
+                                      np.array([2, 7, 2]))
+
+class Test_ImputeWithNeighbors:
+
+    def test__impute_with_neighbors(self):
+        np.testing.assert_array_equal(_impute_with_neighbors([np.nan, 1, np.nan, 3, np.nan]),
+                                      np.array([1, 1, 2, 3, 3]))
+
+
+    def test__impute_with_neighbors_no_missing(self):
+        np.testing.assert_array_equal(_impute_with_neighbors(np.arange(5)),
+                                      np.arange(5))
+
+class Test_ConstructPolyInterpMat:
+
+    def test__construct_poly_interp_mat(self):
+        np.testing.assert_array_equal(_construct_poly_interp_mat(np.arange(6), 3),
+                                      np.array([[3., -2.],
+                                                [2., -1.],
+                                                [1., 0.],
+                                                [0., 1.],
+                                                [-1., 2.],
+                                                [-2., 3.]]))
+
+    def test__construct_poly_interp_mat_wrong_k(self):
+        with pytest.raises(AssertionError):
+            _construct_poly_interp_mat(np.arange(6), 2)
+
+
+class Test_LinearExtrapolate:
+
+    def test__linear_extrapolate(self):
+        assert _linear_extrapolate(0, 0, 1, 3, 4) == 12

5f162b5feb5a19f985fa2ad439479ecb84d0fc1a
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2021-09-01 17:03:48 -0400

    remove test code

--- facebook/contingency_tables.R
+++ facebook/contingency_tables.R
@@ -1,6 +1,5 @@
 library(delphiFacebook)
 
-#Rprof(interval = 0.5)
 params <- read_contingency_params("params.json")
 run_contingency_tables(params)
 message("run_contingency_tables completed successfully")

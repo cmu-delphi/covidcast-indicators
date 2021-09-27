49a5766d0739dd3055cd08d74e0b30ba33ce31dc
Dmitry Shemetov <dmtryshmtv@gmail.com>
2021-09-27 14:22:00 -0700

    Nancodes archiver/export: explicit tests

--- _delphi_utils_python/tests/test_export.py
+++ _delphi_utils_python/tests/test_export.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 from delphi_utils import create_export_csv, Nans
 
+
 def _clean_directory(directory):
     """Clean files out of a directory."""
     for fname in listdir(directory):
@@ -29,6 +30,7 @@ def _non_ignored_files_set(directory):
 
 class TestExport:
     """Tests for exporting CSVs."""
+
     # List of times for data points.
     TIMES = [
         datetime.strptime(x, "%Y-%m-%d")
@@ -54,9 +56,19 @@ class TestExport:
             "val": [3.12345678910, np.nan, 2.2, 2.6],
             "se": [0.15, 0.22, np.nan, 0.34],
             "sample_size": [100, 100, 101, None],
-            "missing_val": [Nans.NOT_MISSING, Nans.OTHER, Nans.NOT_MISSING, Nans.NOT_MISSING],
-            "missing_se": [Nans.NOT_MISSING, Nans.NOT_MISSING, Nans.OTHER, Nans.NOT_MISSING],
-            "missing_sample_size": [Nans.NOT_MISSING] * 3 + [Nans.OTHER]
+            "missing_val": [
+                Nans.NOT_MISSING,
+                Nans.OTHER,
+                Nans.NOT_MISSING,
+                Nans.NOT_MISSING,
+            ],
+            "missing_se": [
+                Nans.NOT_MISSING,
+                Nans.NOT_MISSING,
+                Nans.OTHER,
+                Nans.NOT_MISSING,
+            ],
+            "missing_sample_size": [Nans.NOT_MISSING] * 3 + [Nans.OTHER],
         }
     )
 
@@ -68,9 +80,19 @@ class TestExport:
             "val": [np.nan, np.nan, 2.2, 2.6],
             "se": [0.15, 0.22, np.nan, 0.34],
             "sample_size": [100, 100, 101, None],
-            "missing_val": [Nans.NOT_MISSING, Nans.OTHER, Nans.NOT_MISSING, Nans.NOT_MISSING],
-            "missing_se": [Nans.NOT_MISSING, Nans.NOT_MISSING, Nans.OTHER, Nans.NOT_MISSING],
-            "missing_sample_size": [Nans.NOT_MISSING] * 3 + [Nans.OTHER]
+            "missing_val": [
+                Nans.NOT_MISSING,
+                Nans.OTHER,
+                Nans.NOT_MISSING,
+                Nans.NOT_MISSING,
+            ],
+            "missing_se": [
+                Nans.NOT_MISSING,
+                Nans.NOT_MISSING,
+                Nans.OTHER,
+                Nans.NOT_MISSING,
+            ],
+            "missing_sample_size": [Nans.NOT_MISSING] * 3 + [Nans.OTHER],
         }
     )
 
@@ -116,10 +138,14 @@ class TestExport:
         )
         pd.testing.assert_frame_equal(
             pd.read_csv(join(self.TEST_DIR, "20200215_county_deaths_test.csv")),
-            pd.DataFrame({"geo_id": [51093, 51175],
-                          "val": [round(3.12345678910, 7), 2.1],
-                          "se": [0.15, 0.22],
-                          "sample_size": [100, 100]})
+            pd.DataFrame(
+                {
+                    "geo_id": [51093, 51175],
+                    "val": [round(3.12345678910, 7), 2.1],
+                    "se": [0.15, 0.22],
+                    "sample_size": [100, 100],
+                }
+            ),
         )
 
     def test_export_without_metric(self):
@@ -211,13 +237,16 @@ class TestExport:
         """Test that `remove_null_samples = True` removes entries with null samples."""
         _clean_directory(self.TEST_DIR)
 
-        df_with_nulls = self.DF.copy().append({
-                                "geo_id": "66666",
-                                "timestamp": datetime(2020, 6, 6),
-                                "val": 10,
-                                "se": 0.2,
-                                "sample_size": pd.NA},
-                            ignore_index=True)
+        df_with_nulls = self.DF.copy().append(
+            {
+                "geo_id": "66666",
+                "timestamp": datetime(2020, 6, 6),
+                "val": 10,
+                "se": 0.2,
+                "sample_size": pd.NA,
+            },
+            ignore_index=True,
+        )
 
         create_export_csv(
             df=df_with_nulls,
@@ -241,13 +270,16 @@ class TestExport:
         """Test that `remove_null_samples = False` does not remove entries with null samples."""
         _clean_directory(self.TEST_DIR)
 
-        df_with_nulls = self.DF.copy().append({
-                                "geo_id": "66666",
-                                "timestamp": datetime(2020, 6, 6),
-                                "val": 10,
-                                "se": 0.2,
-                                "sample_size": pd.NA},
-                            ignore_index=True)
+        df_with_nulls = self.DF.copy().append(
+            {
+                "geo_id": "66666",
+                "timestamp": datetime(2020, 6, 6),
+                "val": 10,
+                "se": 0.2,
+                "sample_size": pd.NA,
+            },
+            ignore_index=True,
+        )
 
         create_export_csv(
             df=df_with_nulls,
@@ -267,24 +299,56 @@ class TestExport:
         )
         assert pd.read_csv(join(self.TEST_DIR, "20200606_state_test.csv")).size > 0
 
+    def test_export_df_without_missingness(self):
+        _clean_directory(self.TEST_DIR)
+
+        create_export_csv(
+            df=self.DF.copy(), export_dir=self.TEST_DIR, geo_res="county", sensor="test"
+        )
+        df = pd.read_csv(join(self.TEST_DIR, "20200215_county_test.csv")).astype(
+            {"geo_id": str, "sample_size": int}
+        )
+        expected_df = pd.DataFrame(
+            {
+                "geo_id": ["51093", "51175"],
+                "val": [3.12345678910, 2.1],
+                "se": [0.15, 0.22],
+                "sample_size": [100, 100],
+            }
+        ).astype({"geo_id": str, "sample_size": int})
+        pd.testing.assert_frame_equal(df, expected_df)
+
     def test_export_df_with_missingness(self):
         _clean_directory(self.TEST_DIR)
 
         create_export_csv(
             df=self.DF2.copy(),
             export_dir=self.TEST_DIR,
-            geo_res="state",
+            geo_res="county",
             sensor="test",
-            remove_null_samples=False
         )
         assert _non_ignored_files_set(self.TEST_DIR) == set(
             [
-                "20200215_state_test.csv",
-                "20200301_state_test.csv",
-                "20200315_state_test.csv",
+                "20200215_county_test.csv",
+                "20200301_county_test.csv",
+                "20200315_county_test.csv",
             ]
         )
-        assert pd.read_csv(join(self.TEST_DIR, "20200315_state_test.csv")).size > 0
+        df = pd.read_csv(join(self.TEST_DIR, "20200215_county_test.csv")).astype(
+            {"geo_id": str, "sample_size": int}
+        )
+        expected_df = pd.DataFrame(
+            {
+                "geo_id": ["51093", "51175"],
+                "val": [3.12345678910, np.nan],
+                "se": [0.15, 0.22],
+                "sample_size": [100, 100],
+                "missing_val": [Nans.NOT_MISSING, Nans.OTHER],
+                "missing_se": [Nans.NOT_MISSING] * 2,
+                "missing_sample_size": [Nans.NOT_MISSING] * 2,
+            }
+        ).astype({"geo_id": str, "sample_size": int})
+        pd.testing.assert_frame_equal(df, expected_df)
 
     @mock.patch("delphi_utils.logger")
     def test_export_df_with_contradictory_missingness(self, mock_logger):
@@ -295,7 +359,6 @@ class TestExport:
             export_dir=self.TEST_DIR,
             geo_res="state",
             sensor="test",
-            remove_null_samples=False,
             logger=mock_logger
         )
         assert _non_ignored_files_set(self.TEST_DIR) == set(

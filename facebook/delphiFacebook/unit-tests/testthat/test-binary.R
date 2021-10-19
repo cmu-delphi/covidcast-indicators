5d957b422d9699f11b895cfdec85ebc6e5afb7f4
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2021-10-19 13:10:02 -0400

    update unit tests

--- facebook/delphiFacebook/unit-tests/testthat/test-binary.R
+++ facebook/delphiFacebook/unit-tests/testthat/test-binary.R
@@ -1,8 +1,8 @@
 context("Testing functions for calculating binary and multinomial proportions")
 
 test_that("testing jeffreys_binary command", {
-  input <- tibble(val=c(0), sample_size=1)
-  expected_output <- tibble(val=c(25), sample_size=1, se=binary_se(25, 1))
+  input <- tibble(val=c(0), sample_size=1, effective_sample_size=1)
+  expected_output <- tibble(val=c(25), sample_size=1, effective_sample_size=1, se=binary_se(25, 1))
 
   expect_equal(jeffreys_binary(input), expected_output)
 })
@@ -10,8 +10,8 @@ test_that("testing jeffreys_binary command", {
 test_that("testing jeffreys_multinomial command", {
   jeffreys_multinomial <- jeffreys_multinomial_factory(4)
   
-  input <- tibble(val=c(0), sample_size=3)
-  expected_output <- tibble(val=c(25/4), sample_size=3, se=binary_se(25/4, 3))
+  input <- tibble(val=c(0), sample_size=3, effective_sample_size=3)
+  expected_output <- tibble(val=c(25/4), sample_size=3, effective_sample_size=3, se=binary_se(25/4, 3))
 
   expect_equal(jeffreys_multinomial(input), expected_output)
 })

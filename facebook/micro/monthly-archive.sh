b0e923e86ba822bce3cda67ae08097c74c18cfd0
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2021-11-09 12:11:40 -0500

    remove positional arg remapping
    since monthly-archive.sh doesn't use positional arguments. The $3, etc
    are actually local to awk (e.g. positional word by line).

--- facebook/micro/monthly-archive.sh
+++ facebook/micro/monthly-archive.sh
@@ -9,19 +9,12 @@ fi
 echo ${MONTH}
 R_MONTH=${MONTH#*_}; R_MONTH=${R_MONTH#0}
 
-# Save script's arguments to reference by name in perform_rollup_and_post() so the script's
-# positional arguments and perform_rollup_and_post's positional arguments don't get confusing.
-arg0=$0
-arg3=$3
-arg4=$4
-arg5=$5
-
 perform_rollup_and_post ()
 {
     BATCH="cd $1\nls -1 cvid_responses_${MONTH}*.gz"
     sftp -b <(echo -e "${BATCH}") -P 2222 fb-automation@ftp.delphi.cmu.edu 2>/dev/null | \
         grep "^cvid" | \
-        awk -F_ 'BEGIN{print "cd $1"} {key=$arg3 $arg4 $arg5; if (key!=last && last!="") {print record} last=key; record=$arg0} END{print record}' | \
+        awk -F_ 'BEGIN{print "cd $1"} {key=$3 $4 $5; if (key!=last && last!="") {print record} last=key; record=$0} END{print record}' | \
         sed '/^cvid/ s/^/get /' >fetch.sftp
     sftp -b fetch.sftp -P 2222 fb-automation@ftp.delphi.cmu.edu
     OUT=${MONTH/_/-}

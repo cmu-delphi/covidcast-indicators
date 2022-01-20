8f62e5fe8275fa88efeb06db172f8c8a3e0ebe46
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-01-20 17:02:52 -0500

    indent

--- dsew_community_profile/delphi_dsew_community_profile/pull.py
+++ dsew_community_profile/delphi_dsew_community_profile/pull.py
@@ -377,15 +377,15 @@ def fetch_new_reports(params, logger=None):
         if len(latest_sig_df.index) > 0:
             latest_sig_df = latest_sig_df.reset_index(drop=True)
 
-        assert all(latest_sig_df.groupby(
-                ["timestamp", "geo_id"]
-            ).size(
-            ).reset_index(
-                drop=True
-            ) == 1), f"Duplicate rows in {sig} indicate that one or" \
-            + " more reports was published multiple times and the copies differ"
+            assert all(latest_sig_df.groupby(
+                    ["timestamp", "geo_id"]
+                ).size(
+                ).reset_index(
+                    drop=True
+                ) == 1), f"Duplicate rows in {sig} indicate that one or" \
+                + " more reports was published multiple times and the copies differ"
 
-        ret[sig] = latest_sig_df
+            ret[sig] = latest_sig_df
 
     # add nation from state
     geomapper = GeoMapper()

5ea50459a194b673c6aacdefb11250919db7f37a
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-02-09 15:21:06 -0500

    qsf tools to rename V2a for wave 13

--- facebook/qsf-tools/generate-codebook.R
+++ facebook/qsf-tools/generate-codebook.R
@@ -20,6 +20,7 @@ process_qsf <- function(path_to_qsf,
                         path_to_shortname_map="./static/item_shortquestion_map.csv",
                         path_to_replacement_map="./static/item_replacement_map.csv") {
   q <- read_json(path_to_qsf)
+  wave <- get_wave(path_to_qsf)
   
   # get the survey elements with flow logic (should be one per block randomization branch)
   ii_flow <- q$SurveyElements %>%
@@ -68,6 +69,10 @@ process_qsf <- function(path_to_qsf,
   
   # B13 was originally named incorrectly. Rename manually as needed
   items[items == "B13 "] <- "B13"
+  # V2a in Wave 13 was originally named incorrectly. Rename manually as needed
+  if (wave == 13) {
+    items[items == "V2a"] <- "V2d"
+  }
   
   # get the text of the question:
   questions <- displayed_questions %>% 
@@ -289,7 +294,7 @@ process_qsf <- function(path_to_qsf,
     mutate(replaces = ifelse(variable %in% rownames(replaces_map), 
                              replaces_map[variable, "old_item"], 
                              NA_character_),
-           wave = get_wave(path_to_qsf)
+           wave = wave
     ) %>% 
     select(wave,
            variable,

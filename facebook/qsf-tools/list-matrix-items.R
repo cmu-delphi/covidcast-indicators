6adb7d3fbe962c846a49c215b1a10351e78ba12f
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-03-15 18:40:14 -0400

    add new tool to print matrix items + refactoring

---
+++ facebook/qsf-tools/list-matrix-items.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+## Print a list of survey questions that we handle as matrices.
+##
+## Usage:
+##
+## Rscript list-matrix-items.R [UMD/CMU] path/to/qsf
+
+suppressPackageStartupMessages({
+  library(jsonlite)
+  library(tidyverse)
+  source("qsf-utils.R")
+})
+
+
+print_matrix_items <- function(path_to_qsf, survey_version=c("CMU", "UMD")) {
+  survey_version <- match.arg(survey_version)
+  q <- read_json(path_to_qsf)
+  wave <- get_wave(path_to_qsf)
+  
+  displayed_questions <- subset_qsf_to_displayed(q)
+  
+  # Get survey item names
+  item_names <- displayed_questions %>% 
+    map_chr(~ .x$Payload$DataExportTag) %>%
+    patch_item_names(item_names, survey_version, wave)
+  
+  # Get survey item formats
+  qtype <- get_question_formats(displayed_questions, item_names, survey_version)
+  
+  qdf <- tibble(variable = item_names,
+                type = qtype)
+  
+  print(qdf %>% filter(type == "Matrix") %>% pull(variable))
+  
+  return(NULL)
+}
+
+args <- commandArgs(TRUE)
+
+if (length(args) != 2) {
+  stop("Usage: Rscript list-matrix-items.R [UMD/CMU] path/to/qsf")
+}
+
+survey_version <- args[1]
+path_to_qsf <- args[2]
+
+invisible(print_matrix_items(path_to_qsf, survey_version))

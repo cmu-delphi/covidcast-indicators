9f97a3987c86b6f2cfc461c8de534284026ac87e
QX Teo <37101453+qx-teo@users.noreply.github.com>
2022-04-04 02:37:10 -0400

    Change query method
    Removed brand_id filtering
    Adding more logging statements for clarity
    Changed how df and cursor is found, since the query has changed
    
    Added new comparison method for cursors:
    Similar to python's <= method for strings, but considering numbers as larger than alphabets (Which is how cursors are sorted)

--- safegraph_patterns/delphi_safegraph_patterns/pull.py
+++ safegraph_patterns/delphi_safegraph_patterns/pull.py
@@ -42,29 +42,31 @@ def pull_one(query, headers):
             data=json.dumps(query))
         try:
             response = req.json()
-            df = pd.read_json(
-                json.dumps(response["data"]["bulk_weekly_patterns"])
-            )
+            dump = json.dumps(response["data"]["search"]["weekly_patterns"]["results"]["edges"])
+            dump = json.loads(dump)
+            nodes = [i['node'] for i in dump]
+            cursor = dump[-1]['cursor']
+            df = pd.DataFrame(nodes)
             first_placekey = df.placekey[0]
             n = response["extensions"]["row_count"]
             last_placekey = df.placekey[n-1]
-            df = df[df["safegraph_brand_ids"].isin(brand_ids)]
             break
         except json.decoder.JSONDecodeError as e:
-            req.raise_for_status()
             print(f"Expected JSON; got:\n'''{req.text}'''")
             time.sleep(1)
             success = not success
         except (KeyError,AttributeError) as e:
-            print(df)
-            print(req.text)
-            raise
-    print(f"{query['variables']['last_seen_placekey']}: [{len(df)} of {n}] {first_placekey} -- {last_placekey}")
-    return response, df, (last_placekey if n == BATCH else None)
+            print(f"KeyError or AttributeError; got: {e}\n'''{req.text}'''")
+            time.sleep(1)
             success = not success
+        except BaseException as e: # pylint: disable=W0703
+            print(f"Unexpected {e}, {type(e)}")
+            time.sleep(1)
             success = not success
     if not success:
         raise RuntimeError
+    print(f"{n} records: {first_placekey} -- {last_placekey}")
+    return response, df, (cursor if n == BATCH else None), last_placekey
 
 def make_request(params, day, naics_code):
     """Make a request in Places API."""
@@ -100,11 +102,12 @@ CURSOR_SIGNPOSTS = {
 
 def pull(params, day, naics_code):
     """Make queries in Places API, using multiprocessing."""
+    query, headers = make_request(params, day, naics_code)
+    job_pull = partial(pull_until, query=query, headers=headers)
     job_args = list(zip(
         CURSOR_SIGNPOSTS[naics_code][:-1],
         CURSOR_SIGNPOSTS[naics_code][1:]
     ))
-    print(f"{len(job_args)}: {job_args}")
     with mp.Pool(len(job_args)) as pool:
         all_out = pool.map(job_pull, job_args)
     out_dict = dict(all_out)
@@ -112,12 +115,14 @@ def pull(params, day, naics_code):
     # second signpost, and so on
     dfs = out_dict[job_args[0]]
     for i in range(1, len(job_args)):
-        dfs[-1] = pd.concat([dfs[-1], out_dict[job_args[i]][0]]).drop_duplicates()
+        dfs[-1] = pd.concat([dfs[-1], out_dict[job_args[i]][0]])\
+            .drop_duplicates(subset = ['placekey'])
         dfs.extend(out_dict[job_args[i]][1:])
     return pd.concat(dfs)
 
 def pull_until(cursor_bookends, query, headers):
     """Make queries in Places API in sets of 500, until finish."""
+    curtime = time.time()
     start_cursor, end_cursor = cursor_bookends
     query = json.loads(json.dumps(query)) # make a copy before editing
     query["variables"]["last_cursor"] = start_cursor
@@ -136,5 +141,24 @@ def pull_until(cursor_bookends, query, headers):
             break
         query["variables"]["last_cursor"] = last_cursor
         time.sleep(0.1)
+    print(f"Finished multiprocessing job for {cursor_bookends},\
+        duration {(time.time() - curtime)} seconds")
     return cursor_bookends, dfs
 
+def num_first_comp(x, y):
+    """Return true if Y >= X, Numbers are larger than alphabets."""
+    comp_len = min(len(x), len(y))
+    for i in range(comp_len):
+        xi = x[i]
+        yi = y[i]
+        if xi == yi:
+            continue
+        # Compare Digits first
+        if xi.isdigit() and yi.isdigit():
+            return int(yi) > int(xi)
+        if xi.isdigit() and not yi.isdigit():
+            return False
+        if not xi.isdigit() and yi.isdigit():
+            return True
+        return yi >= xi
+    return False

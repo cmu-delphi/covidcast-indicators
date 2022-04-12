0e41397b49d5fcde1dbd640095835cc6cdf4fca3
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-04-12 11:40:09 -0400

    add exp group labels; modify output name

--- facebook/delphiFacebook/R/individual.R
+++ facebook/delphiFacebook/R/individual.R
@@ -9,10 +9,15 @@
 #'     and "end_time" used in naming the output file
 #'
 #' @importFrom dplyr select rename
+#' @importFrom readr read_csv cols col_character
 #' @importFrom rlang .data
 #' @export
 write_individual <- function(data_full_w, params)
 {
+  # Has columns "cid", "date", and "test_group"
+  experimental_groups <- read_csv("cmu_cid_qp_user_group_mapping_20220408.csv", col_types = cols(.default = col_character()))
+  data_full_w <- data_full_w %>% left_join(experimental_groups, by=c("token"="cid", "Date"="date"))
+
   data_to_write <- select(data_full_w, -.data$token)
   data_to_write <- rename(data_to_write, fips = .data$geo_id)
   data_to_write <- dplyr::filter(data_to_write, !is.na(.data$weight))
@@ -23,7 +28,7 @@ write_individual <- function(data_full_w, params)
   if (!is.null(params$produce_individual_raceeth) && params$produce_individual_raceeth) {
     create_dir_not_exist(params$individual_raceeth_dir)
     for (ii in seq_along(dates)) {
-      write_individual_day(data_to_write, params, dates[ii], params$individual_raceeth_dir)
+      write_individual_day(data_to_write, params, dates[ii], params$individual_raceeth_dir, raceeth_version = "_raceeth")
     }
     
     data_to_write <- data_to_write %>% select(-raceethnicity)
@@ -50,11 +55,11 @@ write_individual <- function(data_full_w, params)
 #' @importFrom readr write_csv
 #' @importFrom dplyr select
 #' @importFrom rlang .data
-write_individual_day <- function(data_to_write, params, date, out_dir) {
+write_individual_day <- function(data_to_write, params, date, out_dir, raceeth_version = "") {
   fname_out <- sprintf(
-    "cvid_responses_%s_recordedby_%s.csv",
+    "cvid_responses_%s_experimental_groups%s.csv",
     format(date, "%Y_%m_%d"),
-    format(as.Date(params$end_date), "%Y_%m_%d")
+    raceeth_version
   )
   
   day_data <- dplyr::filter(data_to_write, .data$Date == date)

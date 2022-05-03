419303848954c36897c660f7a38889b36e56f99b
Brian Clark <clark.bg@gmail.com>
2022-05-03 09:39:33 -0400

    Remove unused lines

--- claims_hosp/HospClaims/automate/agg_claims_drops.py
+++ claims_hosp/HospClaims/automate/agg_claims_drops.py
@@ -63,47 +63,6 @@ def agg_and_write(data_path, force=True):
         dfs.to_csv(out_path, index=False)
         print(f"Wrote {out_path}")
 
-    # matches = defaultdict(list)
-    # for i, f in enumerate(files):
-    #     drop_datetime = f.name.split("_")[3:]
-    #     if len(drop_datetime) > 2:  # there is a group number
-    #         group, date, time = drop_datetime
-    #         dateid = date + time
-    #         matches[dateid].append(i)
-    #
-    # for match, file_idxs in matches.items():
-    #     # check if file exists before writing
-    #     out_name = files[file_idxs][0].name.split("_")
-    #     out_name = '_'.join(out_name[:3] + out_name[4:])
-    #     out_path = files[file_idxs][0].parents[0] / out_name
-    #     if out_path.exists() and not force:
-    #         # print(f"{out_path} exists, skipping")
-    #         continue
-    #
-    #     dfs = [pd.read_csv(files[i], dtype={"PatCountyFIPS": str,
-    #                                         "patCountyFIPS": str}) for i in file_idxs]
-    #     n_rows = [a.shape[0] for a in dfs]
-    #     dfs = pd.concat(dfs)
-    #     if "servicedate" in dfs.columns:
-    #         dfs.rename(columns={"servicedate": "ServiceDate"}, inplace=True)
-    #     if "patCountyFIPS" in dfs.columns:
-    #         dfs.rename(columns={"patCountyFIPS": "PatCountyFIPS"}, inplace=True)
-    #     if "patHRRname" in dfs.columns:
-    #         dfs.rename(columns={"patHRRname": "Pat HRR Name"}, inplace=True)
-    #     if "patAgeGroup" in dfs.columns:
-    #         dfs.rename(columns={"patAgeGroup": "PatAgeGroup"}, inplace=True)
-    #     if "patHRRid" in dfs.columns:
-    #         dfs.rename(columns={"patHRRid": "Pat HRR ID"}, inplace=True)
-    #
-    #     assert np.sum(
-    #         dfs.duplicated(subset=["ServiceDate", "PatCountyFIPS",
-    #                                "Pat HRR Name", "PatAgeGroup"])) == 0, \
-    #         "Duplication across drops!"
-    #     assert dfs.shape[1] == 10, "Wrong number of columns"
-    #     assert sum(n_rows) == dfs.shape[0], "Sum of rows is incorrect"
-    #
-    #     safe_out(out_path, dfs, force)
-
 
 @click.command()
 @click.argument('data_path')

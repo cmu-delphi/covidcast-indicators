0c4daaf15782eeeb0dadcd22c4c32643030597f3
Jingjing Tang <jingjingtang@Jingjings-MacBook-Pro.local>
2022-08-03 23:35:13 -0400

    update code for backfill

--- quidel_covidtest/delphi_quidel_covidtest/pull.py
+++ quidel_covidtest/delphi_quidel_covidtest/pull.py
@@ -2,6 +2,7 @@
 """Collect and process Quidel export files."""
 from os.path import join
 import os
+import glob
 from datetime import datetime, timedelta
 import boto3
 
@@ -384,15 +385,16 @@ def store_backfill_file(df, _end_date, backfill_dir):
     Parameter:
         df: pd.DataFrame
             Pre-process file at ZipCode level
-        _end_date:
+        _end_date: datetime
             The most recent date when the raw data is received
-        backfill_dir:
+        backfill_dir: str
             specified path to store backfill files.
     """
     backfilldata = df.copy()
     backfilldata = gmpr.replace_geocode(backfilldata, from_code="zip", new_code="fips",
                           from_col="zip", new_col="fips", date_col="timestamp")
     backfilldata.rename({"timestamp": "time_value"}, axis=1, inplace=True)
+    #Store one year's backfill data
     _start_date = _end_date.replace(year=_end_date.year-1)
     selected_columns = ['time_value', 'fips',
                         'totalTest_total', 'positiveTest_total',
@@ -405,6 +407,63 @@ def store_backfill_file(df, _end_date, backfill_dir):
     backfilldata = backfilldata.loc[backfilldata["time_value"] >= _start_date,
                                     selected_columns]
     path = backfill_dir + \
-        "/quidel_covidtest_as_of_%s"%datetime.strftime(_end_date, "%Y%m%d") + ".parquet"
+        "/quidel_covidtest_as_of_%s.parquet"%datetime.strftime(_end_date, "%Y%m%d")
     # Store intermediate file into the backfill folder
     backfilldata.to_parquet(path)
+
+def merge_backfill_file(backfill_dir, backfill_merge_day, today,
+                        test_mode=False, check_nd=25):
+    """
+    Merge ~4 weeks' backfill data into one file.
+
+    Usually this function should merge 28 days' data into a new file so as to
+    save the reading time when running the backfill pipelines. We
+
+    Parameters
+    ----------
+    _end_date : datetime
+        The most recent date when the raw data is received
+    backfill_dir : str
+        specified path to store backfill files.
+    backfill_merge_day: int
+        The day of a week that we used to merge the backfill files. e.g. 0
+        is Monday.
+    test_mode: bool
+    check_nd: int
+        The criteria of the number of unmerged files. Ideally, we want the
+        number to be 28, but we use a looser criteria from practical
+        considerations
+    """
+    new_files = glob.glob(backfill_dir + "/quidel_covidtest_as_of_*")
+
+    def get_date(file_link):
+        # Keep the function here consistent with the backfill path in
+        # function `store_backfill_file`
+        fn = file_link.split("/")[-1].split(".parquet")[0].split("_")[-1]
+        return datetime.strptime(fn, "%Y%m%d")
+
+    date_list = list(map(get_date, new_files))
+    earliest_date = min(date_list)
+    latest_date = max(date_list)
+
+    # Check whether to merge
+    # Check the number of files that are not merged
+    if today.weekday() != backfill_merge_day or (today-earliest_date).days <= check_nd:
+        return
+
+    # Start to merge files
+    pdList = []
+    for fn in new_files:
+        df = pd.read_parquet(fn, engine='pyarrow')
+        pdList.append(df)
+    merged_file = pd.concat(pdList).sort_values(["time_value", "fips"])
+    path = backfill_dir + "/quidel_covidtest_from_%s_to_%s.parquet"%(
+        datetime.strftime(earliest_date, "%Y%m%d"),
+        datetime.strftime(latest_date, "%Y%m%d"))
+    merged_file.to_parquet(path)
+
+    # Delete daily files once we have the merged one.
+    if not test_mode:
+        for fn in new_files:
+            os.remove(fn)
+    return

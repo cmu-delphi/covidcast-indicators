2a048795026d5a5f2c8240aceba9a138a1bd455f
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-08-31 14:38:34 -0400

    add arg parsing to main run script
    Added flags for training models and for making predictions/corrections
    using the models. Both features are off (FALSE) by default.

--- Backfill_Correction/run.R
+++ Backfill_Correction/run.R
@@ -1,5 +1,26 @@
-library(delphiBackfillCorrection)
+#!/usr/bin/env Rscript
+
+## Run backfill corrections pipeline.
+##
+## Usage:
+##
+## Rscript run.R [options]
+
+suppressPackageStartupMessages({
+  library(delphiBackfillCorrection)
+  library(argparser)
+})
+
+
+parser <- arg_parser(description='Run backfill corrections pipeline')
+# Default to FALSE if not specified.
+parser <- add_argument(parser, arg="--train_models", flag=TRUE)
+parser <- add_argument(parser, arg="--make_predictions", flag=TRUE)
+args = parse_args(parser)
 
 params <- read_params("params.json")
+params$train_models <- args.train_models
+params$make_predictions <- args.make_predictions
+
 delphiBackfillCorrection::main(params)
 message("backfill correction completed successfully")

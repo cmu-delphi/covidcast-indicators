814e75432dc01d9911225ed455eeed3969e09984
Jingjing Tang <31444565+jingjtang@users.noreply.github.com>
2022-09-23 20:31:45 -0400

    fix a typo

--- Backfill_Correction/delphiBackfillCorrection/unit-tests/testthat/test-preprocessing.R
+++ Backfill_Correction/delphiBackfillCorrection/unit-tests/testthat/test-preprocessing.R
@@ -85,7 +85,7 @@ test_that("testing the calculation of week of a month", {
   expect_equal(get_weekofmonth(as.Date("2021-12-31")), 1)
   expect_equal(get_weekofmonth(as.Date("2022-01-01")), 1)
   expect_equal(get_weekofmonth(as.Date("2022-01-02")), 1)
-  expect_equal(get_weekofmonth(as.Date("2022-01-09")), 1)
+  expect_equal(get_weekofmonth(as.Date("2022-01-09")), 2)
   
   expect_equal(get_weekofmonth(as.Date("2022-09-01")), 1)
   expect_equal(get_weekofmonth(as.Date("2022-09-04")), 2)

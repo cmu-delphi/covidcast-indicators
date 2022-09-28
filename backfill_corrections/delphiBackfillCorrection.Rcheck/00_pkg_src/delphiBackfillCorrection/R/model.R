46fdfd93bbb0f9090fc568139e93a3c1a5709831
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-09-28 13:14:18 -0400

    drop uppercase from dir name

---
+++ backfill_corrections/delphiBackfillCorrection.Rcheck/00_pkg_src/delphiBackfillCorrection/R/model.R
@@ -0,0 +1,259 @@
+#' Filtration for training and testing data with different lags
+#' 
+#' @template test_lag-template
+#' @param lag_pad lag padding for training
+#' @param geo_train_data training data for a certain location
+#' @param geo_test_data testing data for a certain location
+#' 
+#' @importFrom rlang .data .env
+#'
+#' @export
+data_filteration <- function(test_lag, geo_train_data, geo_test_data, lag_pad) {
+  if (test_lag <= 14){
+    test_lag_pad=lag_pad
+    test_lag_pad1=0
+    test_lag_pad2=0
+  }else if (test_lag < 51){
+    test_lag_pad=7
+    test_lag_pad1=6
+    test_lag_pad2=7
+  }else {
+    test_lag_pad=9
+    test_lag_pad1=8
+    test_lag_pad2=9
+  }
+  train_data = geo_train_data %>% 
+    filter(.data$lag >= .env$test_lag - .env$test_lag_pad ) %>%
+    filter(.data$lag <= .env$test_lag + .env$test_lag_pad )
+  test_data = geo_test_data %>%
+    filter(.data$lag >= .env$test_lag - .env$test_lag_pad1 ) %>%
+    filter(.data$lag <= .env$test_lag + .env$test_lag_pad2)
+
+  return (list(train_data, test_data))
+}
+
+#' Add columns to indicate the scale of value at square root level
+#' 
+#' @template train_data-template
+#' @param test_data Data Frame for testing
+#' @param max_raw the maximum value in the training data at square root level
+#' @template value_col-template
+#' 
+#' @export
+add_sqrtscale<- function(train_data, test_data, max_raw, value_col) {
+  if (!(value_col %in% colnames(train_data))){
+    stop("value raw does not exist in training data!")
+  }
+  
+  if (!(value_col %in% colnames(test_data))){
+    stop("value raw does not exist in testing data!")
+  }
+  
+  sqrtscale = c()
+  sub_max_raw = sqrt(max(train_data[[value_col]])) / 2
+  
+  for (split in seq(0, 3)){
+    if (sub_max_raw < (max_raw * (split+1) * 0.1)) break
+    train_data[paste0("sqrty", as.character(split))] = 0
+    test_data[paste0("sqrty", as.character(split))] = 0
+    qv_pre = max_raw * split * 0.2
+    qv_next = max_raw * (split+1) * 0.2
+
+    train_data[(train_data[[value_col]] <= (qv_next)^2)
+               & (train_data[[value_col]] > (qv_pre)^2), 
+               paste0("sqrty", as.character(split))] = 1
+    test_data[(test_data[[value_col]] <= (qv_next)^2)
+              & (test_data[[value_col]] > (qv_pre)^2), 
+              paste0("sqrty", as.character(split))] = 1
+    sqrtscale[split+1] = paste0("sqrty", as.character(split))
+  }
+  return (list(train_data, test_data, sqrtscale))
+}
+
+#' Fetch model and use to generate predictions/perform corrections
+#'
+#' @template train_data-template
+#' @param test_data Data frame for testing 
+#' @template taus-template
+#' @template covariates-template
+#' @template lp_solver-template
+#' @template lambda-template
+#' @template geo_level-template
+#' @template geo-template
+#' @template indicator-template
+#' @template signal-template
+#' @template signal_suffix-template
+#' @template value_type-template
+#' @template test_lag-template
+#' @template train_models-template
+#' @template make_predictions-template
+#' @param model_save_dir directory containing trained models
+#' @param training_end_date Most recent training date
+#'
+#' @importFrom stats predict coef
+#'
+#' @export
+model_training_and_testing <- function(train_data, test_data, taus, covariates,
+                                       lp_solver, lambda, test_lag,
+                                       geo, value_type, model_save_dir, 
+                                       indicator, signal, 
+                                       geo_level, signal_suffix,
+                                       training_end_date, 
+                                       train_models = TRUE,
+                                       make_predictions = TRUE) {
+  success = 0
+  coefs_result = list()
+  coef_list = c("intercept", paste(covariates, '_coef', sep=''))
+  for (tau in taus) {
+    tryCatch(
+      expr = {
+        model_file_name <- generate_filename(indicator, signal, 
+                                        geo_level, signal_suffix, lambda,
+                                        training_end_date, geo, 
+                                        value_type, test_lag, tau)
+        model_path <- file.path(model_save_dir, model_file_name)
+        obj <- get_model(model_path, train_data, covariates, tau,
+                         lambda, lp_solver, train_models=TRUE) 
+
+        if (make_predictions) {
+          y_hat_all = as.numeric(predict(obj, newx = as.matrix(test_data[covariates])))
+          test_data[[paste0("predicted_tau", as.character(tau))]] = y_hat_all
+
+          coefs_result[[success+1]] = coef(obj)
+        }
+
+        success = success + 1
+      },
+      error=function(e) {print(paste("Training failed for", model_path, sep=" "))}
+    )
+  }
+  if (success < 9) {return (NULL)}
+  if (!make_predictions) {return (list())}
+  
+  coef_combined_result = data.frame(tau=taus, geo=geo, test_lag=test_lag)
+  coef_combined_result[coef_list] = as.matrix(do.call(rbind, coefs_result))
+  
+  return (list(test_data, coef_combined_result))
+}
+
+#' Evaluation of the test results based on WIS score
+#' The WIS score calculation is based on the weighted_interval_score function 
+#' from the `evalcast` package from Delphi
+#' 
+#' @param test_data dataframe with a column containing the prediction results of
+#'    each requested quantile. Each row represents an update with certain
+#'    (reference_date, issue_date, location) combination.
+#' @template taus-template
+#' 
+#' @importFrom evalcast weighted_interval_score
+#' 
+#' @export
+evaluate <- function(test_data, taus) {
+  n_row = nrow(test_data)
+  taus_list = as.list(data.frame(matrix(replicate(n_row, taus), ncol=n_row)))
+  
+  # Calculate WIS
+  predicted_all = as.matrix(test_data[c("predicted_tau0.01", "predicted_tau0.025",
+                                        "predicted_tau0.1", "predicted_tau0.25",
+                                        "predicted_tau0.5", "predicted_tau0.75",
+                                        "predicted_tau0.9", "predicted_tau0.975",
+                                        "predicted_tau0.99")])
+  predicted_all_exp = exp(predicted_all)
+  predicted_trans = as.list(data.frame(t(predicted_all - test_data$log_value_target)))
+  test_data$wis = mapply(weighted_interval_score, taus_list, predicted_trans, 0)
+  
+  return (test_data)
+}
+
+#' Train model using quantile regression with Lasso penalty, or load from disk
+#'
+#' @param model_path path to read model from or to save model to
+#' @template train_data-template
+#' @template covariates-template
+#' @param tau decimal quantile to be predicted. Values must be between 0 and 1.
+#' @template lp_solver-template
+#' @template lambda-template
+#' @template train_models-template
+#'
+#' @importFrom quantgen quantile_lasso
+#' @importFrom stringr str_interp
+get_model <- function(model_path, train_data, covariates, tau,
+            lambda, lp_solver, train_models) {
+  if (train_models || !file.exists(model_path)) {
+    if (!train_models && !file.exists(model_path)) {
+      warning(str_interp("user requested use of cached model but file {model_path}"),
+        " does not exist; training new model")
+    }
+    # Quantile regression
+    obj <- quantile_lasso(as.matrix(train_data[covariates]),
+                         train_data$log_value_target, tau = tau,
+                         lambda = lambda, standardize = FALSE, lp_solver = lp_solver)
+
+    # Save model to cache.
+    create_dir_not_exist(dirname(model_path))
+    save(obj, file=model_path)
+  } else {
+    # Load model from cache.
+    obj <- load(model_path)
+  }
+
+  return(obj)
+}
+
+#' Construct filename for model with given parameters
+#'
+#' @template indicator-template
+#' @template signal-template
+#' @template geo-template
+#' @template signal_suffix-template
+#' @template lambda-template
+#' @template value_type-template
+#' @template test_lag-template
+#' @template geo_level-template
+#' @template test_lag-template
+#' @param dw string, indicate the day of a week
+#' @param tau decimal quantile to be predicted. Values must be between 0 and 1.
+#' @param beta_prior_mode bool, indicate whether it is for a beta prior model
+#' @param model_mode bool, indicate whether the file name is for a model
+#' @param training_end_date the most recent training date
+#'
+#' @return path to file containing model object
+#'
+#' @importFrom stringr str_interp
+#' 
+generate_filename <- function(indicator, signal, 
+                              geo_level, signal_suffix, lambda,
+                              training_end_date="", geo="", 
+                              value_type = "", test_lag="", tau="", dw="",
+                              beta_prior_mode = FALSE, model_mode = TRUE) {
+  if (lambda != "") {
+    lambda <- str_interp("lambda${lambda}")
+  }
+  if (test_lag != "") {
+    test_lag <- str_interp("lag${test_lag}")
+  }
+  if (tau != "") {
+    tau <- str_interp("tau${tau}")
+  }
+  if (beta_prior_mode) {
+    beta_prior <- "beta_prior"
+  } else {
+    beta_prior <- ""
+  }
+  if (model_mode) {
+    file_type <- ".model"
+  } else {
+    file_type <- ".csv"
+  }
+  components <- c(as.character(training_end_date), beta_prior,
+                  indicator, signal, signal_suffix,
+                  geo_level, lambda,
+                  geo, test_lag, dw, tau)
+  
+  filename = paste0(
+    # Drop any empty strings.
+    paste(components[components != ""], collapse="_"),
+    file_type
+  )
+  return(filename)
+}
---
+++ backfill_corrections/delphiBackfillCorrection.Rcheck/00_pkg_src/delphiBackfillCorrection/R/tooling.R
@@ -0,0 +1,178 @@
+#' Corrected estimates from a single local signal
+#' 
+#' @template df-template
+#' @template export_dir-template
+#' @param test_date_list Date vector of dates to make predictions for
+#' @param value_cols character vector of numerator and/or denominator field names
+#' @template value_type-template
+#' @template taus-template
+#' @param test_lags integer vector of number of days ago to predict for
+#' @template training_days-template
+#' @template testing_window-template
+#' @template ref_lag-template
+#' @template lambda-template
+#' @template lp_solver-template
+#'
+#' @importFrom dplyr %>% filter
+#' @importFrom plyr rbind.fill
+#' @importFrom tidyr drop_na
+#' @importFrom rlang .data .env
+#'
+#' @export
+run_backfill_local <- function(df, export_dir, test_date_list, value_cols, value_type,
+                         taus = TAUS, test_lags = TEST_LAGS,
+                         training_days = TRAINING_DAYS, testing_window = TESTING_WINDOW,
+                         ref_lag = REF_LAG, lambda = LAMBDA, lp_solver = LP_SOLVER) {
+  # Get all the locations that are considered
+  geo_list <- unique(df[df$time_value %in% test_date_list, "geo_value"])
+  # Build model for each location
+  res_list = list()
+  res_indx = 1
+  coef_df_list = list()
+
+  for (geo in geo_list) {
+    subdf <- df %>% filter(.data$geo_value == .env$geo) %>% filter(.data$lag < .env$ref_lag)
+    min_refd <- min(subdf$time_value)
+    max_refd <- max(subdf$time_value)
+    subdf <- fill_rows(subdf, "time_value", "lag", min_refd, max_refd)
+    if (value_type == "count") { # For counts data only
+      combined_df <- fill_missing_updates(subdf, value_cols[1], "time_value", "lag")
+      combined_df <- add_7davs_and_target(combined_df, "value_raw", "time_value", "lag", ref_lag)
+    } else if (value_type == "fraction") {
+      combined_num_df <- fill_missing_updates(subdf, value_cols[1], "time_value", "lag")
+      combined_num_df <- add_7davs_and_target(combined_num_df, "value_raw", "time_value", "lag", ref_lag)
+          
+      combined_denom_df <- fill_missing_updates(subdf, value_cols[2], "time_value", "lag")
+      combined_denom_df <- add_7davs_and_target(combined_denom_df, "value_raw", "time_value", "lag", ref_lag)
+          
+      combined_df <- merge(combined_num_df, combined_denom_df,
+                           by=c("time_value", "issue_date", "lag", "target_date"), all.y=TRUE,
+                           suffixes=c("_num", "_denom"))
+    }
+    combined_df <- add_params_for_dates(combined_df, "time_value", "lag")
+    
+    for (test_date in test_date_list) {
+      geo_train_data = combined_df %>% 
+        filter(.data$issue_date < .env$test_date) %>%
+        filter(.data$target_date <= .env$test_date) %>%
+        filter(.data$target_date > .env$test_date - .env$training_days) %>%
+        drop_na()
+      geo_test_data = combined_df %>% 
+        filter(.data$issue_date >= .env$test_date) %>%
+        filter(.data$issue_date < .env$test_date + .env$testing_window) %>%
+        drop_na()
+      if (nrow(geo_test_data) == 0) next
+      if (nrow(geo_train_data) <= 200) next
+      if (value_type == "fraction") {
+        geo_prior_test_data = combined_df %>% 
+          filter(.data$issue_date > .env$test_date - 7) %>%
+          filter(.data$issue_date <= .env$test_date)
+            
+        updated_data <- ratio_adj(geo_train_data, geo_test_data, geo_prior_test_data)
+        geo_train_data <- updated_data[[1]]
+        geo_test_data <- updated_data[[2]]
+      }
+      
+      max_raw = sqrt(max(geo_train_data$value_raw))
+      for (test_lag in test_lags) {
+        filtered_data <- data_filteration(test_lag, geo_train_data, geo_test_data)
+        train_data <- filtered_data[[1]]
+        test_data <- filtered_data[[2]]
+            
+        updated_data <- add_sqrtscale(train_data, test_data, max_raw, "value_raw")
+        train_data <- updated_data[[1]]
+        test_data <- updated_data[[2]]
+        sqrtscale <- updated_data[[3]]
+            
+        covariates <- list(
+          Y7DAV, paste0(WEEKDAYS_ABBR, "_ref"), paste0(WEEKDAYS_ABBR, "_issue"),
+          WEEK_ISSUES, SLOPE, SQRTSCALE
+        )
+        params_list <- c(YITL, as.vector(unlist(covariates)))
+            
+        # Model training and testing
+        model_path_prefix <- generate_model_filename_prefix(
+          indicator, signal, geo, signal_suffix, value_type, test_lag, tau, lambda)
+        prediction_results <- model_training_and_testing(
+            train_data, test_data, taus, params_list, lp_solver,
+            lambda, test_date, geo, value_type = value_type, test_lag = test_lag
+        )
+        test_data <- prediction_results[[1]]
+        coefs <- prediction_results[[2]]
+        test_data <- evaluate(test_data, taus)
+        test_data$test_date <- test_date
+        coefs$test_date <- test_date
+        coefs$test_lag <- test_lag
+        coefs$geo_value <- geo
+        
+        res_list[[res_indx]] = test_data
+        coef_df_list[[res_indx]] = coefs
+        res_indx = res_indx+1
+        export_test_result(test_data, coefs, export_dir,
+                           geo, test_lag)
+      }# End for test lags
+    }# End for test date list
+    result_df = do.call(rbind, res_list)
+    coefs_df = do.call(rbind.fill, coef_df_list)
+    export_test_result(result_df, coefs_df, export_dir, geo)
+  }# End for geo list
+}
+
+#' Main function to correct a single local signal
+#'
+#' @template input_dir-template
+#' @template export_dir-template
+#' @param test_start_date Date or string in the format "YYYY-MM-DD" to start
+#'     making predictions on
+#' @param test_end_date Date or string in the format "YYYY-MM-DD" to stop
+#'     making predictions on
+#' @template num_col-template
+#' @template denom_col-template
+#' @template value_type-template
+#' @template training_days-template
+#' @template testing_window-template
+#' @template lambda-template
+#' @template ref_lag-template
+#' @template lp_solver-template
+#'
+#' @importFrom readr read_csv
+#' 
+#' @export
+main_local <- function(input_dir, export_dir,
+                 test_start_date, test_end_date,
+                 num_col, denom_col,value_type = c("count", "fraction"),
+                 training_days = TRAINING_DAYS, testing_window = TESTING_WINDOW,
+                 lambda = LAMBDA, ref_lag = REF_LAG, lp_solver = LP_SOLVER) {
+  value_type <- match.arg(value_type)
+
+  # Check input data
+  df = read_csv(input_dir)
+
+  # Check data type and required columns
+  result <- validity_checks(df, value_type, num_col, denom_col)
+  df <- result[["df"]]
+  value_cols <- result[["value_cols"]]
+  
+  # Get test date list according to the test start date
+  if (is.null(test_start_date)) {
+    test_start_date = max(df$issue_date)
+  } else {
+    test_start_date = as.Date(test_start_date)
+  }
+  
+  if (is.null(test_end_date)) {
+    test_end_date = max(df$issue_date)
+  } else {
+    test_end_date = as.Date(test_end_date)
+  }
+
+  test_date_list = seq(test_start_date, test_end_date, by="days")
+  
+  # Check available training days
+  training_days_check(df$issue_date, training_days)
+  
+  run_backfill_local(df, export_dir,
+               test_date_list, value_cols, value_type,
+               TAUS, TEST_LAGS, training_days, testing_window,
+               ref_lag, lambda, lp_solver)
+}

ba5875078ea44483e7ef50471037e058b20af59a
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-10-07 11:15:46 -0400

    flesh out docker_run.sh

--- backfill_corrections/docker_run.sh
+++ backfill_corrections/docker_run.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+workdir=$1
+shift
+
+DOCKER_IMAGE="ghcr.io/cmu-delphi/covidcast-indicators-backfill_corrections"
+DOCKER_TAG="latest"
+
+# Call like:
+# ./docker_run.sh "/home/indicators/runtime/backfill_corrections" "make pipeline"
 docker run \
     -v "${workdir}"/logs:/backfill_corrections/logs \
     -v "${workdir}"/receiving:/backfill_corrections/receiving \

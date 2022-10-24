5abfd152528bbf795c013b5061cf4fd7167b7438
Nat DeFries <42820733+nmdefries@users.noreply.github.com>
2022-10-24 13:19:54 -0400

    make run_backfill higher level. separate train and test procedures

--- backfill_corrections/delphiBackfillCorrection/R/main.R
+++ backfill_corrections/delphiBackfillCorrection/R/main.R
@@ -9,213 +9,62 @@
 #' @template indicator-template
 #' @template signal-template
 #' 
-#' @importFrom dplyr %>% filter select group_by summarize across everything group_split ungroup
-#' @importFrom tidyr drop_na
+#' @importFrom tidyr crossing
+#' @importFrom stats setNames
+#' @importFrom dplyr bind_rows filter %>%
 #' @importFrom rlang .data .env
-#' 
-#' @export
 run_backfill <- function(df, params,
                          refd_col = "time_value", lag_col = "lag", issued_col = "issue_date",
                          signal_suffixes = c(""), indicator = "", signal = "") {
-  df <- filter(df, .data$lag < params$ref_lag + 30) # a rough filtration to save memory
-
-  geo_levels <- params$geo_levels
-  if ("state" %in% geo_levels) {
-    # If state included, do it last since state processing modifies the
-    # `df` object.
-    geo_levels <- c(setdiff(geo_levels, c("state")), "state")
-  }
-  
-  for (geo_level in geo_levels) {
-    msg_ts(str_interp("geo level ${geo_level}"))
-    # Get full list of interested locations
-    if (geo_level == "state") {
-      # Drop county field and make new "geo_value" field from "state_id".        
-      # Aggregate counties up to state level
-      agg_cols <- c("geo_value", issued_col, refd_col, lag_col)
-      # Sum all non-agg columns. Summarized columns keep original names
-      df <- df %>%
-        select(-.data$geo_value, geo_value = .data$state_id) %>%
-        group_by(across(agg_cols)) %>%
-        summarize(across(everything(), sum)) %>%
-        ungroup()
-    }
-    if (geo_level == "county") {
-      # Keep only 200 most populous (within the US) counties
-      top_200_geos <- get_populous_counties()
-      df <- filter(df, .data$geo_value %in% top_200_geos)
-    }
-      
-    test_data_list <- list()
-    coef_list <- list()
-    
-    for (value_type in params$value_types) {
-      for (signal_suffix in signal_suffixes) {
-        key <- make_key(value_type, signal_suffix)
-        test_data_list[[key]] <- list()
-        coef_list[[key]] <- list()
-      }
-    }
-
-    msg_ts("Splitting data into geo groups")
-    group_dfs <- group_split(df, .data$geo_value)
-
-    # Build model for each location
-    for (subdf in group_dfs) {
-      geo <- subdf$geo_value[1]
-      msg_ts(str_interp("Processing ${geo} geo group"))
-
-      min_refd <- min(subdf[[refd_col]])
-      max_refd <- max(subdf[[refd_col]])
-      subdf <- fill_rows(
-        subdf, refd_col, lag_col, min_refd, max_refd, ref_lag = params$ref_lag
+  # Get all desired model names by combining input params. Need to get the
+  # list of geos from test_data; everything else is static.
+  model_combos <- list()
+  for (geo_level in params$geo_levels) {
+    model_combos[[geo_level]] <- as.data.frame(crossing(
+      geo = unique(test_data[[geo_level]]$geo_value),
+      signal_suffix = signal_suffixes,
+      value_type = params$value_types,
+      test_lag = params$test_lags,
+      tau = params$taus
+    )) %>%
+      mutate(
+        model_name = generate_filename(.env$indicator, .env$signal,
+                                  .env$geo_level, .data$signal_suffix, params$lambda,
+                                  training_end_date=.env$training_end_date, geo=.data$geo,
+                                  value_type = .data$value_type, test_lag=.data$test_lag,
+                                  tau=.data$tau, model_mode = TRUE),
+        in_cache = file.exists(file.path(params$cache_dir, .data$model_name)))
       )
-      
-      for (signal_suffix in signal_suffixes) {
-        # For each suffix listed in `signal_suffixes`, run training/testing
-        # process again. Main use case is for quidel which has overall and
-        # age-based signals.
-        if (signal_suffix != "") {
-          msg_ts(str_interp("signal suffix ${signal_suffix}"))
-          num_col <- paste(params$num_col, signal_suffix, sep = "_")
-          denom_col <- paste(params$denom_col, signal_suffix, sep = "_")
-        } else {
-          num_col <- params$num_col
-          denom_col <- params$denom_col
-        }
-        
-        for (value_type in params$value_types) {
-          msg_ts(str_interp("value type ${value_type}"))
-          # Handle different signal types
-          if (value_type == "count") { # For counts data only
-            combined_df <- fill_missing_updates(subdf, num_col, refd_col, lag_col)
-            combined_df <- add_7davs_and_target(
-              combined_df, "value_raw", refd_col, lag_col, ref_lag = params$ref_lag
-            )
-            
-          } else if (value_type == "fraction") {
-            combined_num_df <- fill_missing_updates(subdf, num_col, refd_col, lag_col)
-            combined_num_df <- add_7davs_and_target(
-              combined_num_df, "value_raw", refd_col, lag_col, ref_lag = params$ref_lag
-            )
-            
-            combined_denom_df <- fill_missing_updates(subdf, denom_col, refd_col, lag_col)
-            combined_denom_df <- add_7davs_and_target(
-              combined_denom_df, "value_raw", refd_col, lag_col, ref_lag = params$ref_lag
-            )
-            
-            combined_df <- merge(
-              combined_num_df, combined_denom_df,
-              by=c(refd_col, issued_col, lag_col, "target_date"), all.y=TRUE,
-              suffixes=c("_num", "_denom")
-            )
-          }
-          combined_df <- add_params_for_dates(combined_df, refd_col, lag_col)
-          combined_df <- combined_df %>% filter(.data$lag < params$ref_lag)
+  }
 
-          geo_train_data <- combined_df %>%
-            filter(.data$issue_date < params$training_end_date) %>%
-            filter(.data$target_date <= params$training_end_date) %>%
-            filter(.data$target_date > params$training_start_date) %>%
-            drop_na()
-          geo_test_data <- combined_df %>%
-            filter(.data$issue_date %in% params$test_dates) %>%
-            drop_na()
+  if (!params$train_models) {
+    for (geo_level in params$geo_levels) {
+      # Only keep missing models.
+      model_combos <- filter(model_combos[[geo_level]], !.data$in_cache)
+    }
+  }
 
-          if (nrow(geo_test_data) == 0) next
-          if (nrow(geo_train_data) <= 200) next
+  if (nrow(bind_rows(model_combos)) > 0) {
+    ## TODO: Get training data
+    # Include filter in data fetch:
+    # df <- filter(df_list[[geo_level]], .data$lag < params$ref_lag + 30) # a rough filtration to save memory
+    train_data <- make_geo_level_dfs(train_data, params, issued_col, refd_col, lag_col)
+    train_data <- split_by_geo_and_fill(train_data, params, refd_col, lag_col)
+    train_models(train_data, params, model_combos,
+                 issued_col = issued_col, signal_suffixes = signal_suffixes,
+                 indicator = indicator, signal = signal)
+  }
 
-          if (value_type == "fraction") {
-            # Use beta prior approach to adjust fractions
-            geo_prior_test_data = combined_df %>%
-              filter(.data$issue_date > min(params$test_dates) - 7) %>%
-              filter(.data$issue_date <= max(params$test_dates))
-            updated_data <- frac_adj(geo_train_data, geo_test_data, geo_prior_test_data,
-                                     indicator = indicator, signal = signal,
-                                     geo_level = geo_level, signal_suffix = signal_suffix,
-                                     lambda = params$lambda, value_type = value_type, geo = geo,
-                                     training_end_date = params$training_end_date,
-                                     model_save_dir = params$cache_dir,
-                                     taus = params$taus,
-                                     lp_solver = params$lp_solver,
-                                     train_models = params$train_models,
-                                     make_predictions = params$make_predictions)
-            geo_train_data <- updated_data[[1]]
-            geo_test_data <- updated_data[[2]]
-          }
-          max_raw = sqrt(max(geo_train_data$value_raw))
-          for (test_lag in params$test_lags) {
-            msg_ts(str_interp("test lag ${test_lag}"))
-            filtered_data <- data_filteration(test_lag, geo_train_data, 
-                                              geo_test_data, params$lag_pad)
-            train_data <- filtered_data[[1]]
-            test_data <- filtered_data[[2]]
-
-            if (nrow(train_data) == 0 || nrow(test_data) == 0) {
-              msg_ts(str_interp(
-                "Not enough data to either train or test for test_lag ${test_lag}, skipping"
-              ))
-              next
-            }
-
-            updated_data <- add_sqrtscale(train_data, test_data, max_raw, "value_raw")
-            train_data <- updated_data[[1]]
-            test_data <- updated_data[[2]]
-            sqrtscale <- updated_data[[3]]
-
-            covariates <- list(
-              Y7DAV, paste0(WEEKDAYS_ABBR, "_issue"),
-              paste0(WEEKDAYS_ABBR, "_ref"), WEEK_ISSUES, SLOPE, sqrtscale
-            )
-            params_list <- c(YITL, as.vector(unlist(covariates)))
-
-            # Model training and testing
-            msg_ts("Training or loading models")
-            prediction_results <- model_training_and_testing(
-              train_data, test_data, taus = params$taus, covariates = params_list,
-              lp_solver = params$lp_solver,
-              lambda = params$lambda, test_lag = test_lag, geo = geo,
-              value_type = value_type, model_save_dir = params$cache_dir,
-              indicator = indicator, signal = signal, geo_level = geo_level,
-              signal_suffix =signal_suffix, training_end_date = params$training_end_date,
-              train_models = params$train_models,
-              make_predictions = params$make_predictions
-            )
-
-            # Model objects are saved during training, so only need to export
-            # output if making predictions/corrections
-            if (params$make_predictions) {
-              msg_ts("Generating predictions")
-              test_data <- prediction_results[[1]]
-              coefs <- prediction_results[[2]]
-              test_data <- evaluate(test_data, params$taus)
-              
-              key <- make_key(value_type, signal_suffix)
-              idx <- length(test_data_list[[key]]) + 1
-              test_data_list[[key]][[idx]] <- test_data
-              coef_list[[key]][[idx]] <- coefs
-            }
-          }# End for test lags
-        }# End for value types
-      }# End for signal suffixes
-      
-      if (params$make_predictions) {
-        for (value_type in params$value_types) {
-          for (signal_suffix in signal_suffixes) {
-            key <- make_key(value_type, signal_suffix)
-            test_combined <- bind_rows(test_data_list[[key]]) 
-            coef_combined <- bind_rows(coef_list[[key]]) 
-            export_test_result(test_combined, coef_combined, 
-                               indicator, signal, 
-                               geo_level, geo, signal_suffix, params$lambda,
-                               params$training_end_date,
-                               value_type, export_dir=params$export_dir)
-          }
-        }
-      }
-      
-    }# End for geo list
-  }# End for geo type
+  if (params$make_predictions) {
+    ## TODO: Get test data
+    # Include filter in data fetch:
+    # df <- filter(df_list[[geo_level]], .data$lag < params$ref_lag + 30) # a rough filtration to save memory
+    test_data <- make_geo_level_dfs(test_data, params, issued_col, refd_col, lag_col)
+    test_data <- split_by_geo_and_fill(test_data, params, refd_col, lag_col)
+    make_predictions(test_data, params,
+                     issued_col = issued_col, signal_suffixes = signal_suffixes,
+                     indicator = indicator, signal = signal)
+  }
 }
 
 #' Perform backfill correction on all desired signals and geo levels

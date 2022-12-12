1bdd7c72354ae99f6627861091736f5de7046797
M5Skid <115434969+M5Skid@users.noreply.github.com>
2022-12-12 11:46:17 -0500

    Update runner.py - use context manager for file input
    Co-authored-by: Katie Mazaitis <krivard@cs.cmu.edu>

--- _delphi_utils_python/delphi_utils/runner.py
+++ _delphi_utils_python/delphi_utils/runner.py
@@ -46,12 +46,12 @@ def run_indicator_pipeline(indicator_fn:  Callable[[Params], None],
     ind_name = indicator_fn.__module__.replace(".run", "")
     #Check for version.cfg in indicator directory
     if os.path.exists("version.cfg"):
-        ver_file = open("version.cfg")
-        current_version = "not found"
-        for line in ver_file:
-            if "current_version" in line:
-                current_version = str.strip(line)
-                current_version = current_version.replace("current_version = ", "")
+        with open("version.cfg") as ver_file:
+            current_version = "not found"
+            for line in ver_file:
+                if "current_version" in line:
+                    current_version = str.strip(line)
+                    current_version = current_version.replace("current_version = ", "")
     #Logging - Starting Indicator    
         logger.info(f"Started {ind_name} with covidcast-indicators version {current_version}")
     else: logger.info(f"Started {ind_name} without version.cfg")

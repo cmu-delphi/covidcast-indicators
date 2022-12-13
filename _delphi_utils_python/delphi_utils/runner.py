0f3c53745e2e56e2f4a956bb25c054224ba83e73
M5Skid <115434969+M5Skid@users.noreply.github.com>
2022-12-13 16:39:34 -0500

    Remove spaces from runner.py to pass linter test

--- _delphi_utils_python/delphi_utils/runner.py
+++ _delphi_utils_python/delphi_utils/runner.py
@@ -41,7 +41,7 @@ def run_indicator_pipeline(indicator_fn:  Callable[[Params], None],
         name=indicator_fn.__module__,
         filename=params["common"].get("log_filename", None),
         log_exceptions=params["common"].get("log_exceptions", True))
-        
+
     #Get version and indicator name for startup
     ind_name = indicator_fn.__module__.replace(".run", "")
     #Check for version.cfg in indicator directory
@@ -52,7 +52,7 @@ def run_indicator_pipeline(indicator_fn:  Callable[[Params], None],
                 if "current_version" in line:
                     current_version = str.strip(line)
                     current_version = current_version.replace("current_version = ", "")
-    #Logging - Starting Indicator    
+    #Logging - Starting Indicator
         logger.info(f"Started {ind_name} with covidcast-indicators version {current_version}")
     else: logger.info(f"Started {ind_name} without version.cfg")
 

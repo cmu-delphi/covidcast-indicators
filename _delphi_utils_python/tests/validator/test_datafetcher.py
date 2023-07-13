7158ff9300308637d1fa4955423815a385fbce6a
minhkhul <118945681+minhkhul@users.noreply.github.com>
2023-07-13 13:20:30 -0400

    Update _delphi_utils_python/tests/validator/test_datafetcher.py
    Co-authored-by: melange396 <george.haff@gmail.com>

--- _delphi_utils_python/tests/validator/test_datafetcher.py
+++ _delphi_utils_python/tests/validator/test_datafetcher.py
@@ -43,7 +43,7 @@ class TestDataFetcher:
         if len(kwargs) == 0 or list(kwargs.keys())==["auth"]:
             return MockResponse([{'source': 'chng', 'db_source': 'chng'},
                 {'source': 'covid-act-now', 'db_source': 'covid-act-now'}], 200)
-        elif "params" in kwargs.keys() and kwargs["params"] == {'signal': 'chng:inactive'}:
+        elif "params" in kwargs and kwargs["params"] == {'signal': 'chng:inactive'}:
             return MockResponse([{"signals": [{"active": False}]}], 200)
         else:
             return MockResponse([{"signals": [{"active": True}]}], 200)

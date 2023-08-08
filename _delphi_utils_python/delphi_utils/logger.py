48e7734d477aae24cc16d30e7df5ad06c0b3f4c4
george haff <george.haff@gmail.com>
2023-08-08 14:58:01 -0400

    attempts to appease linter

--- _delphi_utils_python/delphi_utils/logger.py
+++ _delphi_utils_python/delphi_utils/logger.py
@@ -10,14 +10,15 @@ import os
 import structlog
 import sys
 import threading
-import traceback
+from traceback import format_exception
 
 
 def handle_exceptions(logger):
     """Handle exceptions using the provided logger."""
 
     def exception_handler(scope, etype, value, traceback):
-        logger.exception("Top-level exception occurred", scope=scope, exc_info=(etype, value, traceback))
+        logger.exception("Top-level exception occurred",
+                         scope=scope, exc_info=(etype, value, traceback))
 
     def sys_exception_handler(etype, value, traceback):
         exception_handler("sys", etype, value, traceback)
@@ -26,10 +27,13 @@ def handle_exceptions(logger):
         if args.exc_type == SystemExit and args.exc_value.code == 0:
             # `sys.exit(0)` is considered "successful termination":
             #   https://docs.python.org/3/library/sys.html#sys.exit
-            logger.debug(f"normal thread exit", thread=args.thread,
-                        stack="".join(traceback.format_exception(args.exc_type, args.exc_value, args.exc_traceback)))
+            logger.debug("normal thread exit", thread=args.thread,
+                        stack="".join(
+                            format_exception(
+                                args.exc_type, args.exc_value, args.exc_traceback)))
         else:
-            exception_handler(f"thread: {args.thread}", args.exc_type, args.exc_value, args.exc_traceback)
+            exception_handler(f"thread: {args.thread}",
+                              args.exc_type, args.exc_value, args.exc_traceback)
 
     sys.excepthook = sys_exception_handler
     threading.excepthook = threading_exception_handler
@@ -125,79 +129,94 @@ def get_structured_logger(name=__name__,
 
 
 
-# the above loggers are thread-safe but not multiprocessing-safe.  a `LoggerThread` will spawn a thread that listens to a mp.Queue and logs messages from it with the provided logger, so other processes can send logging messages to it via the logger-like `SubLogger` interface.  the SubLogger even logs the pid of the caller.  this is good to use with a set of jobs that are part of a mp.Pool, but isnt recommended for general use because of overhead from threading and multiprocessing, and because it might introduce lag to log messages.
 
-import multiprocessing
 import contextlib
+import multiprocessing
 
 class LoggerThread():
-  # TODO: add checks to prevent use of a stopped thread?
-  # TODO: reduce level of a bunch of debugging logs (search "self.logger.info")
+    """
+    the bare structlog loggers are thread-safe but not multiprocessing-safe.
+    a `LoggerThread` will spawn a thread that listens to a mp.Queue
+    and logs messages from it with the provided logger,
+    so other processes can send logging messages to it
+    via the logger-like `SubLogger` interface.
+    the SubLogger even logs the pid of the caller.
 
-  class SubLogger():
-    def __init__(self, queue):
-      self.queue = queue
-    def _log(self, level, *args, **kwargs):
-      kwargs_plus = {'sub_pid': multiprocessing.current_process().pid}
-      kwargs_plus.update(kwargs)
-      self.queue.put([level, args, kwargs_plus])
-    # TODO: add log levels beyond `info`
-    def info(self, *args, **kwargs):
-      self._log(logging.INFO, *args, **kwargs)
+    this is good to use with a set of jobs that are part of a mp.Pool,
+    but isnt recommended for general use
+    because of overhead from threading and multiprocessing,
+    and because it might introduce lag to log messages.
+    """
 
+    # TODO: add checks to prevent use of a stopped thread?
+    # TODO: reduce level of a bunch of debugging logs (search "self.logger.info")
 
-  def get_sublogger(self):
-    return self.sublogger
-
-  def __init__(self, logger, q=None):
-    self.logger = logger
-    if q:
-      self.msg_queue = q
-    else:
-      self.msg_queue = multiprocessing.Queue()
-
-    def logger_thread_worker():
-      self.logger.info('thread started')
-      while True:
-        msg = self.msg_queue.get()
-        if msg == 'STOP':
-          self.logger.info('received stop signal')
-          break
-        level, args, kwargs = msg
+    class SubLogger():
+        def __init__(self, queue):
+            self.queue = queue
+        def _log(self, level, *args, **kwargs):
+            kwargs_plus = {'sub_pid': multiprocessing.current_process().pid}
+            kwargs_plus.update(kwargs)
+            self.queue.put([level, args, kwargs_plus])
         # TODO: add log levels beyond `info`
-        if level == logging.INFO:
-          self.logger.info(*args, **kwargs)
+        def info(self, *args, **kwargs):
+            self._log(logging.INFO, *args, **kwargs)
+
+
+    def get_sublogger(self):
+        return self.sublogger
+
+    def __init__(self, logger, q=None):
+        self.logger = logger
+        if q:
+            self.msg_queue = q
         else:
-          self.logger.error('received unknown logging level!  exiting...')
-          break
-      self.logger.info('stopping thread')
+            self.msg_queue = multiprocessing.Queue()
 
-    self.thread = threading.Thread(target=logger_thread_worker, name="LoggerThread__"+self.logger.name)
-    self.logger.info('starting thread')
-    self.thread.start()
+        def logger_thread_worker():
+            self.logger.info('thread started')
+            while True:
+                msg = self.msg_queue.get()
+                if msg == 'STOP':
+                    self.logger.info('received stop signal')
+                    break
+                level, args, kwargs = msg
+                # TODO: add log levels beyond `info`
+                if level == logging.INFO:
+                    self.logger.info(*args, **kwargs)
+                else:
+                    self.logger.error('received unknown logging level!  exiting...')
+                    break
+            self.logger.info('stopping thread')
 
-    self.sublogger = LoggerThread.SubLogger(self.msg_queue)
+        self.thread = threading.Thread(target=logger_thread_worker,
+                                       name="LoggerThread__"+self.logger.name)
+        self.logger.info('starting thread')
+        self.thread.start()
 
-  def stop(self):
-    # TODO: make this safely re-callable?
-    self.logger.info('sending stop signal')
-    self.msg_queue.put('STOP')
-    self.thread.join()
-    self.logger.info('thread stopped')
+        self.sublogger = LoggerThread.SubLogger(self.msg_queue)
+
+    def stop(self):
+        # TODO: make this safely re-callable?
+        self.logger.info('sending stop signal')
+        self.msg_queue.put('STOP')
+        self.thread.join()
+        self.logger.info('thread stopped')
 
 
 
 @contextlib.contextmanager
 def pool_and_threadedlogger(logger, *poolargs):
-  """
-  emulates the multiprocessing.Pool() context manager, but also provides a logger that can be used by pool workers.
-  """
-  with multiprocessing.Manager() as manager:
-    logger_thread = LoggerThread(logger, manager.Queue())
-    try:
-      with multiprocessing.Pool(*poolargs) as pool:
-        yield pool, logger_thread.get_sublogger()
-        pool.close()
-        pool.join()
-    finally:
-      logger_thread.stop()
+    """
+    emulates the multiprocessing.Pool() context manager,
+    but also provides a logger that can be used by pool workers.
+    """
+    with multiprocessing.Manager() as manager:
+        logger_thread = LoggerThread(logger, manager.Queue())
+        try:
+            with multiprocessing.Pool(*poolargs) as pool:
+                yield pool, logger_thread.get_sublogger()
+                pool.close()
+                pool.join()
+        finally:
+            logger_thread.stop()

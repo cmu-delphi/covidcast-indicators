e5bbec51eaafd4040e82c737cebee99c7ad138b0
minhkhul <minhkhul@andrew.cmu.edu>
2024-01-24 21:50:00 -0500

    change hhs_hosp to using run_module and read_params

--- hhs_hosp/patch/patch.py
+++ hhs_hosp/patch/patch.py
@@ -2,10 +2,11 @@ from datetime import datetime, timedelta
 import json
 import subprocess
 from os import makedirs
+from delphi_utils import read_params
+from delphi_hhs.run import run_module
 
-with open('params.json', 'r') as file:
-        data = json.load(file)
 
+params = read_params()
 SOURCE = "hhs"
 export_dir = "25"
 makedirs(export_dir, exist_ok=True)
@@ -21,9 +22,9 @@ while current_date <= END_DATE:
     issue_dir = "issue_%s" % date_str
     makedirs(f"{export_dir}/{issue_dir}/{SOURCE}", exist_ok=True) #create issue & source dir
 
-    data['common']['epidata']['as_of'] = date_str
-    data['common']['export_dir'] = export_dir + "/" + issue_dir + "/" + SOURCE
-    with open('params.json', 'w') as file:
-        json.dump(data, file, indent=4)
-    subprocess.run("env/bin/python -m delphi_hhs", shell=True)
+    params['common']['epidata']['as_of'] = date_str
+    params['common']['export_dir'] = f"{export_dir}/{issue_dir}/{SOURCE}"
+
+    run_module(params)
+    print(f"completed run for issue_{issue_dir}")
     current_date += timedelta(days=1)

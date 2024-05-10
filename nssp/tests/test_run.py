adf5df49b9f0da9b32219c295a99045071e6d406
dsweber2 <david.weber2@pm.me>
2024-05-10 16:08:55 -0500

    drop unneeded nssp tests

--- nssp/tests/test_run.py
+++ nssp/tests/test_run.py
@@ -11,10 +11,7 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 from delphi_nssp.constants import GEOS, SIGNALS, CSV_COLS
 from delphi_nssp.run import (
-    add_needed_columns,
-    generate_weights,
-    sum_all_nan,
-    weighted_geo_sum,
+    add_needed_columns
 )
 
 
@@ -32,80 +29,3 @@ def test_add_needed_columns():
     ]
     assert df["se"].isnull().all()
     assert df["sample_size"].isnull().all()
-
-
-def test_sum_all_nan():
-    """Check that sum_all_nan returns NaN iff everything is a NaN"""
-    no_nans = np.array([3, 5])
-    assert sum_all_nan(no_nans) == 8
-    partial_nan = np.array([np.nan, 3, 5])
-    assert np.isclose(sum_all_nan([np.nan, 3, 5]), 8)
-
-    oops_all_nans = np.array([np.nan, np.nan])
-    assert np.isnan(oops_all_nans).all()
-
-
-def test_weight_generation():
-    dataFrame = pd.DataFrame(
-        {
-            "a": [1, 2, 3, 4, np.nan],
-            "b": [5, 6, 7, 8, 9],
-            "population": [10, 5, 8, 1, 3],
-        }
-    )
-    weighted = generate_weights(dataFrame, column_aggregating="a")
-    weighted_by_hand = pd.DataFrame(
-        {
-            "a": [1, 2, 3, 4, np.nan],
-            "b": [5, 6, 7, 8, 9],
-            "population": [10, 5, 8, 1, 3],
-            "relevant_pop_a": [10, 5, 8, 1, 0],
-            "weighted_a": [10.0, 2 * 5.0, 3 * 8, 4.0 * 1, np.nan * 0],
-        }
-    )
-    assert_frame_equal(weighted, weighted_by_hand)
-    # operations are in-place
-    assert_frame_equal(weighted, dataFrame)
-
-
-def test_weighted_state_sum():
-    dataFrame = pd.DataFrame(
-        {
-            "geo_id": [
-                "1",
-                "1",
-                "101",
-                "101",
-                "102",
-            ],
-            "timestamp": np.zeros(5),
-            "a": [1, 2, 3, 4, 12],
-            "b": [5, 6, 7, np.nan, np.nan],
-            "population": [10, 5, 8, 1, 3],
-        }
-    )
-    weighted = generate_weights(dataFrame, column_aggregating="b")
-    agg = weighted_geo_sum(weighted, "geo_id", "b")
-    expected_agg = pd.DataFrame(
-        {
-            "timestamp": np.zeros(3),
-            "geo_id": ["1", "101", "102"],
-            "relevant_pop_b": [10 + 5, 8 + 0, 0],
-            "weighted_b": [5 * 10 + 6 * 5, 7 * 8 + 0, np.nan],
-            "val": [80 / 15, 56 / 8, np.nan],
-        }
-    )
-    assert_frame_equal(agg, expected_agg)
-
-    weighted = generate_weights(dataFrame, column_aggregating="a")
-    agg_a = weighted_geo_sum(weighted, "geo_id", "a")
-    expected_agg_a = pd.DataFrame(
-        {
-            "timestamp": np.zeros(3),
-            "geo_id": ["1", "101", "102"],
-            "relevant_pop_a": [10 + 5, 8 + 1, 3],
-            "weighted_a": [1 * 10 + 2 * 5, 3 * 8 + 1 * 4, 12 * 3],
-            "val": [20 / 15, 28 / 9, 36 / 3],
-        }
-    )
-    assert_frame_equal(agg_a, expected_agg_a)

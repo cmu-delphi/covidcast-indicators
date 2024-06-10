ae6f011ad588c17aa698ae5baef2133a16df7b28
minhkhul <118945681+minhkhul@users.noreply.github.com>
2024-06-10 12:03:11 -0400

    nssp pipeline code (#1952)
    * to make nssp run in staging
    
    * add nssp to Jenkinsfile
    
    * nssp_token name change
    
    * et code
    
    * Update nssp/delphi_nssp/run.py
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    
    * Update nssp/README.md
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    
    * Update nssp/DETAILS.md
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    
    * Update nssp/delphi_nssp/__main__.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * Update nssp/delphi_nssp/pull.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * Update nssp/delphi_nssp/run.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * readme update
    
    * column names mapping + signals name standardization to fit with other available sources and signals"
    
    * improve readability
    
    * Add type_dict constant
    
    * more type_dict
    
    * add more unit test pull
    
    * data for unit test of pull
    
    * hrr + msa geos
    
    * use enumerate for clarity
    
    * set nssp sircal max_age to 13 days
    
    * set nssp sircal max_age to 15 days, to account for nighttime run
    
    * add validation to params
    
    * Update nssp/DETAILS.md
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * Update nssp/delphi_nssp/constants.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * et code
    
    * Update nssp/delphi_nssp/run.py
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    
    * Update nssp/README.md
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    
    * Update nssp/DETAILS.md
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    
    * Update nssp/delphi_nssp/__main__.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * Update nssp/delphi_nssp/pull.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * Update nssp/delphi_nssp/run.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * readme update
    
    * column names mapping + signals name standardization to fit with other available sources and signals"
    
    * improve readability
    
    * Add type_dict constant
    
    * more type_dict
    
    * add more unit test pull
    
    * data for unit test of pull
    
    * hrr + msa geos
    
    * use enumerate for clarity
    
    * to make nssp run in staging
    
    * add nssp to Jenkinsfile
    
    * nssp_token name change
    
    * set nssp sircal max_age to 15 days, to account for nighttime run
    
    * set nssp sircal max_age to 13 days
    
    * add validation to params
    
    * Update nssp/DETAILS.md
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * Update nssp/delphi_nssp/constants.py
    
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    
    * nssp correlation rmd and general notebook folder
    
    * making Black happy
    
    * update to new geomapper function
    
    * following 120 line convention everywhere
    
    * happy linter
    
    * happy black formatter in nssp
    
    * drop unneeded nssp tests
    
    * updates borked old tests, caught by @dshemetov
    
    * rebase woes and version consistency
    
    * Update nssp-params-prod.json.j2 min/max lag to 13
    
    * Update params.json.template min/max lag to 7 and 13
    
    * missed column renames for geo_mapper, unneeded index
    
    * lint+fix: update from linter changes
    * add make format to nssp Makefile
    * run make format on nssp
    
    * ci: update ci to lint nssp
    
    * lint: linter happy
    
    * lint: pydocstyle happy
    
    * lint: pydocstyle happy
    
    * pct_visits to pct_ed_visits
    
    ---------
    
    Co-authored-by: David Weber <david.weber2@pm.me>
    Co-authored-by: nmdefries <42820733+nmdefries@users.noreply.github.com>
    Co-authored-by: Dmitry Shemetov <dshemetov@ucdavis.edu>

---
+++ nssp/tests/test_run.py
@@ -0,0 +1,31 @@
+from datetime import datetime, date
+import json
+from unittest.mock import patch
+import tempfile
+import os
+import time
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from delphi_nssp.constants import GEOS, SIGNALS, CSV_COLS
+from delphi_nssp.run import (
+    add_needed_columns
+)
+
+
+def test_add_needed_columns():
+    df = pd.DataFrame({"geo_id": ["us"], "val": [1]})
+    df = add_needed_columns(df, col_names=None)
+    assert df.columns.tolist() == [
+        "geo_id",
+        "val",
+        "se",
+        "sample_size",
+        "missing_val",
+        "missing_se",
+        "missing_sample_size",
+    ]
+    assert df["se"].isnull().all()
+    assert df["sample_size"].isnull().all()

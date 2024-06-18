2d3f4fc0ca6378807a9f1239bb37222581ea374e
minhkhul <118945681+minhkhul@users.noreply.github.com>
2024-06-18 15:54:10 -0400

    linter

--- doctor_visits/delphi_doctor_visits/update_sensor.py
+++ doctor_visits/delphi_doctor_visits/update_sensor.py
@@ -11,11 +11,11 @@ Modified:
 # standard packages
 from datetime import timedelta
 from multiprocessing import Pool, cpu_count
+import cvxpy as cp
 
 # third party
 import numpy as np
 import pandas as pd
-import cvxpy as cp
 
 # first party
 from delphi_utils import Weekday
@@ -126,16 +126,20 @@ def update_sensor(
         (burn_in_dates >= startdate) & (burn_in_dates <= enddate))[0][:len(sensor_dates)]
 
     # handle if we need to adjust by weekday
-    params = Weekday.get_params(
-        data,
-        "Denominator",
-        Config.CLI_COLS + Config.FLU1_COL,
-        Config.DATE_COL,
-        [1, 1e5, 1e10, 1e15],
-        logger,
-        cp.CLARABEL, # switch to Clarabel to avoid numerical issues associated w ECOS
-    ) if weekday else None
-    if weekday and np.any(np.all(params == 0,axis=1)):
+    params = (
+        Weekday.get_params(
+            data,
+            "Denominator",
+            Config.CLI_COLS + Config.FLU1_COL,
+            Config.DATE_COL,
+            [1, 1e5, 1e10, 1e15],
+            logger,
+            cp.CLARABEL,  # switch to Clarabel to avoid numerical issues associated w ECOS
+        )
+        if weekday
+        else None
+    )
+    if weekday and np.any(np.all(params == 0, axis=1)):
         # Weekday correction failed for at least one count type
         return None
 

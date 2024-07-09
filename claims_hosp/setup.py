2229dd515866f085e16307c2269160d743c76eda
george haff <george.haff@gmail.com>
2024-07-09 19:15:39 -0400

    attempting to fix jenkins build

--- claims_hosp/setup.py
+++ claims_hosp/setup.py
@@ -14,6 +14,7 @@ required = [
     "pytest-cov",
     "pytest",
     "cvxpy<1.6",
+    "scs<3.2.6", # TODO: remove this ; it is a cvxpy dependency, and the excluded version appears to break our jenkins build. see: https://github.com/cvxgrp/scs/issues/283
 ]
 
 setup(
--- doctor_visits/setup.py
+++ doctor_visits/setup.py
@@ -12,7 +12,7 @@ required = [
     "pytest",
     "scikit-learn",
     "cvxpy>=1.5",
-]
+    "scs<3.2.6", # TODO: remove this ; it is a cvxpy dependency, and the excluded version appears to break our jenkins build. see: https://github.com/cvxgrp/scs/issues/283]
 
 setup(
     name="delphi_doctor_visits",

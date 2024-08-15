6c0abad7af8b01d79aded2fec06864ba67a9f7e0
minhkhul <minhkhul@andrew.cmu.edu>
2024-08-15 16:30:40 -0400

    rename tests for clarity + restructure test_patch tests to clarify purpose

--- nssp/tests/test_patch.py
+++ nssp/tests/test_patch.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 class TestPatchModule:
     @mock_patch('logging.Logger')
-    def test_good_patch_config_case1(self, mock_logger):
+    def test_config_missing_custom_run_and_patch_section(self, mock_logger):
         # Case 1: missing custom_run flag and patch section
         patch_config = {
             "common": {}
@@ -20,7 +20,7 @@ class TestPatchModule:
         ])
 
     @mock_patch('logging.Logger')
-    def test_good_patch_config_case2(self, mock_logger):
+    def test_config_missing_end_issue_in_patch_section(self, mock_logger):
         # Case 2: missing end_issue in patch section
         patch_config = {
             "common": {
@@ -38,7 +38,7 @@ class TestPatchModule:
         ])
 
     @mock_patch('logging.Logger')
-    def test_good_patch_config_case3(self, mock_logger):
+    def test_config_invalid_start_issue_and_missing_source_dir(self, mock_logger):
         # Case 3: start_issue not in yyyy-mm-dd format and source_dir doesn't exist.
         patch_config = {
             "common": {
@@ -58,7 +58,7 @@ class TestPatchModule:
         ])
 
     @mock_patch('logging.Logger')
-    def test_good_patch_config_case4(self, mock_logger):
+    def test_config_start_issue_after_end_issue(self, mock_logger):
         # Case 4: start_issue after end_issue
         patch_config = {
             "common": {
@@ -75,7 +75,7 @@ class TestPatchModule:
         mock_logger.error.assert_called_once_with("Start issue date is after end issue date.")
 
     @mock_patch('logging.Logger')
-    def test_good_patch_config_case5(self, mock_logger):
+    def test_config_all_valid_configurations(self, mock_logger):
         # Case 5: All good configuration
         patch_config = {
             "common": {
@@ -94,53 +94,12 @@ class TestPatchModule:
     @mock_patch('delphi_nssp.patch.run_module')
     @mock_patch('delphi_nssp.patch.get_structured_logger')
     @mock_patch('delphi_nssp.patch.read_params')
-    def test_patch(self, mock_read_params, mock_get_structured_logger, mock_run_module):
+    def test_patch_confirm_dir_structure_created(self, mock_read_params, mock_get_structured_logger, mock_run_module):
         mock_read_params.return_value = {
             "common": {
                 "log_filename": "test.log",
                 "custom_run": True
             },
-            "patch": {
-                "start_issue": "2021-01-01",
-                "end_issue": "2021-01-02",
-                "patch_dir": "./patch_dir",
-                "source_dir": "./source_dir"
-            }
-        }
-
-        patch()
-
-        assert 'current_issue' in mock_read_params.return_value['patch']
-        assert mock_read_params.return_value['patch']['current_issue'] == '2021-01-02'
-
-        assert os.path.isdir('./patch_dir')
-        assert os.path.isdir('./patch_dir/issue_20201227/nssp')
-
-        start_date = datetime(2020, 12, 28)
-        end_date = datetime(2021, 1, 3)
-        date = start_date
-
-        while date <= end_date:
-            date_str = date.strftime("%Y%m%d")
-            assert not os.path.isdir(f'./patch_dir/issue_{date_str}/nssp')
-            date += timedelta(days=1)
-
-        # Clean up the created directories after the test
-        shutil.rmtree(mock_read_params.return_value["patch"]["patch_dir"])
-
-    # @mock_patch('delphi_nssp.patch.run_module')
-    @mock_patch('delphi_nssp.patch.get_structured_logger')
-    @mock_patch('delphi_nssp.patch.read_params')
-    def test_patch(self, mock_read_params, mock_get_structured_logger):
-    # def test_patch(self, mock_read_params, mock_get_structured_logger, mock_run_module):
-        mock_read_params.return_value = {
-            "common": {
-                "log_filename": "test.log",
-                "custom_run": True
-            },
-            "indicator": {
-                "socrata_token": "test_token"
-                },
             "patch": {
                 "start_issue": "2021-01-01",
                 "end_issue": "2021-01-16",
@@ -163,6 +122,30 @@ class TestPatchModule:
             else:
                 assert not os.path.isdir(f'./patch_dir/issue_{date_str}/nssp')
             date += timedelta(days=1)
+
+        # Clean up the created directories after the test
+        shutil.rmtree(mock_read_params.return_value["patch"]["patch_dir"])
+
+    @mock_patch('delphi_nssp.patch.get_structured_logger')
+    @mock_patch('delphi_nssp.patch.read_params')
+    def test_patch_confirm_values_generated(self, mock_read_params, mock_get_structured_logger):
+        mock_read_params.return_value = {
+            "common": {
+                "log_filename": "test.log",
+                "custom_run": True
+            },
+            "indicator": {
+                "socrata_token": "test_token"
+                },
+            "patch": {
+                "start_issue": "2021-01-01",
+                "end_issue": "2021-01-16",
+                "patch_dir": "./patch_dir",
+                "source_dir": "./source_dir"
+            }
+        }
+
+        patch()
 
         # Make sure issue_20210103 has latest weekly data (data from 20210109 instead of 20210108)
         df_20210108 = pd.read_csv('source_dir/2021-01-08.csv')

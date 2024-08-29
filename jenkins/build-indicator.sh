e9528508d45e619656bafc1a26944bf7e39953e1
Dmitry Shemetov <dshemetov@ucdavis.edu>
2024-08-29 09:13:32 -0700

    build: fix Jenkins build script

--- jenkins/build-indicator.sh
+++ jenkins/build-indicator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Jenkins build 
+# Jenkins build
 #
 
 set -eo pipefail
@@ -17,4 +17,4 @@ source env/bin/activate
 pip install pip==23.0.1 --retries 10 --timeout 20
 pip install numpy --retries 10 --timeout 20
 pip install ../_delphi_utils_python/. --retries 10 --timeout 20
-[ ! -f setup.py ] || pip install . --retries 10 --timeout 20
+[ ! -f pyproject.toml ] || pip install . --retries 10 --timeout 20

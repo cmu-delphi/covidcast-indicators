9408c81ddfb2f59d15c7f109ec4813c33340d935
Amaris Sim <aysim319@gmail.com>
2024-09-09 10:28:46 -0400

    reverting unneeded change

--- doctor_visits/tests/test_update_sensor.py
+++ doctor_visits/tests/test_update_sensor.py
@@ -4,17 +4,24 @@ import logging
 import pandas as pd
 
 from delphi_doctor_visits.update_sensor import update_sensor
+from delphi_doctor_visits.process_data import csv_to_df
 
 TEST_LOGGER = logging.getLogger()
 
 class TestUpdateSensor:
+    start_date = datetime(2020, 2, 4)
+    end_date = datetime(2020, 2, 5)
+    drop_date = datetime(2020, 2, 6)
     def test_update_sensor(self):
-        df = pd.read_pickle("./test_data/SYNEDI_AGG_OUTPATIENT_07022020_1455CDT.pkl")
+        claims_df = csv_to_df(filepath="./test_data/SYNEDI_AGG_OUTPATIENT_07022020_1455CDT.csv.gz",
+                              startdate=self.start_date, enddate=self.end_date, dropdate=self.drop_date,
+                              logger=TEST_LOGGER)
+
         actual = update_sensor(
-            data=df,
-            startdate=datetime(2020, 2, 4),
-            enddate=datetime(2020, 2, 5),
-            dropdate=datetime(2020, 2,6),
+            data=claims_df,
+            startdate=self.start_date,
+            enddate=self.end_date,
+            dropdate=self.drop_date,
             geo="state",
             parallel=False,
             weekday=False,

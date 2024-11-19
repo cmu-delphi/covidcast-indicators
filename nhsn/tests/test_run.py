9a9780bd1920c5e22ca78a4579cb2ba34440bec0
Amaris Sim <aysim319@gmail.com>
2024-11-19 10:22:09 -0500

    working on test

--- nhsn/tests/test_run.py
+++ nhsn/tests/test_run.py
@@ -1,61 +1,41 @@
 from os import listdir
+
+import pandas as pd
 import pytest
+
+from delphi_nhsn.constants import SIGNALS_MAP
 
 
 class TestRun:
     # the 14th was a Monday
-    @pytest.mark.parametrize("date", ["2020-09-14", "2020-09-17", "2020-09-18"])
-    def test_output_files_exist(self, params, run_as_module, date):
-        for output_folder in folders:
+    def test_output_files_exist(self, params, run_as_module):
+        for output_folder in params["common"]["export_dir"]:
             csv_files = listdir(output_folder)
 
             geos = ["nation", "state"]
             dates = [
-                "202030",
-                "202031",
-                "202032",
-                "202033",
-                "202034",
-                "202035",
-                "202036",
+                "2021-08-21", "2021-08-28", "2021-09-04",
+                "2021-09-11", "2021-09-18", "2021-09-25",
+                "2021-10-02", "2021-10-16"
             ]
-            metrics = ['deaths_covid_incidence',
-                       'deaths_allcause_incidence',
-                       'deaths_percent_of_expected',
-                       'deaths_pneumonia_notflu_incidence',
-                       'deaths_covid_and_pneumonia_notflu_incidence',
-                       'deaths_flu_incidence',
-                       'deaths_pneumonia_or_flu_or_covid_incidence']
-            sensors = ["num", "prop"]
+            metrics = SIGNALS_MAP.values()
 
             expected_files = []
             for geo in geos:
                 for d in dates:
                     for metric in metrics:
-                        if metric == "deaths_percent_of_expected":
-                            expected_files += [f"weekly_{d}_{geo}_{metric}.csv"]
-                        else:
-                            for sensor in sensors:
-                                expected_files += [f"weekly_{d}_{geo}_{metric}_{sensor}.csv"]
+                        expected_files += [f"weekly_{d}_{geo}_{metric}.csv"]
             assert set(expected_files).issubset(set(csv_files))
 
-    # the 14th was a Monday
-    @pytest.mark.parametrize("date", ["2020-09-14", "2020-09-17", "2020-09-18"])
-    def test_output_file_format(self, run_as_module, date):
-        is_mon_or_thurs = dt.datetime.strptime(date, "%Y-%m-%d").weekday() == (0 or 3)
-
-        folders = ["daily_cache"]
-        if is_mon_or_thurs:
-            folders.append("receiving")
-
-        geos = ["nation", "state"]
-        for geo in geos:
-            for output_folder in folders:
-                df = pd.read_csv(
-                    join(output_folder, f"weekly_202026_{geo}_deaths_covid_incidence_prop.csv")
-                )
-                expected_columns = [
-                    "geo_id", "val", "se", "sample_size",
-                    "missing_val", "missing_se", "missing_sample_size"
-                ]
-                assert (df.columns.values == expected_columns).all()
+    # def test_output_file_format(self, run_as_module, params):
+    #     geos = ["nation", "state"]
+    #     for geo in geos:
+    #         for output_folder in params["common"]["export_dir"]:
+    #             df = pd.read_csv(
+    #                 join(output_folder, f"weekly_202026_{geo}_deaths_covid_incidence_prop.csv")
+    #             )
+    #             expected_columns = [
+    #                 "geo_id", "val", "se", "sample_size",
+    #                 "missing_val", "missing_se", "missing_sample_size"
+    #             ]
+    #             assert (df.columns.values == expected_columns).all()

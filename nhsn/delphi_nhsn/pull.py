6e1e54ac4d822dab609bc24e5c35bfdcd31d4bab
Amaris Sim <aysim319@gmail.com>
2025-01-14 09:57:13 -0500

    making dataset_id into constants

--- nhsn/delphi_nhsn/pull.py
+++ nhsn/delphi_nhsn/pull.py
@@ -8,7 +8,8 @@ import pandas as pd
 from delphi_utils import create_backup_csv
 from sodapy import Socrata
 
-from .constants import PRELIM_SIGNALS_MAP, PRELIM_TYPE_DICT, SIGNALS_MAP, TYPE_DICT
+from .constants import PRELIM_SIGNALS_MAP, PRELIM_TYPE_DICT, SIGNALS_MAP, TYPE_DICT, \
+    MAIN_DATASET_ID, PRELIM_DATASET_ID
 
 
 def pull_data(socrata_token: str, dataset_id: str):
@@ -89,7 +90,7 @@ def pull_nhsn_data(
     """
     # Pull data from Socrata API
     df = (
-        pull_data(socrata_token, dataset_id="ua7e-t2fy")
+        pull_data(socrata_token, dataset_id=MAIN_DATASET_ID)
         if not custom_run
         else pull_data_from_file(backup_dir, issue_date, logger, prelim_flag=False)
     )
@@ -145,7 +146,7 @@ def pull_preliminary_nhsn_data(
         Dataframe as described above.
     """
     df = (
-        pull_data(socrata_token, dataset_id="mpgq-jmmr")
+        pull_data(socrata_token, dataset_id=PRELIM_DATASET_ID)
         if not custom_run
         else pull_data_from_file(backup_dir, issue_date, logger, prelim_flag=True)
     )
